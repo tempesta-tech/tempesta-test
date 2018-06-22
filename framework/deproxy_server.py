4d99043dd19c7df645abf543b6b8762c2ca97a8b
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-06-22 23:57:41 +0300

    Don't count connections

--- framework/deproxy_server.py
+++ framework/deproxy_server.py
@@ -88,11 +88,11 @@ class BaseDeproxyServer(deproxy.Server, port_checks.FreePortsChecker):
             # --------- ATTENTION -------------
             # Due to the polling cycle, creating new connection can be
             # performed before removing old connection.
-            # So we can have case with expected + 1 amount of connections
+            # So we can have case with > expected amount of connections
             # It's not a error case, it's a problem of polling
-            assert len(self.connections) <= self.conns_n + 1, \
-                ('Too many connections, expect %d, got %d'
-                 % (self.conns_n, len(self.connections)))
+            #assert len(self.connections) <= self.conns_n, \
+            #    ('Too many connections, expect %d, got %d'
+            #     % (self.conns_n, len(self.connections)))
 
     def run_start(self):
         tf_cfg.dbg(3, '\tDeproxy: Server: Start on %s:%d.' % \
