7cbc29f36aabec4cd111b3e6090eaf29e4644a4a
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-07-31 16:10:58 +0300

    Print errors and failures

--- run_tests.py
+++ run_tests.py
@@ -307,6 +307,9 @@ if not tests or (test_resume.state.last_id == tests[-1].id()
                  and test_resume.state.last_completed):
     state_reader.drop()
 
+tf_cfg.dbg(1, "Errors: %i" % len(result.errors))
+tf_cfg.dbg(1, "Failures: %i" % len(result.failures))
+
 if len(result.errors) > 0:
     sys.exit(1)
 
