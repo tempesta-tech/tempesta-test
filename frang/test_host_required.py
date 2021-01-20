e34f766043a6d21c827c64d42ef7742551802750
Ivan Koveshnikov <ikoveshnik@gmail.com>
2021-01-21 00:14:49 +0500

    Add tests for frang directive `http_host_required': basic h2 test

--- frang/test_host_required.py
+++ frang/test_host_required.py
@@ -221,3 +221,108 @@ class HostHeader(tester.TempestaTest):
         self.assertTrue(deproxy_cl.connection_is_closed())
         self.assertEqual(klog.warn_count(self.WARN_IP_ADDR), 1,
                           "Frang limits warning is not shown")
+
+
+class AuthorityHeader(tester.TempestaTest):
+    """Test for 'http_host_required' directive, h2 protocol version,
+    Curl is not flexible as Deproxy, so we cant set headers as we want to,
+    so only basic tests are done here. Key `=H "host:..."` sets authority
+    header for h2, not Host header.
+    """
+
+    clients = [
+        {
+            'id' : 'curl-ip',
+            'type' : 'external',
+            'binary' : 'curl',
+            'cmd_args' : (
+                '-kf ' # Set non-null return code on 4xx-5xx responses.
+                'https://${tempesta_ip}/ '
+                )
+        },
+        {
+            'id' : 'curl-dns',
+            'type' : 'external',
+            'binary' : 'curl',
+            'cmd_args' : (
+                '-kf ' # Set non-null return code on 4xx-5xx responses.
+                'https://${tempesta_ip}/ '
+                '-H "host: tempesta-test.com"'
+                )
+        },
+    ]
+
+    backends = [
+        {
+            'id' : '0',
+            'type' : 'deproxy',
+            'port' : '8000',
+            'response' : 'static',
+            'response_content' :
+                'HTTP/1.1 200 OK\r\n'
+                'Content-Length: 0\r\n'
+                'Connection: keep-alive\r\n\r\n'
+        }
+    ]
+
+    tempesta = {
+        'config' : """
+            cache 0;
+            listen 443 proto=h2;
+
+            tls_certificate ${tempesta_workdir}/tempesta.crt;
+            tls_certificate_key ${tempesta_workdir}/tempesta.key;
+
+            frang_limits {
+                http_host_required;
+            }
+
+            server ${server_ip}:8000;
+
+        """
+    }
+
+    WARN_IP_ADDR = "Warning: frang: Host header field contains IP address"
+
+    def test_pass(self):
+        """ Authority header contains host in DNS form, request is allowed.
+        """
+        curl = self.get_client('curl-dns')
+
+        self.start_all_servers()
+        self.start_tempesta()
+        srv = self.get_server('0')
+        self.deproxy_manager.start()
+        self.assertTrue(srv.wait_for_connections(timeout=1))
+        klog = dmesg.DmesgFinder(ratelimited=False)
+
+        curl.start()
+        self.wait_while_busy(curl)
+        self.assertEqual(0, curl.returncode,
+                         msg=("Curl return code is not 0 (%d)." %
+                              (curl.returncode)))
+        self.assertEqual(klog.warn_count(self.WARN_IP_ADDR), 0,
+                          "Frang limits warning is incorrectly shown")
+        curl.stop()
+
+    def test_block(self):
+        """ Authority header  contains name in IP address form, request is
+        rejected.
+        """
+        curl = self.get_client('curl-ip')
+
+        self.start_all_servers()
+        self.start_tempesta()
+        srv = self.get_server('0')
+        self.deproxy_manager.start()
+        self.assertTrue(srv.wait_for_connections(timeout=1))
+        klog = dmesg.DmesgFinder(ratelimited=False)
+
+        curl.start()
+        self.wait_while_busy(curl)
+        self.assertEqual(1, curl.returncode,
+                         msg=("Curl return code is not 1 (%d)." %
+                              (curl.returncode)))
+        self.assertEqual(klog.warn_count(self.WARN_IP_ADDR), 1,
+                          "Frang limits warning is not shown")
+        curl.stop()
