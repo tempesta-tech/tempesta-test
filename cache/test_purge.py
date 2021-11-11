fbec3144893de5a452596ee11950010b0affe3c5
Alexey Dvoichenkov <ad@tempesta-tech.com>
2021-11-11 16:12:08 +0300

    functional tests for PURGE w/ update

--- cache/test_purge.py
+++ cache/test_purge.py
@@ -35,8 +35,124 @@ class TestPurge(functional.FunctionalTest):
             chains.proxy(method='HEAD', uri=uri),
             chains.cache(method='GET', uri=uri),
             chains.cache(method='HEAD', uri=uri)
-            ]
+        ]
         return result
 
     def test_purge(self):
         self.generic_test_routine(self.config, self.chains())
+
+    def purge_get(self, uri, method='GET'):
+        c = chains.proxy(uri=uri)
+        c.request.method = 'PURGE'
+        c.request.headers['X-Tempesta-Cache'] = method
+        c.request.update()
+        # A response body must be empty
+        c.response.headers['Content-Length'] = '0'
+        c.response.body = ''
+        c.response.update()
+        # Our X-header goes to an upstream
+        c.fwd_request.headers['X-Tempesta-Cache'] = method
+        return c
+
+    def test_purge_get_basic(self):
+        # Basic PURGE use.
+        uri = '/page.html'
+        ch = [
+            # All cacheable method to the resource must be cached
+            chains.proxy(method='GET', uri=uri),
+            chains.proxy(method='HEAD', uri=uri),
+            chains.cache(method='GET', uri=uri),
+            chains.cache(method='HEAD', uri=uri),
+            # PURGE + GET works like a cache update, so all following requests
+            # must be served from the cache.
+            self.purge_get(uri),
+            chains.cache(method='GET', uri=uri),
+            # Note that due to how Tempesta handles HEAD this doesn't help us
+            # with HEAD pre-caching.
+            chains.proxy(method='HEAD', uri=uri),
+            chains.cache(method='HEAD', uri=uri),
+        ]
+        self.generic_test_routine(self.config, ch)
+
+    def test_purge_get_update(self):
+        # Return a new response from the upstream to the PURGE+GET request
+        # pair, make sure we receive this new response from subsequent
+        # requests.
+        uri = '/page.html'
+        page = 'New page text!\n'
+        ch = [
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+            self.purge_get(uri),
+            chains.cache(method='GET', uri=uri),
+        ]
+        for c in (ch[2].server_response, ch[3].response):
+            c.body = page
+            c.headers['Content-Length'] = len(page)
+            c.update()
+        self.generic_test_routine(self.config, ch)
+
+    def test_useless_x_tempesta_cache(self):
+        # Send "X-Tempesta-Cache" with a GET request, and make sure it doesn't
+        # affect anything.
+        uri = '/page.html'
+        ch = [
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+        ]
+        for c in ch:
+            c.request.headers['X-Tempesta-Cache'] = 'GET'
+            c.request.update()
+            if c.fwd_request:
+                c.fwd_request.headers['X-Tempesta-Cache'] = 'GET'
+                c.fwd_request.update()
+        self.generic_test_routine(self.config, ch)
+
+    def test_purge_get_garbage(self):
+        # Send some garbage in the "X-Tempesta-Cache" header. The entry must
+        # be purged, but not re-cached. (This works the same way as a plain
+        # PURGE, so we're not using the helper method here.)
+        uri = '/page.html'
+        def purge(method):
+            p = chains.cache(method='PURGE', uri=uri)
+            p.request.headers['X-Tempesta-Cache'] = method
+            p.request.update()
+            return p
+        ch = [
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+            purge('FRED'),
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+            purge('GETWRONG'),
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+        ]
+        self.generic_test_routine(self.config, ch)
+
+    def test_purge_get_uncached(self):
+        # Send a PURGE request with X-Tempesta-Cache for a non-cached
+        # entry. Make sure a new cache entry is populated after the request.
+        uri = '/page.html'
+        ch = [
+            self.purge_get(uri),
+            chains.cache(method='GET', uri=uri),
+        ]
+        self.generic_test_routine(self.config, ch)
+
+    def test_purge_get_uncacheable(self):
+        # Send a PURGE request with "X-Tempesta-Cache" for an existing cache
+        # entry, and generate a non-cacheable response. Make sure that there
+        # is neither the old nor the new response in the cache.
+        uri = '/page.html'
+        purge = self.purge_get(uri)
+        for c in (purge.server_response, purge.response):
+            c.headers['Cache-Control'] = 'private'
+            c.update()
+        ch = [
+            chains.proxy(method='GET', uri=uri),
+            chains.cache(method='GET', uri=uri),
+            purge,
+            chains.proxy(method='GET', uri=uri),
+        ]
+        self.generic_test_routine(self.config, ch)
