e76d313cbf8993632ddf173938790657f741969d
pale-emperor <beloglazmv@gmail.com>
2022-06-07 14:06:13 +0300

    tls-perf added

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
-apt install python3-pip nginx net-tools -y
+apt install python3-pip nginx net-tools libssl-dev -y
 
 python3 -m pip install -r requirements.txt
+
+# tls-perf
+git clone https://github.com/tempesta-tech/tls-perf.git /tmp/tls-perf
+cd /tmp/tls-perf
+make
+ln -s /tmp/tls-perf/tls-perf /bin/tls-perf
