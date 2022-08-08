28b43aa2ea946f5b3e4ec2d71346755e45936277
Roman Belozerov <rb@tempesta-tech.com>
2022-08-08 16:42:40 +0400

    added plain configs for multiple_listeners/test_on_the_fly.py

--- multiple_listeners/config_for_tests_on_fly.py
+++ multiple_listeners/config_for_tests_on_fly.py
@@ -1,68 +1,68 @@
-backends = [
+# backends = [
+#
+#     {
+#         'id': 'nginx',
+#         'type': 'nginx',
+#         'port': '8000',
+#         'status_uri': 'http://127.0.0.100:8000/nginx_status',
+#         'config': """
+#             pid ${pid};
+#             worker_processes  auto;
+#             events {
+#                 worker_connections   1024;
+#                 use epoll;
+#             }
+#             http {
+#                 keepalive_timeout ${server_keepalive_timeout};
+#                 keepalive_requests ${server_keepalive_requests};
+#                 sendfile         on;
+#                 tcp_nopush       on;
+#                 tcp_nodelay      on;
+#                 open_file_cache max=1000;
+#                 open_file_cache_valid 30s;
+#                 open_file_cache_min_uses 2;
+#                 open_file_cache_errors off;
+#                 error_log /dev/null emerg;
+#                 access_log off;
+#                 server {
+#                     listen        127.0.0.100:8000;
+#                     location / {
+#                         return 200;
+#                     }
+#                     location /nginx_status {
+#                         stub_status on;
+#                     }
+#                 }
+#             }
+#         """,
+#     }
+#
+# ]
 
-    {
-        'id': 'nginx',
-        'type': 'nginx',
-        'port': '8000',
-        'status_uri': 'http://127.0.0.100:8000/nginx_status',
-        'config': """
-            pid ${pid};
-            worker_processes  auto;
-            events {
-                worker_connections   1024;
-                use epoll;
-            }
-            http {
-                keepalive_timeout ${server_keepalive_timeout};
-                keepalive_requests ${server_keepalive_requests};
-                sendfile         on;
-                tcp_nopush       on;
-                tcp_nodelay      on;
-                open_file_cache max=1000;
-                open_file_cache_valid 30s;
-                open_file_cache_min_uses 2;
-                open_file_cache_errors off;
-                error_log /dev/null emerg;
-                access_log off;
-                server {
-                    listen        127.0.0.100:8000;
-                    location / {
-                        return 200;
-                    }
-                    location /nginx_status {
-                        stub_status on;
-                    }
-                }
-            }
-        """,
-    }
-
-]
-
-tempesta = {
-    'config': """
-
-        %s
-
-        srv_group default {
-            server 127.0.0.100:8000;
-        }
-
-        vhost tempesta-cat {
-            proxy_pass default;
-        }
-
-        tls_match_any_server_name;
-        tls_certificate ECDSA/tfw-root.crt;
-        tls_certificate_key ECDSA/tfw-root.key;
-
-        cache 0;
-        cache_fulfill * *;
-        block_action attack reply;
-
-        http_chain {
-            -> tempesta-cat;
-        }
-
-    """
-}
+# tempesta = {
+#     'config': """
+#
+#         %s
+#
+#         srv_group default {
+#             server 127.0.0.100:8000;
+#         }
+#
+#         vhost tempesta-cat {
+#             proxy_pass default;
+#         }
+#
+#         tls_match_any_server_name;
+#         tls_certificate ECDSA/tfw-root.crt;
+#         tls_certificate_key ECDSA/tfw-root.key;
+#
+#         cache 0;
+#         cache_fulfill * *;
+#         block_action attack reply;
+#
+#         http_chain {
+#             -> tempesta-cat;
+#         }
+#
+#     """
+# }
--- multiple_listeners/test_on_the_fly.py
+++ multiple_listeners/test_on_the_fly.py
@@ -1,7 +1,6 @@
 """TestCase for change Tempesta config on the fly."""
 from framework import tester
-from multiple_listeners import config_for_tests_on_fly as tc
-
+# from multiple_listeners import config_for_tests_on_fly as tc
 
 __author__ = 'Tempesta Technologies, Inc.'
 __copyright__ = 'Copyright (C) 2022 Tempesta Technologies, Inc.'
@@ -10,62 +9,154 @@ __license__ = 'GPL2'
 WRK_SCRIPT = 'conn_close'  # with header 'connection: close'
 STATUS_OK = 200
 
-SOCKET_START = ('127.0.0.4:8282', )
-SOCKET_AFTER_RELOAD = ('127.0.1.5:7654', )
+SOCKET_START = ('127.0.0.4:8282',)
+SOCKET_AFTER_RELOAD = ('127.0.1.5:7654',)
 
 
-def make_tempesta_config(sockets: tuple) -> dict:
-    """
-    Add `listen` socket to Tempesta config.
+# def make_tempesta_config(sockets: tuple) -> dict:
+#     """
+#     Add `listen` socket to Tempesta config.
+#
+#     Args:
+#         sockets (tuple): sockets to add
+#
+#     Returns:
+#         config (dict): Tempesta config
+#
+#     """
+#     listen_parameters = ''
+#     for soc in sockets:
+#         listen_parameters += 'listen {0};\n\t'.format(soc)
+#     return {
+#         'config': tc.tempesta['config'] % listen_parameters,
+#     }
 
-    Args:
-        sockets (tuple): sockets to add
 
-    Returns:
-        config (dict): Tempesta config
-
-    """
-    listen_parameters = ''
-    for soc in sockets:
-        listen_parameters += 'listen {0};\n\t'.format(soc)
-    return {
-        'config': tc.tempesta['config'] % listen_parameters,
-    }
-
-
-def make_test_clients() -> list:
-    """
-    Create wrk clients.
-
-    Returns:
-        clients (list): created clients
-
-    """
-    test_clients = []
-    for soc in (SOCKET_START + SOCKET_AFTER_RELOAD):
-        test_clients.append(
-            {
-                'id': 'wrk-{0}'.format(soc),
-                'type': 'wrk',
-                'addr': soc,
-            }
-        )
-        test_clients.append(
-            {
-                'id': 'curl-{0}'.format(soc),
-                'type': 'external',
-                'binary': 'curl',
-                'cmd_args': '-Ikf http://{0}/'.format(soc),
-            }
-        )
-    return test_clients
-
+# def make_test_clients() -> list:
+#     """
+#     Create wrk clients.
+#
+#     Returns:
+#         clients (list): created clients
+#
+#     """
+#     test_clients = []
+#     for soc in (SOCKET_START + SOCKET_AFTER_RELOAD):
+#         test_clients.append(
+#             {
+#                 'id': 'wrk-{0}'.format(soc),
+#                 'type': 'wrk',
+#                 'addr': soc,
+#             }
+#         )
+#         test_clients.append(
+#             {
+#                 'id': 'curl-{0}'.format(soc),
+#                 'type': 'external',
+#                 'binary': 'curl',
+#                 'cmd_args': '-Ikf http://{0}/'.format(soc),
+#             }
+#         )
+#     return test_clients
+#
 
 class TestOnTheFly(tester.TempestaTest):
 
-    backends = tc.backends
-    clients = make_test_clients()
-    tempesta = make_tempesta_config(SOCKET_START)
+    backends = [
+        {
+            'id': 'nginx',
+            'type': 'nginx',
+            'port': '8000',
+            'status_uri': 'http://${server_ip}:8000/nginx_status',
+            'config': """
+                pid ${pid};
+                worker_processes  auto;
+                events {
+                    worker_connections   1024;
+                    use epoll;
+                }
+                http {
+                    keepalive_timeout ${server_keepalive_timeout};
+                    keepalive_requests ${server_keepalive_requests};
+                    sendfile         on;
+                    tcp_nopush       on;
+                    tcp_nodelay      on;
+                    open_file_cache max=1000;
+                    open_file_cache_valid 30s;
+                    open_file_cache_min_uses 2;
+                    open_file_cache_errors off;
+                    error_log /dev/null emerg;
+                    access_log off;
+                    server {
+                        listen        ${server_ip}:8000;
+                        location / {
+                            return 200;
+                        }
+                        location /nginx_status {
+                            stub_status on;
+                        }
+                    }
+                }
+            """,
+        }
+    ]
+
+    clients = [
+        {
+            'id': 'wrk-127.0.0.4:8282',
+            'type': 'wrk',
+            'addr': '127.0.0.4:8282'
+        },
+        {
+            'id': 'curl-127.0.0.4:8282',
+            'type': 'external',
+            'binary': 'curl',
+            'cmd_args': '-Ikf http://127.0.0.4:8282/'
+        },
+        {
+            'id': 'wrk-127.0.1.5:7654',
+            'type': 'wrk',
+            'addr': '127.0.1.5:7654'
+        },
+        {
+            'id': 'curl-127.0.1.5:7654',
+            'type': 'external',
+            'binary': 'curl',
+            'cmd_args': '-Ikf http://127.0.1.5:7654/'
+        },
+    ]
+
+    tempesta = {
+        'config': """
+
+            listen 127.0.0.4:8282;
+
+            srv_group default {
+                server ${server_ip}:8000;
+            }
+
+            vhost tempesta-cat {
+                proxy_pass default;
+            }
+
+            tls_match_any_server_name;
+            tls_certificate ECDSA/tfw-root.crt;
+            tls_certificate_key ECDSA/tfw-root.key;
+
+            cache 0;
+            cache_fulfill * *;
+            block_action attack reply;
+
+            http_chain {
+                -> tempesta-cat;
+            }
+
+        """
+    }
+
+    # backends = tc.backends
+    # clients = make_test_clients()
+    # tempesta = make_tempesta_config(SOCKET_START)
 
     def start_all(self):
         self.start_all_servers()
