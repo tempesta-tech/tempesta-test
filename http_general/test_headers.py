00ca732e2e4939dbce73323ec0cc5a3962dc111e
Alexander Shlemin <as@tempesta-tech.com>
2022-09-11 17:27:15 +0200

    Add WordPress login response test case (#290)
    Response contains multiple Set-Cookie headers

--- http_general/test_headers.py
+++ http_general/test_headers.py
@@ -9,6 +9,27 @@ __copyright__ = 'Copyright (C) 2022 Tempesta Technologies, Inc.'
 __license__ = 'GPL2'
 
 
+# Response from the WordPress POST /wp-login.php
+wordpress_login_response = """HTTP/1.1 200 OK
+Date: Thu, 08 Sep 2022 11:50:34 GMT
+Server: Apache/2.4.54 (Debian)
+X-Powered-By: PHP/7.4.30
+Expires: Wed, 11 Jan 1984 05:00:00 GMT
+Cache-Control: no-cache, must-revalidate, max-age=0
+Set-Cookie: wordpress_test_cookie=WP%20Cookie%20check; path=/
+X-Frame-Options: SAMEORIGIN
+Set-Cookie: wordpress_86a9106ae65537651a8e456835b316ab=admin%7C1662810634%7CY5HVGAwBX3g13hZEvGgwSf7fyUY1t5ZaPi2JsH8Fpsa%7C634effa8a901f9b410b6fd18ca0512039ffe2f362a0d70b6d82ff995b7f8be22; path=/wp-content/plugins; HttpOnly
+Set-Cookie: wordpress_86a9106ae65537651a8e456835b316ab=admin%7C1662810634%7CY5HVGAwBX3g13hZEvGgwSf7fyUY1t5ZaPi2JsH8Fpsa%7C634effa8a901f9b410b6fd18ca0512039ffe2f362a0d70b6d82ff995b7f8be22; path=/wp-admin; HttpOnly
+Set-Cookie: wordpress_logged_in_86a9106ae65537651a8e456835b316ab=admin%7C1662810634%7CY5HVGAwBX3g13hZEvGgwSf7fyUY1t5ZaPi2JsH8Fpsa%7Cd20c220a6974e7c1bdad6eb90b19b37986bbb06ada7bff996b55d0269c077c90; path=/; HttpOnly
+Vary: Accept-Encoding
+Content-Length: 15
+Keep-Alive: timeout=5, max=100
+Connection: Keep-Alive
+Content-Type: text/html; charset=UTF-8
+
+<!DOCTYPE html>"""
+
+
 class BackendSetCoookie(tester.TempestaTest):
 
     backends = [
@@ -35,6 +56,13 @@ class BackendSetCoookie(tester.TempestaTest):
                 'Content-Length: 0\r\n\r\n'
             ),
         },
+        {
+            'id': 'wordpress-login',
+            'type': 'deproxy',
+            'port': '8002',
+            'response': 'static',
+            'response_content': wordpress_login_response,
+        },
     ]
 
     tempesta = {
@@ -43,13 +71,16 @@ class BackendSetCoookie(tester.TempestaTest):
         listen 80;
         srv_group sg1 { server ${server_ip}:8000; }
         srv_group sg2 { server ${server_ip}:8001; }
+        srv_group sg3 { server ${server_ip}:8002; }
 
         vhost cookie1 { proxy_pass sg1; }  # single cookie
         vhost cookie2 { proxy_pass sg2; }  # two cookie headers
+        vhost wordpress-login { proxy_pass sg3; }  # WordPress login response
 
         http_chain {
           uri == "/cookie1" -> cookie1;
           uri == "/cookie2" -> cookie2;
+          uri == "/wordpress-login" -> wordpress-login;
         }
         """
     }
@@ -76,7 +107,8 @@ class BackendSetCoookie(tester.TempestaTest):
         client = self.get_client('deproxy')
         for path in (
                 "cookie1",  # single Set-Cookie header
-                "cookie2"  # two Set-Cookie headers
+                "cookie2",  # two Set-Cookie headers
+                "wordpress-login",  # WordPress response with multiple Set-Cookie headers
         ):
             with self.subTest("GET cookies", path=path):
                 client.make_request(f"GET /{path} HTTP/1.1\r\n\r\n")
