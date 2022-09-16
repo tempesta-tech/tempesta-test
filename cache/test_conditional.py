ee3ef9d058cd98f990761a91393da634bb27636e
Roman Belozerov <rb@tempesta-tech.com>
2022-09-16 10:59:54 +0400

    reworked test_conditional.py for new framework

--- cache/test_conditional.py
+++ cache/test_conditional.py
@@ -1,150 +1,197 @@
 """Functional tests of caching different methods."""
 
-from __future__ import print_function
-from helpers import tf_cfg, deproxy, chains
-from testers import functional
-
 __author__ = 'Tempesta Technologies, Inc.'
-__copyright__ = 'Copyright (C) 2017 Tempesta Technologies, Inc.'
+__copyright__ = 'Copyright (C) 2022 Tempesta Technologies, Inc.'
 __license__ = 'GPL2'
+
+from framework.deproxy_client import DeproxyClient
+from framework.deproxy_server import StaticDeproxyServer
+from framework.tester import TempestaTest
+from helpers import tf_cfg
+from helpers.deproxy import HttpMessage
+
 
 # TODO: Check that all headers that must be present in 304 response are there.
 # Not implemented since some of them (at least Vary) affect cache behaviour.
 
-class TestConditional(functional.FunctionalTest):
 
-    config = ('cache 2;\n'
-              'cache_fulfill * *;\n'
-              'cache_methods GET;\n')
+class TestConditional(TempestaTest):
+    tempesta = {
+        'config': """
+listen 80;
 
-    def make_304(self, response):
-        # Connection is added since tempesta send 'Connection: keep-alive' or
-        # 'Connection: close'
-        hdrs_304 = ['cache-control', 'content-location', 'date', 'etag',
-                    'expires', 'vary', 'connection']
-        hdrs = response.headers.keys()
-        for hdr in hdrs:
-            if not hdr in hdrs_304:
-                del response.headers[hdr]
-        response.status = '304'
-        response.body = ''
-        response.body_void = True
-        response.update()
+server ${server_ip}:8000;
 
-    def chains(self, etag=None, last_modified=None):
-        uri = '/page.html'
-        # Tests uses castom ETag and Last-Modified, so remove predefined
-        remove_hdrs = ['ETag', 'Last-Modified']
-        test_chains = [chains.proxy(method='GET', uri=uri),
-                       chains.cache(method='GET', uri=uri)]
-        for resp in [test_chains[0].response, test_chains[0].server_response,
-                     test_chains[1].response]:
-            for hdr in remove_hdrs:
-                del resp.headers[hdr]
-            if etag:
-                resp.headers['ETag'] = etag
-            if last_modified:
-                resp.headers['Last-Modified'] = last_modified
-            resp.update()
-        return test_chains
+vhost default {
+    proxy_pass default;
+}
 
-    def chains_200(self, cond_hdr, etag=None, last_modified=None):
-        chains = self.chains(etag, last_modified)
+cache 2;
+cache_fulfill * *;
+cache_methods GET;
+"""
+    }
 
-        hdr, val = cond_hdr
-        chains[1].request.headers[hdr] = val
-        chains[1].request.update()
+    backends = [
+        {
+            'id': 'deproxy',
+            'type': 'deproxy',
+            'port': '8000',
+            'response': 'static',
+            'response_content': '',
+        },
+    ]
 
-        return chains
+    clients = [
+        {
+            'id': 'deproxy',
+            'type': 'deproxy',
+            'addr': '${tempesta_ip}',
+            'port': '80',
+        },
+    ]
 
-    def chains_304(self, cond_hdr, etag=None, last_modified=None):
-        chains = self.chains_200(cond_hdr, etag, last_modified)
-        self.make_304(chains[1].response)
-        return chains
+    def start_all(self):
+        """Start all services."""
+        self.start_all_servers()
+        self.start_tempesta()
+        self.start_all_clients()
+        self.deproxy_manager.start()
+        self.assertTrue(self.wait_all_connections())
 
-    # If-None-Match. Client has cached resource.
+    def make_request(self, request: str):
+        """"""
+        client: DeproxyClient = self.get_client('deproxy')
+        curr_responses = len(client.responses)
+        client.make_request(request)
+        client.wait_for_response(timeout=1)
+        self.assertEqual(curr_responses + 1, len(client.responses), 'Response is lost.')
+
+    def _test(self, etag: str, expected_status_code: str, if_none_match: str = None,
+              if_modified_since: str = None, ):
+        """"""
+        self.start_all()
+        srv: StaticDeproxyServer = self.get_server('deproxy')
+        client: DeproxyClient = self.get_client('deproxy')
+
+        srv.set_response(
+            'HTTP/1.1 200 OK\r\n'
+            + 'Connection: keep-alive\r\n'
+            + 'Content-Length: 13\r\n'
+            + 'Content-Type: text/html\r\n'
+            + 'Server: Deproxy Server\r\n'
+            + 'Last-Modified: Mon, 12 Dec 2016 13:59:39 GMT\r\n'
+            + f'Date: {HttpMessage.date_time_string()}\r\n'
+            + f'Etag: {etag}\r\n'
+            + '\r\n'
+            + '<html></html>\r\n'
+        )
+
+        self.make_request(
+            f'GET /page.html HTTP/1.1\r\n'
+            + f'Host: {tf_cfg.cfg.get("Client", "hostname")}\r\n'
+            + 'Connection: keep-alive\r\n'
+            + 'Accept: */*\r\n'
+            + '\r\n',
+        )
+        self.assertIn(etag, str(client.last_response), )
+
+        if if_none_match:
+            option_header = f'if-none-match: {if_none_match}\r\n'
+        elif if_modified_since:
+            option_header = f'if-modified-since: {if_modified_since}\r\n'
+        else:
+            option_header = ''
+
+        self.make_request(
+            f'GET /page.html HTTP/1.1\r\n'
+            + f'Host: {tf_cfg.cfg.get("Client", "hostname")}\r\n'
+            + 'Connection: keep-alive\r\n'
+            + 'Accept: */*\r\n'
+            + option_header
+            + '\r\n'
+        )
+        self.assertIn(etag, str(client.last_response), )
+        self.assertEqual(client.last_response.status, expected_status_code, '')
+        self.assertEqual(len(srv.requests), 1, )
 
     def test_none_match(self):
-        "Client have cached resource, send 304"
-        etag = '"asdfqwerty"'
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', etag)
-        chains = self.chains_304(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Client have cached resource, send 304."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='"asdfqwerty"',
+            if_modified_since=None,
+        )
 
     def test_none_match_weak_srv(self):
-        "Same as test_none_match() but server sends weak etag"
-        etag = '"asdfqwerty"'
-        w_etag = 'W/%s' % etag
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', etag)
-        chains = self.chains_304(cond_hdr, w_etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Same as test_none_match() but server sends weak etag."""
+        self._test(
+            etag='W/"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='"asdfqwerty"',
+            if_modified_since=None,
+        )
 
     def test_none_match_weak_clnt(self):
-        "Same as test_none_match() but client sends weak etag"
-        etag = '"asdfqwerty"'
-        w_etag = 'W/%s' % etag
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', w_etag)
-        chains = self.chains_304(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Same as test_none_match() but client sends weak etag."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='W/"asdfqwerty"',
+            if_modified_since=None,
+        )
 
     def test_none_match_weak_all(self):
-        "Same as test_none_match() but both server and client send weak etag"
-        etag = '"asdfqwerty"'
-        w_etag = 'W/%s' % etag
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', w_etag)
-        chains = self.chains_304(cond_hdr, w_etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Same as test_none_match() but both server and client send weak etag."""
+        self._test(
+            etag='W/"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='W/"asdfqwerty"',
+            if_modified_since=None,
+        )
 
     def test_none_match_list(self):
-        """Same as test_none_match() but client has saved copies of several
-        resources"""
-        etag = '"asdfqwerty"'
-        etag_list = '"fsdfds", %s, "sdfrgg"' % etag
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', etag_list)
-        chains = self.chains_304(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Same as test_none_match() but client has saved copies of several resources."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='"fsdfds", "asdfqwerty", "sdfrgg"',
+            if_modified_since=None,
+        )
 
     def test_none_match_any(self):
-        """Same as test_none_match() but client has cached entire world
-        cached"""
-        etag = '"asdfqwerty"'
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', '*')
-        chains = self.chains_304(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
-
-    # If-None-Match. Client has no cached resource.
+        """Same as test_none_match() but client has cached entire world cached."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match='*',
+            if_modified_since=None,
+        )
 
     def test_none_match_nc(self):
-        "Client have no cached resource, send full response"
-        etag = '"asdfqwerty"'
-        etag_other = '"jfgfdgnjdn"'
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-None-Match', etag_other)
-        chains = self.chains_200(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
-
-    # If-Modified-Since
+        """Client have no cached resource, send full response."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='200',
+            if_none_match='"jfgfdgnjdn"',
+            if_modified_since=None,
+        )
 
     def test_mod_since(self):
-        "Client have cached resource, send 304"
-        etag = '"asdfqwerty"'
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        after_lm = " Mon, 19 Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-Modified-Since', after_lm)
-        chains = self.chains_304(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Client have cached resource, send 304."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='304',
+            if_none_match=None,
+            if_modified_since='Mon, 12 Dec 2016 13:59:39 GMT',
+        )
 
     def test_mod_since_nc(self):
-        "Client have no cached resource, send full response"
-        etag = '"asdfqwerty"'
-        lm = " Mon, 12 Dec 2016 13:59:39 GMT"
-        before_lm = " Mon, 5  Dec 2016 13:59:39 GMT"
-        cond_hdr = ('If-Modified-Since', before_lm)
-        chains = self.chains_200(cond_hdr, etag, lm)
-        self.generic_test_routine(self.config, chains)
+        """Client have no cached resource, send full response."""
+        self._test(
+            etag='"asdfqwerty"',
+            expected_status_code='200',
+            if_none_match=None,
+            if_modified_since='Mon, 5 Dec 2016 13:59:39 GMT',
+        )
