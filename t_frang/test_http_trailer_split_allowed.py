7ed92aae6e2c501fcfed8b833e5fef50425b05e0
ProshNad <n.proshutina@gmail.com>
2022-10-07 06:59:00 +0000

    changes for trailer split allowed

--- t_frang/test_http_trailer_split_allowed.py
+++ t_frang/test_http_trailer_split_allowed.py
@@ -16,7 +16,7 @@ Content-Length: 0\r\n
 Connection: keep-alive\r\n\r\n
 """
 
-TEMPESTA_CONF = """
+TEMPESTA_CONF_ON = """
 cache 0;
 listen 80;
 
@@ -29,9 +29,17 @@ frang_limits {
 server ${server_ip}:8000;
 """
 
+TEMPESTA_CONF_OFF = """
+cache 0;
+listen 80;
+
+
+server ${server_ip}:8000;
+"""
+
 WARN = 'frang: HTTP field appear in header and trailer'
 
-ACCEPTED_REQUESTS = 'POST / HTTP/1.1\r\n' \
+ACCEPTED_REQUESTS_LIMIT_ON = 'POST / HTTP/1.1\r\n' \
                 'Host: debian\r\n' \
                 'Transfer-Encoding: gzip, chunked\r\n' \
                 '\r\n' \
@@ -54,7 +62,7 @@ ACCEPTED_REQUESTS = 'POST / HTTP/1.1\r\n' \
                 'HdrTest: testVal\r\n' \
                 '\r\n' \
 
-NOT_ACCEPTED_REQUEST = 'GET / HTTP/1.1\r\n' \
+NOT_ACCEPTED_REQUEST_LIMIT_ON = 'GET / HTTP/1.1\r\n' \
                 'Host: debian\r\n' \
                 'HdrTest: testVal\r\n' \
                 'Transfer-Encoding: chunked\r\n' \
@@ -63,11 +71,10 @@ NOT_ACCEPTED_REQUEST = 'GET / HTTP/1.1\r\n' \
                 'test\r\n' \
                 '0\r\n' \
                 'HdrTest: testVal\r\n' \
-                '\r\n' \
+                '\r\n'
 
 
-
-class FrangHttpTrailerSplitTestCase(tester.TempestaTest):
+class FrangHttpTrailerSplitLimitOnTestCase(tester.TempestaTest):
 
     clients = [
         {
@@ -89,7 +96,7 @@ class FrangHttpTrailerSplitTestCase(tester.TempestaTest):
     ]
 
     tempesta = {
-        'config': TEMPESTA_CONF,
+        'config': TEMPESTA_CONF_ON,
     }
 
     def setUp(self):
@@ -108,48 +115,30 @@ class FrangHttpTrailerSplitTestCase(tester.TempestaTest):
         )
 
     def test_accepted_request(self):
-        """Test with content_type, success."""
         self.start_all()
 
         deproxy_cl = self.get_client('client')
         deproxy_cl.start()
         deproxy_cl.make_requests(
-            ACCEPTED_REQUESTS,
+            ACCEPTED_REQUESTS_LIMIT_ON,
         )
         deproxy_cl.wait_for_response(1)
         self.assertEqual(
             3,
             len(deproxy_cl.responses),
         )
-        assert list(p.status for p in deproxy_cl.responses) == ['200', '200', '200']
+        assert list(p.status for p in deproxy_cl.responses) == ['200'] * 3
         self.assertFalse(
             deproxy_cl.connection_is_closed(),
         )
 
     def test_not_accepted_request(self):
-        self._test_base_scenario(
-            request_body=NOT_ACCEPTED_REQUEST,
-        )
-
-
-    def _test_base_scenario(
-        self,
-        request_body: str,
-        expected_warning: str = WARN,
-    ):
-        """
-        Test base scenario for process different requests.
-
-        Args:
-            request_body (str): request body
-            expected_warning (str): expected warning in logs
-        """
         self.start_all()
 
         deproxy_cl = self.get_client('client')
         deproxy_cl.start()
         deproxy_cl.make_requests(
-            request_body,
+            NOT_ACCEPTED_REQUEST_LIMIT_ON,
         )
         deproxy_cl.wait_for_response()
 
@@ -161,7 +150,69 @@ class FrangHttpTrailerSplitTestCase(tester.TempestaTest):
             deproxy_cl.connection_is_closed(),
         )
         self.assertEqual(
-            self.klog.warn_count(expected_warning),
+            self.klog.warn_count(WARN),
             COUNT_WARNINGS_OK,
             ERROR_MSG,
+        )
+
+
+class FrangHttpTrailerSplitLimitOffTestCase(tester.TempestaTest):
+    """
+    Accept all requests
+    """
+
+    clients = [
+        {
+            'id': 'client',
+            'type': 'deproxy',
+            'addr': '${tempesta_ip}',
+            'port': '80',
+        },
+    ]
+
+    backends = [
+        {
+            'id': '0',
+            'type': 'deproxy',
+            'port': '8000',
+            'response': 'static',
+            'response_content': RESPONSE_CONTENT,
+        },
+    ]
+
+    tempesta = {
+        'config': TEMPESTA_CONF_OFF,
+    }
+
+    def setUp(self):
+        """Set up test."""
+        super().setUp()
+        self.klog = dmesg.DmesgFinder(ratelimited=False)
+
+    def start_all(self):
+        """Start all requirements."""
+        self.start_all_servers()
+        self.start_tempesta()
+        self.deproxy_manager.start()
+        srv = self.get_server('0')
+        self.assertTrue(
+            srv.wait_for_connections(timeout=1),
+        )
+
+    def test_accepted_request(self):
+        self.start_all()
+
+        deproxy_cl = self.get_client('client')
+        deproxy_cl.start()
+        deproxy_cl.make_requests(
+            ACCEPTED_REQUESTS_LIMIT_ON + NOT_ACCEPTED_REQUEST_LIMIT_ON,
+        )
+        deproxy_cl.wait_for_response(1)
+        self.assertEqual(
+            4,
+            len(deproxy_cl.responses),
+        )
+        assert list(p.status for p in deproxy_cl.responses) == ['200']*4
+        self.assertFalse(
+            deproxy_cl.connection_is_closed(),
         )
