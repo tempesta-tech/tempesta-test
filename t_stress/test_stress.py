734a727cec96cf2a4d6968f0ea104b995deb829c
Alexander Shlemin <as@tempesta-tech.com>
2022-10-19 14:53:58 +0200

    Replace deproxy server with NGINX in stress tests

--- t_stress/test_stress.py
+++ t_stress/test_stress.py
@@ -1,18 +1,20 @@
 """
 HTTP Stress tests - load Tempesta FW with multiple connections.
 """
+
+from pathlib import Path
 import time
 
 from helpers import remote, sysnet, tf_cfg
 from framework import tester
 
-__author__ = 'Tempesta Technologies, Inc.'
-__copyright__ = 'Copyright (C) 2022 Tempesta Technologies, Inc.'
-__license__ = 'GPL2'
+__author__ = "Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2022 Tempesta Technologies, Inc."
+__license__ = "GPL2"
 
 # Keep 10+ open connections
 CONCURRENT_CONNECTIONS = max(
-    int(tf_cfg.cfg.get('General', 'concurrent_connections')),
+    int(tf_cfg.cfg.get("General", "concurrent_connections")),
     10
 )
 # Number of threads to use for wrk and h2load tests
@@ -31,55 +33,42 @@ TEMPESTA_TO_SERVER_MTU = 0
 # Server -> Tempesta interface
 SERVER_TO_TEMPESTA_MTU = 0
 
-# Deproxy response content size in bytes
-LARGE_CONTENT_LENGTH = 1024 * 64 * 1024
+# Backend response content size in bytes
+LARGE_CONTENT_LENGTH = 1024 * 64
 
-# Deproxy server config with large response
-DEPROXY_CONFIG = {
-    'id': 'deproxy',
-    'type': 'deproxy',
-    'port': '8000',
-    'response': 'static',
-    'response_content': (
-        'HTTP/1.1 200 OK\r\n'
-        'Server: test\r\n'
-        'Date: test\r\n'
-        f"Content-Length: {LARGE_CONTENT_LENGTH}\r\n"
-        '\r\n'
-        f"{'@' * LARGE_CONTENT_LENGTH}"
-    ),
-}
-
-# Config to start Nginx instead of Tempesta on 80 and 443 ports
-NGINX_CONFIG = """
+# NGINX backend config with the large page response
+NGINX_LARGE_PAGE_CONFIG = """
 pid ${pid};
 worker_processes  auto;
+#error_log /dev/stdout info;
+error_log /dev/null emerg;
+
 events {
     worker_connections   1024;
     use epoll;
 }
+
 http {
-    keepalive_timeout ${server_keepalive_timeout};
-    keepalive_requests ${server_keepalive_requests};
+    keepalive_timeout 65;
+    keepalive_requests 100;
     sendfile         on;
     tcp_nopush       on;
     tcp_nodelay      on;
+
     open_file_cache max=1000;
     open_file_cache_valid 30s;
     open_file_cache_min_uses 2;
     open_file_cache_errors off;
-    error_log /dev/null emerg;
+
+    # Disable access log altogether.
     access_log off;
+
     server {
-        listen 80;
-        listen 443 ssl http2;
-        server_name tempesta-tech.com;
-        ssl_certificate ${tempesta_workdir}/tempesta.crt;
-        ssl_certificate_key ${tempesta_workdir}/tempesta.key;
-        ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-        ssl_ciphers HIGH:!aNULL:!MD5;
+        listen 8000;
+
         location / {
-            proxy_pass http://${server_ip}:8000;
+            root ${server_resources};
+            try_files /large.html =404;
         }
         location /nginx_status {
             stub_status on;
@@ -87,6 +76,46 @@ http {
     }
 }
 """
+
+
+class LargePageNginxBackendMixin:
+    """Mixin that adds `nginx-large-page` backend on 8000 port."""
+
+    nginx_backend_page_size = LARGE_CONTENT_LENGTH
+
+    @property
+    def large_page_path(self):
+        return Path(tf_cfg.cfg.get("Server", "resources")) / "large.html"
+
+    def setUp(self):
+        if self._base:
+            self.skipTest("This is an abstract class")
+        self.backends = [
+            *self.backends[:],
+            {
+                "id": "nginx-large-page",
+                "type": "nginx",
+                "port": "8000",
+                "status_uri": "http://${server_ip}:8000/nginx_status",
+                "config": NGINX_LARGE_PAGE_CONFIG,
+            },
+        ]
+        super().setUp()
+        self.create_large_page()
+
+    def tearDown(self):
+        super().tearDown()
+        self.remove_large_page()
+
+    def create_large_page(self):
+        server = self.get_server("nginx-large-page")
+        server.node.run_cmd(
+            f"fallocate -l {self.nginx_backend_page_size} {self.large_page_path}"
+        )
+
+    def remove_large_page(self):
+        server = self.get_server("nginx-large-page")
+        server.node.remove_file(str(self.large_page_path))
 
 
 class CustomMtuMixin:
@@ -99,18 +128,18 @@ class CustomMtuMixin:
         self._prev_mtu = {}
         self.set_mtu(
             node=remote.tempesta,
-            destination_ip=tf_cfg.cfg.get('Client', 'ip'),
-            mtu=TEMPESTA_TO_CLIENT_MTU
+            destination_ip=tf_cfg.cfg.get("Client", "ip"),
+            mtu=TEMPESTA_TO_CLIENT_MTU,
         )
         self.set_mtu(
             node=remote.tempesta,
-            destination_ip=tf_cfg.cfg.get('Server', 'ip'),
-            mtu=TEMPESTA_TO_SERVER_MTU
+            destination_ip=tf_cfg.cfg.get("Server", "ip"),
+            mtu=TEMPESTA_TO_SERVER_MTU,
         )
         self.set_mtu(
             node=remote.server,
-            destination_ip=tf_cfg.cfg.get('Tempesta', 'ip'),
-            mtu=SERVER_TO_TEMPESTA_MTU
+            destination_ip=tf_cfg.cfg.get("Tempesta", "ip"),
+            mtu=SERVER_TO_TEMPESTA_MTU,
         )
 
     def tearDown(self):
@@ -124,31 +153,24 @@ class CustomMtuMixin:
     def set_mtu(self, node, destination_ip, mtu):
         if mtu:
             dev = sysnet.route_dst_ip(node=node, ip=destination_ip)
-            prev = sysnet.change_mtu(
-                node=node,
-                dev=dev,
-                mtu=mtu
-            )
+            prev = sysnet.change_mtu(node=node, dev=dev, mtu=mtu)
             if not dev in self._prev_mtu:
                 self._prev_mtu[dev] = [node, dev, prev]
 
 
-class BaseWrkStress(CustomMtuMixin, tester.TempestaTest, base=True):
-    """Stress test generated by `wrk` with concurrent connections."""
-
-    backends = [
-        DEPROXY_CONFIG,
-    ]
+class BaseWrkStress(
+    CustomMtuMixin, LargePageNginxBackendMixin, tester.TempestaTest, base=True
+):
+    """Base class for `wrk` stress tests."""
 
     def start_all(self):
         self.start_all_servers()
-        self.deproxy_manager.start()
         self.start_tempesta()
         self.assertTrue(self.wait_all_connections(1))
 
     def test_concurrent_connections(self):
         self.start_all()
-        wrk = self.get_client('wrk')
+        wrk = self.get_client("wrk")
         wrk.set_script("foo", content='wrk.method="GET"')
         wrk.connections = CONCURRENT_CONNECTIONS
         wrk.duration = int(DURATION)
@@ -166,7 +188,7 @@ class WrkStress(BaseWrkStress):
     """HTTP stress test generated by `wrk` with concurrent connections."""
 
     tempesta = {
-        'config' : """
+        "config": """
             listen 80 proto=http;
             server ${server_ip}:8000;
             cache 0;
@@ -175,9 +197,9 @@ class WrkStress(BaseWrkStress):
 
     clients = [
         {
-            'id' : 'wrk',
-            'type' : 'wrk',
-            'addr' : "${tempesta_ip}:80/1",
+            "id": "wrk",
+            "type": "wrk",
+            "addr": "${tempesta_ip}:80/1",
         },
     ]
 
@@ -186,7 +208,7 @@ class TlsWrkStress(BaseWrkStress):
     """HTTPS stress test generated by `wrk` with concurrent connections."""
 
     tempesta = {
-        'config' : """
+        "config": """
         listen 443 proto=https;
         tls_certificate ${tempesta_workdir}/tempesta.crt;
         tls_certificate_key ${tempesta_workdir}/tempesta.key;
@@ -198,20 +220,18 @@ class TlsWrkStress(BaseWrkStress):
 
     clients = [
         {
-            'id' : 'wrk',
-            'type' : 'wrk',
-            'ssl': True,
-            'addr' : "${tempesta_ip}:443",
+            "id": "wrk",
+            "type": "wrk",
+            "ssl": True,
+            "addr": "${tempesta_ip}:443",
         },
     ]
 
 
-class BaseCurlStress(CustomMtuMixin, tester.TempestaTest, base=True):
+class BaseCurlStress(
+    CustomMtuMixin, LargePageNginxBackendMixin, tester.TempestaTest, base=True
+):
     """Base class for HTTPS ans HTTP/2 stress tests with `curl`."""
-
-    backends = [
-        DEPROXY_CONFIG,
-    ]
 
     tempesta_tmpl = """
         listen 443 proto=%s;
@@ -225,46 +245,42 @@ class BaseCurlStress(CustomMtuMixin, tester.TempestaTest, base=True):
     clients = [
         # Client to make a single request
         {
-            'id': 'single',
-            'type': 'curl',
-            'uri': f"/1",
-            'headers': {
-                'Connection': 'close',
+            "id": "single",
+            "type": "curl",
+            "uri": f"/1",
+            "headers": {
+                "Connection": "close",
             },
-            'cmd_args': (
-                f" --max-time {DURATION}"
-            ),
+            "cmd_args": (f" --max-time {DURATION}"),
+            "disable_output": True,
         },
         # Client to request URLs (/1 /2 ..) sequentially
         {
-            'id': 'sequential',
-            'type': 'curl',
-            'uri': f"/[1-{REQUESTS_COUNT}]",
-            'headers': {
-                'Connection': 'close',
+            "id": "sequential",
+            "type": "curl",
+            "uri": f"/[1-{REQUESTS_COUNT}]",
+            "headers": {
+                "Connection": "close",
             },
-            'cmd_args': (
-                f" --max-time {DURATION}"
-            ),
+            "cmd_args": (f" --max-time {DURATION}"),
+            "disable_output": True,
         },
         # Client to request URLs (/1 /2 ..) in a pipilene
         {
-            'id': 'pipelined',
-            'type': 'curl',
-            'uri': f"/[1-{REQUESTS_COUNT}]",
-            'cmd_args': (
-                f" --max-time {DURATION}"
-            ),
+            "id": "pipelined",
+            "type": "curl",
+            "uri": f"/[1-{REQUESTS_COUNT}]",
+            "cmd_args": (f" --max-time {DURATION}"),
+            "disable_output": True,
         },
         # Client to request URLs (/1 /2 ..) in parallel
         {
-            'id': 'concurrent',
-            'type': 'curl',
-            'uri': f"/[1-{REQUESTS_COUNT}]",
-            'parallel': CONCURRENT_CONNECTIONS,
-            'cmd_args': (
-                f" --max-time {DURATION}"
-            ),
+            "id": "concurrent",
+            "type": "curl",
+            "uri": f"/[1-{REQUESTS_COUNT}]",
+            "parallel": CONCURRENT_CONNECTIONS,
+            "cmd_args": (f" --max-time {DURATION}"),
+            "disable_output": True,
         },
     ]
 
@@ -272,13 +288,12 @@ class BaseCurlStress(CustomMtuMixin, tester.TempestaTest, base=True):
         if self._base:
             self.skipTest("This is an abstract class")
         self.tempesta = {
-            'config': self.tempesta_tmpl % (self.proto),
+            "config": self.tempesta_tmpl % (self.proto),
         }
         super().setUp()
 
     def start_all(self):
         self.start_all_servers()
-        self.deproxy_manager.start()
         self.start_tempesta()
         self.assertTrue(self.wait_all_connections(1))
 
@@ -294,7 +309,7 @@ class BaseCurlStress(CustomMtuMixin, tester.TempestaTest, base=True):
     def test_range_requests(self):
         """Send requests sequentially, stop on error."""
         self.start_all()
-        client = self.get_client('single')
+        client = self.get_client("single")
         started = time.time()
         delta = 0
 
@@ -307,75 +322,70 @@ class BaseCurlStress(CustomMtuMixin, tester.TempestaTest, base=True):
 
             response = client.last_response
             self.assertFalse(
-                response.stderr,
-                f"Error after {delta} seconds and {i} requests."
+                response.stderr, f"Error after {delta} seconds and {i} requests."
             )
             self.assertEqual(response.status, 200)
             self.assertEqual(
-                int(client.last_response.headers['content-length']),
-                LARGE_CONTENT_LENGTH
+                int(client.last_response.headers["content-length"]),
+                LARGE_CONTENT_LENGTH,
             )
 
-        tf_cfg.dbg(2, f"Test completed after {time.time() - started} seconds and {i} requests.")
+        tf_cfg.dbg(
+            2, f"Test completed after {time.time() - started} seconds and {i} requests."
+        )
 
     def test_sequential_requests(self):
         """Send requests sequentially, continue on errors."""
-        self.make_requests('sequential')
+        self.make_requests("sequential")
 
     def test_pipelined_requests(self):
         """Send requests in a single pipeline."""
-        self.make_requests('pipelined')
+        self.make_requests("pipelined")
 
     def test_concurrent_requests(self):
         """Send requests in parallel."""
-        self.make_requests('concurrent')
+        self.make_requests("concurrent")
 
 
 class CurlStress(BaseCurlStress):
     """HTTP stress test generated by `curl`."""
+
     tempesta_tmpl = """
         listen 80 proto=%s;
         server ${server_ip}:8000;
         cache 0;
     """
-    proto = 'http'
+    proto = "http"
 
 
 class TlsCurlStress(BaseCurlStress):
     """HTTPS stress test generated by `curl`."""
-    proto = 'https'
+
+    proto = "https"
 
     def setUp(self):
-        self.clients = [
-            {**client, 'ssl': True}
-            for client in self.clients
-        ]
+        self.clients = [{**client, "ssl": True} for client in self.clients]
         super().setUp()
 
 
 class H2CurlStress(BaseCurlStress):
     """HTTP/2 stress test generated by `curl`."""
-    proto = 'h2'
+
+    proto = "h2"
 
     def setUp(self):
-        self.clients = [
-            {**client, 'http2': True}
-            for client in self.clients
-        ]
+        self.clients = [{**client, "http2": True} for client in self.clients]
         super().setUp()
 
 
-class H2LoadStress(CustomMtuMixin, tester.TempestaTest):
+class H2LoadStress(CustomMtuMixin, LargePageNginxBackendMixin, tester.TempestaTest):
     """
     HTTP/2 stress test generated by`h2load`,
     with multiple streams per connection.
     """
-    backends = [
-        DEPROXY_CONFIG,
-    ]
 
     tempesta = {
-        'config': """
+        "config": """
             listen 443 proto=h2;
             server ${server_ip}:8000;
             tls_certificate ${tempesta_workdir}/tempesta.crt;
@@ -387,29 +397,28 @@ class H2LoadStress(CustomMtuMixin, tester.TempestaTest):
 
     clients = [
         {
-            'id' : 'h2load',
-            'type' : 'external',
-            'binary' : 'h2load',
-            'ssl' : True,
-            'cmd_args' : (
-                ' https://${tempesta_ip}'
+            "id": "h2load",
+            "type": "external",
+            "binary": "h2load",
+            "ssl": True,
+            "cmd_args": (
+                " https://${tempesta_ip}"
                 f" --clients {CONCURRENT_CONNECTIONS}"
                 f" --threads {THREADS}"
                 f" --max-concurrent-streams {REQUESTS_COUNT}"
                 f" --duration {DURATION}"
-        )
+            ),
         },
     ]
 
     def start_all(self):
         self.start_all_servers()
-        self.deproxy_manager.start()
         self.start_tempesta()
         self.assertTrue(self.wait_all_connections(1))
 
     def test(self):
         self.start_all()
-        client = self.get_client('h2load')
+        client = self.get_client("h2load")
         client.start()
         self.wait_while_busy(client)
         client.stop()
