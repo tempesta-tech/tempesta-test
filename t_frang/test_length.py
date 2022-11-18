e770f6c82f1bb71a935eb8c03f989f4c34a305d1
Roman <rb@tempesta-tech.com>
2022-11-18 17:09:25 +0400

    parametrization of test_length.py

--- t_frang/test_length.py
+++ t_frang/test_length.py
@@ -9,104 +9,6 @@ __license__ = "GPL2"
 class FrangLengthTestCase(FrangTestCase):
     """Tests for length related directives."""
 
-    clients = [
-        {
-            "id": "curl-1",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://${server_ip}:8765/over5 -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-11",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://${server_ip}:8765 -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-12",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://${server_ip}:8765/qwe -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-13",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://${server_ip}:8765/1234 -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-2",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://{0}:8765/ -H "Host: tempesta-tech.com:8765"  -H "X-Long: {1}"'.format(
-                '${server_ip}',
-                '1'*293
-                ),
-        },
-        {
-            "id": "curl-22",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-Ikf -v http://${server_ip}:8765/ -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-3",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-kf -v http://{0}:8765/ -H "Host: tempesta-tech.com:8765" -d {1}'.format(
-                '${server_ip}',
-                {"some_key_long_one": "some_value"},
-            ),
-        },
-        {
-            "id": "curl-31",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-kf -v http://${server_ip}:8765/ -H "Host: tempesta-tech.com:8765"',
-        },
-        {
-            "id": "curl-32",
-            "type": "external",
-            "binary": "curl",
-            "cmd_args": '-kf -v http://{0}:8765/ -H "Host: tempesta-tech.com:8765" -d {1}'.format(
-                '${server_ip}',
-                {"12345678": "1"},
-            ),
-        },
-    ]
-
-    tempesta = {
-        "config": """
-            frang_limits {
-                http_uri_len 5;
-                http_field_len 300;
-                http_body_len 10;
-            }
-
-            listen ${server_ip}:8765;
-
-            srv_group default {
-                server ${server_ip}:8000;
-            }
-
-            vhost tempesta-cat {
-                proxy_pass default;
-            }
-
-            tls_match_any_server_name;
-            tls_certificate ${tempesta_workdir}/tempesta.crt;
-            tls_certificate_key ${tempesta_workdir}/tempesta.key;
-
-            cache 0;
-            cache_fulfill * *;
-            block_action attack reply;
-
-            http_chain {
-                -> tempesta-cat;
-            }
-        """,
-    }
-
     def test_uri_len(self):
         """
         Test 'http_uri_len'.
@@ -114,88 +16,27 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_uri_len 5;` and make request with uri greater length
 
         """
-        curl = self.get_client("curl-1")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            1,
+        client = self.base_scenario(
+            frang_config="http_uri_len 5;",
+            requests=["POST /123456789 HTTP/1.1\r\nHost: localhost\r\n\r\n"],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP URI length exceeded for",
-            ),
-            1,
+        self.check_response(
+            client, status_code="403", warning_msg="frang: HTTP URI length exceeded for"
         )
 
-        curl.stop()
-
-    def test_uri_len_without_reaching_the_limit_zero_len(self):
+    def test_uri_len_without_reaching_the_limit(self):
         """
         Test 'http_uri_len'.
 
         Set up `http_uri_len 5;` and make request with uri 1 length
 
         """
-        curl = self.get_client("curl-11")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
+        client = self.base_scenario(
+            frang_config="http_uri_len 5;", requests=["POST / HTTP/1.1\r\nHost: localhost\r\n\r\n"]
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP URI length exceeded for",
-            ),
-            0,
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP URI length exceeded for"
         )
-
-        curl.stop()
-
-    def test_uri_len_without_reaching_the_limit(self):
-        """
-        Test 'http_uri_len'.
-
-        Set up `http_uri_len 5;` and make request with uri 4 length
-
-        """
-        curl = self.get_client("curl-12")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
-        )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP URI length exceeded for",
-            ),
-            0,
-        )
-
-        curl.stop()
 
     def test_uri_len_on_the_limit(self):
         """
@@ -204,28 +45,13 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_uri_len 5;` and make request with uri 5 length
 
         """
-        curl = self.get_client("curl-13")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
+        client = self.base_scenario(
+            frang_config="http_uri_len 5;",
+            requests=["POST /1234 HTTP/1.1\r\nHost: localhost\r\n\r\n"],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP URI length exceeded for",
-            ),
-            0,
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP URI length exceeded for"
         )
-
-        curl.stop()
 
     def test_field_len(self):
         """
@@ -234,58 +60,43 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_field_len 300;` and make request with header greater length
 
         """
-        curl = self.get_client("curl-2")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            1,
+        client = self.base_scenario(
+            frang_config="http_field_len 300;",
+            requests=[f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nX-Long: {'1' * 320}\r\n\r\n"],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP field length exceeded for",
-            ),
-            1,
+        self.check_response(
+            client, status_code="403", warning_msg="frang: HTTP field length exceeded for"
         )
-
-        curl.stop()
 
     def test_field_without_reaching_the_limit(self):
         """
         Test 'http_field_len'.
 
-        Set up `http_field_len 300;
+        Set up `http_field_len 300; and make request with header 200 length
 
         """
-        curl = self.get_client("curl-22")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
+        client = self.base_scenario(
+            frang_config="http_field_len 300;",
+            requests=[f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nX-Long: {'1' * 200}\r\n\r\n"],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP field length exceeded for",
-            ),
-            0,
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP field length exceeded for"
         )
 
-        curl.stop()
+    def test_field_without_reaching_the_limit_2(self):
+        """
+        Test 'http_field_len'.
+
+        Set up `http_field_len 300; and make request with header 300 length
+
+        """
+        client = self.base_scenario(
+            frang_config="http_field_len 300;",
+            requests=[f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nX-Long: {'1' * 292}\r\n\r\n"],
+        )
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP field length exceeded for"
+        )
 
     def test_body_len(self):
         """
@@ -294,28 +105,15 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_body_len 10;` and make request with body greater length
 
         """
-        curl = self.get_client("curl-3")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            1,
+        client = self.base_scenario(
+            frang_config="http_body_len 10;",
+            requests=[
+                f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nContent-Length: 20\r\n\r\n{'x' * 20}"
+            ],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP body length exceeded for",
-            ),
-            1,
+        self.check_response(
+            client, status_code="403", warning_msg="frang: HTTP body length exceeded for"
         )
-
-        curl.stop()
 
     def test_body_len_without_reaching_the_limit_zero_len(self):
         """
@@ -324,28 +122,13 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_body_len 10;` and make request with body 0 length
 
         """
-        curl = self.get_client("curl-31")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
+        client = self.base_scenario(
+            frang_config="http_body_len 10;",
+            requests=[f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nContent-Length: 0\r\n\r\n"],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP body length exceeded for",
-            ),
-            0,
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP body length exceeded for"
         )
-
-        curl.stop()
 
     def test_body_len_without_reaching_the_limit(self):
         """
@@ -354,25 +137,12 @@ class FrangLengthTestCase(FrangTestCase):
         Set up `http_body_len 10;` and make request with body shorter length
 
         """
-        curl = self.get_client("curl-32")
-
-        self.start_all_servers()
-        self.start_tempesta()
-
-        curl.start()
-        self.wait_while_busy(curl)
-
-        self.assertEqual(
-            self.klog.warn_count(
-                " Warning: parsed request has been filtered out:",
-            ),
-            0,
+        client = self.base_scenario(
+            frang_config="http_body_len 10;",
+            requests=[
+                f"POST /1234 HTTP/1.1\r\nHost: localhost\r\nContent-Length: 10\r\n\r\n{'x' * 10}"
+            ],
         )
-        self.assertEqual(
-            self.klog.warn_count(
-                "Warning: frang: HTTP body length exceeded for",
-            ),
-            0,
+        self.check_response(
+            client, status_code="200", warning_msg="frang: HTTP body length exceeded for"
         )
-
-        curl.stop()
