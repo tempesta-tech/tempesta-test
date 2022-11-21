fbcc23819067142bd274f8e830b7ae66cdb83423
Roman <rb@tempesta-tech.com>
2022-11-21 11:57:14 +0400

    parametrization of `http_method_override_allowed` tests.

--- t_frang/test_http_method_override_allowed.py
+++ t_frang/test_http_method_override_allowed.py
@@ -1,33 +1,12 @@
 """Tests for Frang directive `http_method_override_allowed`."""
-from framework import tester
-from helpers import dmesg
 
 __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2022 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
-ERROR_MSG = "Frang limits warning is not shown"
-COUNT_WARNINGS_OK = 1
-COUNT_WARNINGS_ZERO = 0
+import time
 
-RESPONSE_CONTENT = """HTTP/1.1 200 OK\r
-Content-Length: 0\r\n
-Connection: keep-alive\r\n\r\n
-"""
-
-TEMPESTA_CONF = """
-cache 0;
-listen 80;
-
-
-frang_limits {
-    http_method_override_allowed true;
-    http_methods post put get;
-}
-
-
-server ${server_ip}:8000;
-"""
+from t_frang.frang_test_case import FrangTestCase
 
 WARN = "frang: restricted HTTP method"
 WARN_ERROR = "frang: restricted overridden HTTP method"
@@ -45,48 +24,6 @@ X-Method-Override: PUT\r
 POST / HTTP/1.1\r
 Host: tempesta-tech.com\r
 X-HTTP-Method: PUT\r
-\r
-"""
-
-REQUEST_UNSAFE_OVERRIDE_X_HTTP_METHOD_OVERRIDE = """
-GET / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-HTTP-Method-Override: POST\r
-\r
-"""
-
-REQUEST_UNSAFE_OVERRIDE_X_METHOD_OVERRIDE = """
-GET / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-Method-Override: POST\r
-\r
-"""
-
-REQUEST_UNSAFE_OVERRIDE_X_HTTP_METHOD = """
-GET / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-HTTP-Method: POST\r
-\r
-"""
-
-NOT_ACCEPTED_REQUEST_X_HTTP_METHOD_OVERRIDE = """
-POST / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-HTTP-Method-Override: OPTIONS\r
-\r
-"""
-
-NOT_ACCEPTED_REQUEST_X_METHOD_OVERRIDE = """
-POST / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-Method-Override: OPTIONS\r
-\r
-"""
-
-NOT_ACCEPTED_REQUEST_X_HTTP_METHOD = """
-POST / HTTP/1.1\r
-Host: tempesta-tech.com\r
-X-HTTP-Method: OPTIONS\r
 \r
 """
 
@@ -120,155 +57,107 @@ X-Method-Override: GET\r
 """
 
 
-class FrangHttpMethodsOverrideTestCase(tester.TempestaTest):
-
-    clients = [
-        {
-            "id": "client",
-            "type": "deproxy",
-            "addr": "${tempesta_ip}",
-            "port": "80",
-        },
-    ]
-
-    backends = [
-        {
-            "id": "0",
-            "type": "deproxy",
-            "port": "8000",
-            "response": "static",
-            "response_content": RESPONSE_CONTENT,
-        },
-    ]
-
-    tempesta = {
-        "config": TEMPESTA_CONF,
-    }
-
-    def setUp(self):
-        """Set up test."""
-        super().setUp()
-        self.klog = dmesg.DmesgFinder(ratelimited=False)
-
-    def start_all(self):
-        """Start all requirements."""
-        self.start_all_servers()
-        self.start_tempesta()
-        self.deproxy_manager.start()
-        srv = self.get_server("0")
-        self.assertTrue(
-            srv.wait_for_connections(timeout=1),
-        )
-
+class FrangHttpMethodsOverrideTestCase(FrangTestCase):
     def test_accepted_request(self):
-        self.start_all()
-
-        deproxy_cl = self.get_client("client")
-        deproxy_cl.start()
-        deproxy_cl.make_requests(
-            ACCEPTED_REQUESTS
-            + REQUEST_FALSE_OVERRIDE
-            + DOUBLE_OVERRIDE
-            + MULTIPLE_OVERRIDE
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=[
+                ACCEPTED_REQUESTS,
+                REQUEST_FALSE_OVERRIDE,
+                DOUBLE_OVERRIDE,
+                MULTIPLE_OVERRIDE,
+            ],
         )
-        deproxy_cl.wait_for_response(1)
-        assert (
-            list(p.status for p in deproxy_cl.responses) == ["200"] * 6
-        ), f"Real status: {list(p.status for p in deproxy_cl.responses)}"
-        self.assertEqual(
-            6,
-            len(deproxy_cl.responses),
-        )
-        self.assertFalse(
-            deproxy_cl.connection_is_closed(),
-        )
+        time.sleep(1)
+        self.check_response(client, status_code="200", warning_msg="frang: ")
 
     def test_not_accepted_request_x_http_method_override(self):
         """
         override methods not allowed by limit http_methods
         for X_HTTP_METHOD_OVERRIDE
         """
-        self._test_base_scenario(
-            request_body=NOT_ACCEPTED_REQUEST_X_HTTP_METHOD_OVERRIDE,
-            expected_warning=WARN_ERROR,
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=[
+                "POST / HTTP/1.1\r\nHost: localhost\r\nX-HTTP-Method-Override: OPTIONS\r\n\r\n"
+            ],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="403", warning_msg=WARN_ERROR)
 
     def test_not_accepted_request_x_method_override(self):
         """
         override methods not allowed by limit http_methods
         for X_METHOD_OVERRIDE
         """
-        self._test_base_scenario(
-            request_body=NOT_ACCEPTED_REQUEST_X_METHOD_OVERRIDE,
-            expected_warning=WARN_ERROR,
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=["POST / HTTP/1.1\r\nHost: localhost\r\nX-Method-Override: OPTIONS\r\n\r\n"],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="403", warning_msg=WARN_ERROR)
 
     def test_not_accepted_request_x_http_method(self):
         """
         override methods not allowed by limit http_methods
         for X_HTTP_METHOD
         """
-        self._test_base_scenario(
-            request_body=NOT_ACCEPTED_REQUEST_X_HTTP_METHOD, expected_warning=WARN_ERROR
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=[
+                "POST / HTTP/1.1\r\nHost: tempesta-tech.com\r\nX-HTTP-Method: OPTIONS\r\n\r\n"
+            ],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="403", warning_msg=WARN_ERROR)
 
     def test_unsafe_override_x_http_method_override(self):
         """
         should not be allowed to be overridden by unsafe methods
         for X-HTTP-Method-Override
         """
-        self._test_base_scenario(
-            request_body=REQUEST_UNSAFE_OVERRIDE_X_HTTP_METHOD_OVERRIDE,
-            expected_warning=WARN_UNSAFE,
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=[
+                "GET / HTTP/1.1\r\nHost: tempesta-tech.com\r\nX-HTTP-Method-Override: POST\r\n\r\n"
+            ],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="400", warning_msg=WARN_UNSAFE)
 
     def test_unsafe_override_x_http_method(self):
         """
         should not be allowed to be overridden by unsafe methods
         for X-HTTP-Method
         """
-        self._test_base_scenario(
-            request_body=REQUEST_UNSAFE_OVERRIDE_X_HTTP_METHOD,
-            expected_warning=WARN_UNSAFE,
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=["GET / HTTP/1.1\r\nHost: tempesta-tech.com\r\nX-HTTP-Method: POST\r\n\r\n"],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="400", warning_msg=WARN_UNSAFE)
 
     def test_unsafe_override_x_method_override(self):
         """
         should not be allowed to be overridden by unsafe methods
         for X-Method-Override
         """
-        self._test_base_scenario(
-            request_body=REQUEST_UNSAFE_OVERRIDE_X_METHOD_OVERRIDE,
-            expected_warning=WARN_UNSAFE,
+        client = self.base_scenario(
+            frang_config="http_method_override_allowed true;\n\thttp_methods post put get;",
+            requests=[
+                "GET / HTTP/1.1\r\nHost: tempesta-tech.com\r\nX-Method-Override: POST\r\n\r\n"
+            ],
         )
+        time.sleep(1)
+        self.check_response(client, status_code="400", warning_msg=WARN_UNSAFE)
 
-    def _test_base_scenario(self, request_body: str, expected_warning: str = WARN):
-        """
-        Test base scenario for process different requests.
-
-        Args:
-            request_body (str): request body
-            expected_warning (str): expected warning in logs
-        """
-        self.start_all()
-
-        deproxy_cl = self.get_client("client")
-        deproxy_cl.start()
-        deproxy_cl.make_requests(
-            request_body,
+    def test_default_http_method_override_allowed(self):
+        """Test default `http_method_override_allowed` value."""
+        client = self.base_scenario(
+            frang_config="http_methods post put get;",
+            requests=[
+                "POST / HTTP/1.1\r\nHost: tempesta-tech.com\r\nX-Method-Override: PUT\r\n\r\n"
+            ],
         )
-        deproxy_cl.wait_for_response()
-
-        self.assertEqual(
-            0,
-            len(deproxy_cl.responses),
-        )
-        self.assertTrue(
-            deproxy_cl.connection_is_closed(),
-        )
-
-        self.assertEqual(
-            self.klog.warn_count(expected_warning),
-            COUNT_WARNINGS_OK,
-            ERROR_MSG,
-        )
+        time.sleep(1)
+        self.check_response(client, status_code="403", warning_msg=WARN_ERROR)
