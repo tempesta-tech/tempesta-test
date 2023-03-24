904a2c10be5bbb4ac600092086dd57c198eab510
Roman <rb@tempesta-tech.com>
2023-03-24 16:15:26 +0400

    updated __init__.py

--- t_sched/__init__.py
+++ t_sched/__init__.py
@@ -1,4 +1,11 @@
 __all__ = [
+    "test_h2_hash_func",
+    "test_h2_hash_stress",
+    "test_h2_per_group_lb",
+    "test_h2_ratio_dynamic",
+    "test_h2_ratio_dynamic_recalc",
+    "test_h2_ratio_static",
+    "test_h2_ratio_weight",
     "test_hash_func",
     "test_hash_stress",
     "test_per_group_lb",
