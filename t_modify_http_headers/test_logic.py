1dffca94d0c10ed8969044db00911c92f076ed44
Roman <rb@tempesta-tech.com>
2023-05-04 17:35:38 +0400

    reworked tests - old version is too hard. Added tests: - adding non-exist header for `..._hdr_add`; - removing one and many headers; - removing special headers; - removing non-exist header; - h2 request tests with dynamic table; Removed  `test_multiple_replacing_same_header`, `test_overwrite_non_singular_header`, `test_overwrite_singular_header`, `test_overwrite_raw_header` tests because now it is low priority logic.

--- t_modify_http_headers/test_logic.py
+++ t_modify_http_headers/test_logic.py
@@ -4,30 +4,128 @@ __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2023 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
-from t_modify_http_headers.utils import AddHeaderBase, H2Config
+from framework import tester
 
 
-class TestReqHeader(AddHeaderBase):
-    directive = "req"
-    cache = False
-    request = (
+def generate_http1_request(optional_headers=[]) -> str:
+    return (
         f"GET / HTTP/1.1\r\n"
-        + "host: localhost\r\n"
-        + "Connection: keep-alive\r\n"
-        + "Accept: */*\r\n"
-        + "Content-Encoding: gzip\r\n"
-        + "x-my-hdr: original text\r\n"
-        + "x-my-hdr: original text\r\n"
+        + "Host: localhost\r\n"
+        + "".join(f"{header[0]}: {header[1]}\r\n" for header in optional_headers)
         + "\r\n"
     )
+
+
+def generate_response(optional_headers=[]) -> str:
+    return (
+        "HTTP/1.1 200 OK\r\n"
+        + "Date: test\r\n"
+        + "Server: debian\r\n"
+        + "".join(f"{header[0]}: {header[1]}\r\n" for header in optional_headers)
+        + "Content-Length: 0\r\n\r\n"
+    )
+
+
+def generate_h2_request(optional_headers=[]) -> list:
+    return [
+        (":authority", "localhost"),
+        (":path", "/"),
+        (":scheme", "https"),
+        (":method", "GET"),
+    ] + optional_headers
+
+
+class TestLogicBase(tester.TempestaTest, base=True):
+    tempesta = {
+        "config": """
+    listen 80;
+    listen 443 proto=h2;
+    
+    server ${server_ip}:8000;
+
+    tls_certificate ${tempesta_workdir}/tempesta.crt;
+    tls_certificate_key ${tempesta_workdir}/tempesta.key;
+    tls_match_any_server_name;
+    """,
+    }
+
+    clients = [
+        {
+            "id": "deproxy-1",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        },
+    ]
+
+    backends = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "port": "8000",
+            "response": "static",
+            "response_content": "",
+        }
+    ]
+
+    cache: bool
+    requests_n: int
+    directive: str
+    h2: bool
+
+    def update_tempesta_config(self, config: str):
+        tempesta = self.get_tempesta()
+        cache = "cache 2;\ncache_fulfill * *;\n" if self.cache else "cache 0;"
+        tempesta.config.defconfig += config + "\n" + cache
+
+    def base_scenario(self, config: str, optional_headers: list, expected_headers: list):
+        self.update_tempesta_config(config=config)
+        self.start_all_services()
+
+        server = self.get_server("deproxy")
+        client = self.get_client("deproxy-1")
+
+        server.set_response(generate_response(optional_headers))
+
+        # send 2 requests for checking cache and dynamic table
+        for _ in range(2 if self.cache else self.requests_n):
+            client.send_request(
+                generate_h2_request(optional_headers)
+                if self.h2
+                else generate_http1_request(optional_headers),
+                "200",
+            )
+
+        self.check_response_and_request(expected_headers, client, server)
+
+        return client, server
+
+    def check_response_and_request(self, expected_headers: list, client, server):
+        for header in expected_headers:
+            if self.directive == "req":
+                self.assertIn(header[1], list(server.last_request.headers.find_all(header[0])))
+                self.assertNotIn(header[1], list(client.last_response.headers.find_all(header[0])))
+            else:
+                self.assertIn(header[1], list(client.last_response.headers.find_all(header[0])))
+                self.assertNotIn(header[1], list(server.last_request.headers.find_all(header[0])))
 
     def test_set_non_exist_header(self):
         """
         Header will be added in request/response if it is missing from base request/response.
         """
-        self.directive = f"{self.directive}_hdr_set"
         self.base_scenario(
-            config=f'{self.directive} non-exist-header "qwe";\n',
+            config=f'{self.directive}_hdr_set non-exist-header "qwe";\n',
+            optional_headers=[],
+            expected_headers=[("non-exist-header", "qwe")],
+        )
+
+    def test_add_non_exist_header(self):
+        """
+        Header will be added in request/response if it is missing from base request/response.
+        """
+        self.base_scenario(
+            config=f'{self.directive}_hdr_add non-exist-header "qwe";\n',
+            optional_headers=[],
             expected_headers=[("non-exist-header", "qwe")],
         )
 
@@ -35,202 +133,143 @@ class TestReqHeader(AddHeaderBase):
         """
         Headers must be removed from base request/response if header is in base request/response.
         """
-        self.directive = f"{self.directive}_hdr_set"
+
         client, server = self.base_scenario(
-            config=f"{self.directive} x-my-hdr;\n",
+            config=f"{self.directive}_hdr_set x-my-hdr;\n",
+            optional_headers=[("x-my-hdr", "original header")],
             expected_headers=[],
         )
 
-        if self.directive == "req_hdr_set":
+        if self.directive == "req":
             self.assertNotIn("x-my-hdr", server.last_request.headers.keys())
         else:
             self.assertNotIn("x-my-hdr", client.last_response.headers.keys())
 
+    def test_delete_many_headers(self):
+        """
+        Headers must be removed from base request/response if header is in base request/response.
+        """
+
+        client, server = self.base_scenario(
+            config=f"{self.directive}_hdr_set x-my-hdr;\n",
+            optional_headers=[("x-my-hdr", "original header"), ("x-my-hdr", "original header")],
+            expected_headers=[],
+        )
+
+        if self.directive == "req":
+            self.assertNotIn("x-my-hdr", server.last_request.headers.keys())
+        else:
+            self.assertNotIn("x-my-hdr", client.last_response.headers.keys())
+
+    def test_delete_special_headers(self):
+        """
+        Headers must be removed from base request/response if header is in base request/response.
+        """
+        client, server = self.base_scenario(
+            config=f"{self.directive}_hdr_set Etag;\n",
+            optional_headers=[("Erag", '"qwe"')],
+            expected_headers=[],
+        )
+
+        if self.directive == "req":
+            self.assertNotIn("etag", server.last_request.headers.keys())
+        else:
+            self.assertNotIn("etag", client.last_response.headers.keys())
+
     def test_delete_non_exist_header(self):
         """Request/response does not modify if header is missing from base request/response."""
-        self.directive = f"{self.directive}_hdr_set"
         self.base_scenario(
-            config=f"{self.directive} non-exist-header;\n",
-            expected_headers=[("x-my-hdr", "original text")],
+            config=f"{self.directive}_hdr_set non-exist-header;\n",
+            optional_headers=[],
+            expected_headers=[],
         )
-
-    def test_overwrite_raw_header(self):
-        """New value for header must be added to old value as list."""
-        self.directive = f"{self.directive}_hdr_add"
-        client, server = self.base_scenario(
-            config=f'{self.directive} x-my-hdr "some text";\n',
-            expected_headers=[("x-my-hdr", "original text, some text")],
-        )
-
-        if self.directive == "req_hdr_add":
-            self.assertNotIn(
-                "original text", list(server.last_request.headers.find_all("x-my-hdr"))
-            )
-        else:
-            self.assertNotIn(
-                "original text",
-                list(client.last_response.headers.find_all("x-my-hdr")),
-            )
-
-    def test_overwrite_singular_header(self):
-        """New value for header must not be added."""
-        self.directive = f"{self.directive}_hdr_add"
-        client, server = self.base_scenario(
-            config=f'{self.directive} host "tempesta";\n',
-            expected_headers=[("host", "localhost")],
-        )
-
-        self.assertNotIn("tempesta", list(server.last_request.headers.find_all("host")))
-
-    def test_overwrite_non_singular_header(self):
-        """New value for header must not be added."""
-        self.directive = f"{self.directive}_hdr_add"
-        client, server = self.base_scenario(
-            config=f'{self.directive} Content-Encoding "br";\n',
-            expected_headers=[("content-encoding", "gzip")],
-        )
-        if self.directive == "req_hdr_add":
-            self.assertNotIn("br", list(server.last_request.headers.find_all("content-encoding")))
-        else:
-            self.assertNotIn("br", list(client.last_response.headers.find_all("content-encoding")))
-
-    def test_add_exist_header(self):
-        """Header will be modified if 'header-value' is in base request/response."""
-        self.directive = f"{self.directive}_hdr_add"
-        client, server = self.base_scenario(
-            config=f'{self.directive} x-my-hdr "original text";\n',
-            expected_headers=[("x-my-hdr", "original text, original text")],
-        )
-        if self.directive == "req_hdr_add":
-            self.assertNotIn(
-                "original text", list(server.last_request.headers.find_all("x-my-hdr"))
-            )
-        else:
-            self.assertNotIn(
-                "original text", list(client.last_response.headers.find_all("x-my-hdr"))
-            )
 
     def test_set_large_header(self):
-        self.directive = f"{self.directive}_hdr_set"
         self.base_scenario(
-            config=f'{self.directive} x-my-hdr "{"12345" * 2000}";\n',
-            expected_headers=[("x-my-hdr", "12345" * 2000)],
+            config=f'{self.directive}_hdr_set x-my-hdr "{"12" * 2000}";\n',
+            optional_headers=[],
+            expected_headers=[("x-my-hdr", "12" * 2000)],
         )
 
-    def test_multiple_appending_same_header(self):
-        """All headers must be added in request/response as list."""
-        self.directive = f"{self.directive}_hdr_add"
-        self.base_scenario(
-            config=(
-                f'{self.directive} x-my-hdr-2 "first text";\n'
-                f'{self.directive} x-my-hdr-2 "second text";\n'
-                f'{self.directive} x-my-hdr-2 "third text";\n'
-            ),
-            expected_headers=[("x-my-hdr-2", "first text, second text, third text")],
-        )
 
-    def test_multiple_replacing_same_header(self):
-        """Only last header must be added in request/response."""
-        self.directive = f"{self.directive}_hdr_set"
-        client, server = self.base_scenario(
-            config=(
-                f'{self.directive} x-my-hdr-2 "first text";\n'
-                f'{self.directive} x-my-hdr-2 "second text";\n'
-                f'{self.directive} x-my-hdr-2 "third text";\n'
-            ),
-            expected_headers=[("x-my-hdr-2", "third text")],
-        )
-
-        if self.directive == "req_hdr_set":
-            self.assertEqual(1, len(list(server.last_request.headers.find_all("x-my-hdr-2"))))
-        else:
-            self.assertEqual(1, len(list(client.last_response.headers.find_all("x-my-hdr-2"))))
-
-
-class TestRespHeader(TestReqHeader):
-    backends = [
+class H2Config:
+    clients = [
         {
-            "id": "deproxy",
-            "type": "deproxy",
-            "port": "8000",
-            "response": "static",
-            "response_content": (
-                "HTTP/1.1 200 OK\r\n"
-                + "Date: test\r\n"
-                + "Server: debian\r\n"
-                + "x-my-hdr: original text\r\n"
-                + "x-my-hdr: original text\r\n"
-                + 'Etag: "qwe"\r\n'
-                + "Content-Encoding: gzip\r\n"
-                + "Content-Length: 0\r\n\r\n"
-            ),
-        }
+            "id": "deproxy-1",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+        },
     ]
-    request = (
-        f"GET / HTTP/1.1\r\n"
-        + "Host: localhost\r\n"
-        + "Connection: keep-alive\r\n"
-        + "Accept: */*\r\n"
-        + "\r\n"
-    )
+
+
+class TestReqHeader(TestLogicBase):
+    directive = "req"
+    cache = False
+    requests_n = 1
+    h2 = False
+
+
+class TestRespHeader(TestLogicBase):
     directive = "resp"
-
-    def test_overwrite_singular_header(self):
-        """New value for header must not be added."""
-        self.directive = f"{self.directive}_hdr_add"
-        client, server = self.base_scenario(
-            config=f'{self.directive} Etag "asd";\n',
-            expected_headers=[("Etag", '"qwe"')],
-        )
-
-        self.assertNotIn('"asd"', list(client.last_response.headers.find_all("Etag")))
-        self.assertNotIn("asd", list(client.last_response.headers.find_all("Etag")))
-        self.assertNotIn('"qwe", "asd"', list(client.last_response.headers.find_all("Etag")))
+    cache = False
+    requests_n = 1
+    h2 = False
 
 
-class TestCachedRespHeader(TestRespHeader):
+class TestCachedRespHeader(TestLogicBase):
+    directive = "resp"
     cache = True
+    requests_n = 2
+    h2 = False
 
 
-class TestReqHeaderH2(H2Config, TestReqHeader):
-    request = [
-        (":authority", "localhost"),
-        (":path", "/"),
-        (":scheme", "https"),
-        (":method", "GET"),
-        ("host", "localhost"),
-        ("x-my-hdr", "original text"),
-        ("x-my-hdr", "original text"),
-        ("content-encoding", "gzip"),
-    ]
+class TestReqHeaderH2(H2Config, TestLogicBase):
+    directive = "req"
+    cache = False
+    requests_n = 2
+    h2 = True
 
 
-class TestRespHeaderH2(H2Config, TestRespHeader):
+class TestRespHeaderH2(H2Config, TestLogicBase):
+    directive = "resp"
+    cache = False
+    requests_n = 1
+    h2 = True
+
     def test_add_header_from_static_table(self):
         """Tempesta must add header from static table as byte."""
-        self.directive = f"{self.directive}_hdr_add"
         client, server = self.base_scenario(
-            config=f'{self.directive} cache-control "no-cache";\n',
+            config=f'{self.directive}_hdr_set cache-control "no-cache";\n',
+            optional_headers=[],
             expected_headers=[("cache-control", "no-cache")],
         )
 
-        self.assertIn(b"X/08no-cache", client.response_buffer)
+        self.assertIn(b"\x08no-cache", client.response_buffer)
 
     def test_add_header_from_dynamic_table(self):
         """Tempesta must add header from dynamic table for second response."""
-        self.directive = f"{self.directive}_hdr_add"
-        self.update_tempesta_config(config=f'{self.directive} x-my-hdr-3 "text";\n')
+        self.update_tempesta_config(config=f'{self.directive}_hdr_set x-my-hdr "text";\n')
         self.start_all_services()
 
+        optional_headers = [("x-my-hdr", "text")]
+        request = generate_h2_request(optional_headers)
+
         client = self.get_client("deproxy-1")
+        server = self.get_server("deproxy")
+        server.set_response(generate_response(optional_headers))
 
-        client.send_request(self.request, "200")
-        self.assertIn(b"@\nx-my-hdr-3\x04text", client.response_buffer)
+        client.send_request(request, "200")
+        self.assertIn(b"\x08x-my-hdr\x04text", client.response_buffer)
 
-        client.send_request(self.request, "200")
-        self.assertNotIn(b"@\nx-my-hdr-3\x04text", client.response_buffer)
+        client.send_request(request, "200")
+        self.assertNotIn(b"\nx-my-hdr\x04text", client.response_buffer)
         self.assertIn(b"\xbe", client.response_buffer)
 
 
-class TestCachedRespHeaderH2(TestRespHeaderH2):
+class TestCachedRespHeaderH2(H2Config, TestLogicBase):
+    directive = "resp"
     cache = True
+    requests_n = 2
+    h2 = True
