607276888cbb362edef84768f4ea4818ffb95b13
Constantine <kt@tempesta-tech.com>
2023-08-28 16:18:26 +0300

    Test MatchHost has been rewriten
    The previous approach that used chains replaced with no chains, because chains
    is a semi-depricated part that belongs to old framework. Full request
    comparision dropped, because request can be changed by tempesta during
    forwarding thus full comparision is not a valid approach. Now just assign id
    for each request and check it on receive, it's enough for current test.
    The goal of test is verify correctnes of HTTPTables rules and routing
    without checking correctnes of forwarded message.

--- forwarding/test_match_host_forwarded.py
+++ forwarding/test_match_host_forwarded.py
@@ -3,7 +3,6 @@ Tests for verifying correctness of matching
 all host headers (URI, Host, Forwarded).
 """
 from framework import tester
-from helpers import chains
 
 __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2022 Tempesta Technologies, Inc."
@@ -11,7 +10,6 @@ __license__ = "GPL2"
 
 
 class TestMatchHost(tester.TempestaTest):
-
     backends = [
         {
             "id": 0,
@@ -73,21 +71,21 @@ class TestMatchHost(tester.TempestaTest):
     requests_opt = [
         {
             "uri": "http://testshop.com",  # <--must be matched by "host eq"
-            "headers": [("Host", "testwiki.com"), ("Forwarded", "host=testapp.com")],
+            "headers": ["Host: testwiki.com", "Forwarded: host=testapp.com"],
             "block": False,
             "sid": 0,
         },
         {
             "uri": "http://testshop.com",  # <--must be matched by "host eq"
-            "headers": [("Host", "badhost.com"), ("Forwarded", "host=badhost.com")],
+            "headers": ["Host: badhost.com", "Forwarded: host=badhost.com"],
             "block": False,
             "sid": 0,
         },
         {
             "uri": "http://testshop.com",
             "headers": [
-                ("Host", "testapp.com"),  # <--must be matched by "hdr host == testapp.com"
-                ("Forwarded", "host=testwiki.com"),
+                "Host: testapp.com",  # <--must be matched by "hdr host == testapp.com"
+                "Forwarded: host=testwiki.com",
             ],
             "block": False,
             "sid": 2,
@@ -95,8 +93,8 @@ class TestMatchHost(tester.TempestaTest):
         {
             "uri": "http://badhost.com",
             "headers": [
-                ("Host", "badhost.com"),
-                ("Forwarded", "host=testshop.com"),  # <--must be matched by "hdr forwarded"
+                "Host: badhost.com",
+                "Forwarded: host=testshop.com",  # <--must be matched by "hdr forwarded"
             ],
             "block": False,
             "sid": 0,
@@ -104,9 +102,9 @@ class TestMatchHost(tester.TempestaTest):
         {
             "uri": "http://badhost.com",
             "headers": [
-                ("Host", "badhost.com"),
-                ("Forwarded", "host=unkhost.com"),
-                ("Forwarded", "host=testshop.com"),  # <--must be matched by "hdr forwarded"
+                "Host: badhost.com",
+                "Forwarded: host=unkhost.com",
+                "Forwarded: host=testshop.com",  # <--must be matched by "hdr forwarded"
             ],
             "block": False,
             "sid": 0,
@@ -114,8 +112,8 @@ class TestMatchHost(tester.TempestaTest):
         {
             "uri": "/foo",
             "headers": [
-                ("Host", "testwiki.com"),  # <--must be matched by "host eq"
-                ("Forwarded", "host=forwarded.host.ignored"),
+                "Host: testwiki.com",  # <--must be matched by "host eq"
+                "Forwarded: host=forwarded.host.ignored",
             ],
             "block": False,
             "sid": 1,
@@ -123,8 +121,8 @@ class TestMatchHost(tester.TempestaTest):
         {
             "uri": "/foo",
             "headers": [
-                ("Host", "TesTaPp.cOm"),  # <--must be matched by "host eq"
-                ("Forwarded", "HoSt=forwarded.host.ignored"),
+                "Host: TesTaPp.cOm",  # <--must be matched by "host eq"
+                "Forwarded: HoSt=forwarded.host.ignored",
             ],
             "block": False,
             "sid": 2,
@@ -132,52 +130,31 @@ class TestMatchHost(tester.TempestaTest):
         {
             "uri": "/foo",
             "headers": [
-                ("Host", "[fd80::1cb2:ad12:ca16:98ef]:8080"), # <--must be matched by "host eq"
-                (
-                    "Forwarded",
-                    'host="forwarded.host.ignored"',
-                ),  
+                "Host: [fd80::1cb2:ad12:ca16:98ef]:8080",  # <--must be matched by "host eq"
+                'Forwarded: host="forwarded.host.ignored"',
             ],
             "block": False,
             "sid": 2,
         },
         {
             "uri": "/foo",
-            "headers": [("Host", "badhost.com"), ("Forwarded", "host=forwarded.host.ignored")],
+            "headers": ["Host: badhost.com", "Forwarded: host=forwarded.host.ignored"],
             "block": True,
             "sid": 0,
         },
-        {"uri": "/foo", "headers": [("Host", "unkhost.com")], "block": True, "sid": 0},
+        {"uri": "/foo", "headers": ["Host: unkhost.com"], "block": True, "sid": 0},
     ]
 
     blocked_response_status = "403"
-    chains = []
 
     def add_client(self, cid):
         client = {"id": cid, "type": "deproxy", "addr": "${tempesta_ip}", "port": "80"}
         self.clients.append(client)
 
-    def init_chain(self, req_opt):
-        ch = chains.base(uri=req_opt["uri"])
-        if req_opt["block"]:
-            for header, value in req_opt["headers"]:
-                ch.request.headers.delete_all(header)
-                ch.request.headers.add(header, value)
-                ch.request.update()
-                ch.fwd_request = None
-        else:
-            for request in [ch.request, ch.fwd_request]:
-                for header, value in req_opt["headers"]:
-                    request.headers.delete_all(header)
-                    request.headers.add(header, value)
-                    request.update()
-        self.chains.append(ch)
-
     def setUp(self):
-        del self.chains[:]
+        # Create a client for each request
         count = len(self.requests_opt)
         for i in range(count):
-            self.init_chain(self.requests_opt[i])
             self.add_client(i)
         tester.TempestaTest.setUp(self)
 
@@ -188,29 +165,32 @@ class TestMatchHost(tester.TempestaTest):
         self.deproxy_manager.start()
         self.assertTrue(self.wait_all_connections())
 
-    def process(self, client, server, chain):
-        client.make_request(chain.request.msg)
+    def process(self, client, server, opt, req_id):
+        uri = opt["uri"]
+        first_line = "GET " + uri + " HTTP/1.1\r\n"
+        base_headers = "\r\n".join(opt["headers"]) + "\r\n"
+        req_id_hdr = "x-req-id: " + str(req_id) + "\r\n"
+        request = first_line + base_headers + req_id_hdr + "\r\n"
+
+        client.make_request(request)
         client.wait_for_response()
 
-        if chain.fwd_request:
-            chain.fwd_request.set_expected()
-            self.assertEqual(server.last_request, chain.fwd_request)
+        if not opt["block"]:
+            last_req_id = int(server.last_request.headers.get("x-req-id"))
+            self.assertEqual(last_req_id, req_id)
         else:
             last_response_status = client.last_response.status
             self.assertEqual(self.blocked_response_status, last_response_status)
 
-    def test_chains(self):
+    def test(self):
         """
         Send requests with different hosts
         and check correctness of forwarding
-        by compare last request on client and
-        server.
+        comparing id of the last request on
+        client and server.
         """
         self.start_all()
-        count = len(self.chains)
-        for i in range(count):
-            sid = self.requests_opt[i]["sid"]
-            self.process(self.get_client(i), self.get_server(sid), self.chains[i])
 
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+        for req_id, opt in enumerate(self.requests_opt):
+            sid = opt["sid"]
+            self.process(self.get_client(req_id), self.get_server(sid), opt, req_id)
