fdef97fdeec8fb7309257f681f92d54f81d6ccc7
Roman <rb@tempesta-tech.com>
2023-10-26 13:27:14 +0400

    added parameterization via decorators. Function example: ``` from framework.parametrize import param, parameterize
    @parameterize.expand([
        param(name="first", param1=1, param2=2),
        param("second", param1=2, param2=1),
        param("second", 1, 1),
        param("second", 1, param2=3),
    ])
    def test(self, name, param1, param2)
       ...
    ```
    
    Class example:
    ```
    from framework.parametrize import parameterize_class
    
    @parameterize_class([
        {"name": "Http", "clients": [DEPROXY_CLIENT]},
        {"name": "H2", "clients": [DEPROXY_CLIENT_H2]},
    ])
    class TestResponse(tester.TempestaTest):
       ...
    ```

---
+++ framework/parameterize.py
@@ -0,0 +1,53 @@
+__author__ = "Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2023 Tempesta Technologies, Inc."
+__license__ = "GPL2"
+
+
+import parameterized as p
+from parameterized import param
+
+
+def get_func_name(func, param_num, params):
+    suffix = params.kwargs.get("name", None)
+
+    if suffix is None:
+        if params.args:
+            suffix = params.args[0]
+        else:
+            raise AttributeError(
+                "Please use string or integer type as first function parameter "
+                "or add the 'name' argument."
+            )
+
+    return f"{func.__name__}_{p.parameterized.to_safe_name(f'{suffix}')}"
+
+
+def get_class_name(cls, num, params_dict: dict):
+    if params_dict.get("name", None) is not None:
+        suffix = p.parameterized.to_safe_name(params_dict["name"])
+    else:
+        raise AttributeError("Please add the 'name' variable to the class parameters.")
+
+    return f"{cls.__name__}{p.parameterized.to_safe_name(suffix)}"
+
+
+def parameterize_class(
+    attrs, input_values=None, class_name_func=get_class_name, classname_func=None
+):
+    """Default wrapper for parametrizing a class from `parameterized` library."""
+    return p.parameterized_class(attrs, input_values, class_name_func, classname_func)
+
+
+class parameterize(p.parameterized):
+    @classmethod
+    def expand(
+        cls,
+        input_,
+        name_func=get_func_name,
+        doc_func=None,
+        skip_on_empty=False,
+        namespace=None,
+        **legacy,
+    ):
+        """Default wrapper for parametrizing a function from `parameterized` library."""
+        return super().expand(input_, name_func, doc_func, skip_on_empty, namespace, **legacy)
