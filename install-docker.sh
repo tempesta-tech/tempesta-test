c3d2a3218e8dd90cba7159c3311e27b51abecc6e
Mikhail Babitski <mb@tempesta-tech.com>
2023-11-27 19:44:41 +0400

    Add useradd for docker group to install script

--- install-docker.sh
+++ install-docker.sh
@@ -13,9 +13,14 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add self to docker group
+sudo usermod -aG docker $USER
+echo "DONE!"
+echo "Reboot / relogin if not root."
