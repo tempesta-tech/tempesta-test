3f9eaa29f5fffde57bfa3fc8e231b70a80dc425b
Vitaly Skopets <vs@tempesta-tech.com>
2024-01-06 01:48:12 +0400

    Rewrite test stress health monitor

--- reconf/test_stress_health_monitor.py
+++ reconf/test_stress_health_monitor.py
@@ -1,71 +1,262 @@
 """
-Live reconfiguration stress test for custom server group
+On the fly reconfiguration test for server group
 with health monitor.
 """
-
-from helpers import tempesta
-
-from . import reconf_stress
 
 __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2018 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+from framework import tester
 
-class HealthMonitorCustomSg(reconf_stress.LiveReconfStress):
+TEMPESTA_CONFIG = """
 
-    sg_name = "custom"
-    hmonitor = "monitor1"
-    defconfig = (
-        "server_failover_http 404 500 30;\n"
-        "health_check monitor1 {\n"
-        '  request "GET / HTTP/1.1\r\nHost: example.com\r\n\r\n";\n'
-        '  request_url "/page.html";\n'
-        "  resp_code 200;\n"
-        "  timeout 15;\n"
-        "}\n"
-        "http_chain {\n"
-        "  -> custom;\n"
-        "}\n"
-        "\n"
-    )
+listen 80 proto=http;
 
-    def add_sg(self, config, sg_name, servers):
-        sg = tempesta.ServerGroup(sg_name)
-        for s in servers:
-            sg.add_server(s.ip, s.config.port, s.conns_n)
-        sg.options = " health %s;" % self.hmonitor
-        config.add_sg(sg)
+server_failover_http 404 50 5;
+cache 0;
 
-    def test_hm_add_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_add)
+srv_group srv_grp1 {
+    server ${server_ip}:8080;
+}
 
-    def test_hm_del_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_del)
+vhost srv_grp1{
+    proxy_pass srv_grp1;
+}
 
-    def test_hm_del_add_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_del_add)
+http_chain {
+    -> srv_grp1;
+}
+%s
+"""
+
+TEMPESTA_CONFIG_ADD_HM = """
+
+listen 80 proto=http;
+
+server_failover_http 404 50 5;
+cache 0;
+
+health_check monitor1 {
+    request "GET / HTTP/1.1\r\n\r\n";
+    request_url	"/";
+    resp_code	200;
+    resp_crc32	auto;
+    timeout		1;
+}
+
+srv_group srv_grp1 {
+    server ${server_ip}:8080;
+    server ${server_ip}:8081;
+
+    health monitor1;
+}
+
+vhost srv_grp1{
+    proxy_pass srv_grp1;
+}
+
+http_chain {
+    -> srv_grp1;
+}
+%s
+"""
+
+TEMPESTA_CONFIG_ADD_SRV_GROUP = """
+
+listen 80 proto=http;
+
+server_failover_http 404 50 5;
+cache 0;
+
+health_check monitor1 {
+    request "GET / HTTP/1.1\r\n\r\n";
+    request_url	"/";
+    resp_code	200;
+    resp_crc32	auto;
+    timeout		1;
+}
 
 
-class HealthMonitorChangedAutoCustomSg(HealthMonitorCustomSg):
+srv_group srv_grp1 {
+    server ${server_ip}:8080;
+    server ${server_ip}:8081;
 
-    hmonitor_new = "auto"
+    health monitor1;
+}
 
-    def set_hmonitor(self, hm):
-        for sg in self.tempesta.config.server_groups:
-            sg.options = " health %s;" % hm
+srv_group srv_grp2 {
+    server ${server_ip}:8082;
+    server ${server_ip}:8083;
 
-    def configure_srvs_add(self):
-        reconf_stress.LiveReconfStress.configure_srvs_add(self)
-        self.set_hmonitor(self.hmonitor_new)
+    health monitor1;
+}
 
-    def configure_srvs_del(self):
-        reconf_stress.LiveReconfStress.configure_srvs_del(self)
-        self.set_hmonitor(self.hmonitor_new)
+srv_group srv_grp3 {
+    server ${server_ip}:8084;
+    server ${server_ip}:8085;
 
-    def configure_srvs_del_add(self):
-        reconf_stress.LiveReconfStress.configure_srvs_del_add(self)
-        self.set_hmonitor(self.hmonitor_new)
+    health monitor1;
+}
+
+vhost srv_grp1{
+    proxy_pass srv_grp1;
+}
+
+vhost srv_grp2{
+    proxy_pass srv_grp2;
+}
+
+vhost srv_grp3{
+    proxy_pass srv_grp3;
+}
+
+http_chain {
+    uri == "/server1" -> srv_grp1;
+    uri == "/server2" -> srv_grp2;
+    uri == "/server3" -> srv_grp3;
+}
+%s
+"""
+
+TEMPESTA_CONFIG_DEL_SRV_GROUP = """
+
+listen 80 proto=http;
+
+server_failover_http 404 50 5;
+cache 0;
+
+health_check monitor1 {
+    request "GET / HTTP/1.1\r\n\r\n";
+    request_url	"/";
+    resp_code	200;
+    resp_crc32	auto;
+    timeout		1;
+}
+
+srv_group srv_grp1 {
+    server ${server_ip}:8080;
+    server ${server_ip}:8081;
+
+    health monitor1;
+}
+
+vhost srv_grp1{
+    proxy_pass srv_grp1;
+}
+
+http_chain {
+    -> srv_grp1;
+}
+%s
+"""
+
+SRV_GRP1 = [
+    {
+        "id": f"server{num}",
+        "type": "deproxy",
+        "port": f"800{num}",
+        "response": "static",
+        "response_content": (
+            "HTTP/1.1 200 OK\r\n"
+            "From: /server1\r\n"
+            "Server: debian\r\n"
+            "Content-length: 9\r\n"
+            "\r\n"
+            "test-data"
+        ),
+    }
+    for num in range(2)
+]
+
+SRV_GRP2 = [
+    {
+        "id": f"server{num}",
+        "type": "deproxy",
+        "port": f"800{num}",
+        "response": "static",
+        "response_content": (
+            "HTTP/1.1 200 OK\r\n"
+            "From: /server2\r\n"
+            "Server: debian\r\n"
+            "Content-length: 9\r\n"
+            "\r\n"
+            "test-data"
+        ),
+    }
+    for num in range(2, 4)
+]
+
+SRV_GRP3 = [
+    {
+        "id": f"server{num}",
+        "type": "deproxy",
+        "port": f"800{num}",
+        "response": "static",
+        "response_content": (
+            "HTTP/1.1 200 OK\r\n"
+            "From: /server3\r\n"
+            "Server: debian\r\n"
+            "Content-length: 9\r\n"
+            "\r\n"
+            "test-data"
+        ),
+    }
+    for num in range(4, 6)
+]
+
+
+class TestReconfHealthMonitor(tester.TempestaTest):
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        },
+    ]
+
+    backends = SRV_GRP1 + SRV_GRP2 + SRV_GRP3
+
+    tempesta = {
+        "config": TEMPESTA_CONFIG,
+    }
+
+    def test_reconf_add_hm(self):
+        self.start_all_servers()
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG)
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_ADD_HM)
+        tempesta.reload()
+
+    def test_reconf_add_srv_group(self):
+        self.start_all_servers()
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_ADD_HM)
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_ADD_SRV_GROUP)
+        tempesta.reload()
+
+    def test_reconf_del_srv_group(self):
+        self.start_all_servers()
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_ADD_SRV_GROUP)
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_DEL_SRV_GROUP)
+        tempesta.reload()
+
+    def test_reconf_del_hm(self):
+        self.start_all_servers()
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG_ADD_HM)
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(TEMPESTA_CONFIG)
+        tempesta.reload()
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
