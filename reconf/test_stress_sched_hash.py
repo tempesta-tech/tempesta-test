a3518b22abef1aa472e4b8ecc824c182be50be25
Vitaly Skopets <vs@tempesta-tech.com>
2024-01-08 17:54:40 +0400

    Rewrite test stress shed hash

--- reconf/test_stress_sched_hash.py
+++ reconf/test_stress_sched_hash.py
@@ -1,61 +1,116 @@
 """
-Live reconfiguration stress test for hash scheduler.
+On the fly reconfiguration test for hash scheduler.
 """
 
-from helpers import tempesta
-
-from . import reconf_stress
-
 __author__ = "Tempesta Technologies, Inc."
-__copyright__ = "Copyright (C) 2017 Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2024 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+from framework import tester
 
-class SchedHashImplDefaultSg(reconf_stress.LiveReconfStress):
 
-    sg_name = "default"
+class TestSchedHashReconf(tester.TempestaTest):
+    backends_count = 5
 
-    def set_hash_sched(self):
-        for sg in self.tempesta.config.server_groups:
-            sg.sched = "hash"
+    tempesta_orig = {
+        "config": """
+        listen 80;
+        listen 443 proto=h2;
+    
+        tls_certificate ${tempesta_workdir}/tempesta.crt;
+        tls_certificate_key ${tempesta_workdir}/tempesta.key;
+        tls_match_any_server_name;
+    
+        sched hash;
+        cache 0;
+    
+        server ${server_ip}:8000;
+        server ${server_ip}:8001;
+        """
+    }
 
-    def configure_srvs_start(self):
-        reconf_stress.LiveReconfStress.configure_srvs_start(self)
-        self.set_hash_sched()
+    tempesta_add_srv = {
+        "config": """
+        listen 80;
+        listen 443 proto=h2;
 
-    def configure_srvs_add(self):
-        reconf_stress.LiveReconfStress.configure_srvs_add(self)
-        self.set_hash_sched()
+        tls_certificate ${tempesta_workdir}/tempesta.crt;
+        tls_certificate_key ${tempesta_workdir}/tempesta.key;
+        tls_match_any_server_name;
 
-    def configure_srvs_del(self):
-        reconf_stress.LiveReconfStress.configure_srvs_del(self)
-        self.set_hash_sched()
+        sched hash;
+        cache 0;
 
-    def configure_srvs_del_add(self):
-        reconf_stress.LiveReconfStress.configure_srvs_del_add(self)
-        self.set_hash_sched()
+        server ${server_ip}:8000;
+        server ${server_ip}:8001;
+        server ${server_ip}:8002;
+        server ${server_ip}:8003;
+        server ${server_ip}:8004;
+        """
+    }
+
+    tempesta_replace_srv = {
+        "config": """
+        listen 80;
+        listen 443 proto=h2;
+
+        tls_certificate ${tempesta_workdir}/tempesta.crt;
+        tls_certificate_key ${tempesta_workdir}/tempesta.key;
+        tls_match_any_server_name;
+
+        sched hash;
+        cache 0;
+
+        server ${server_ip}:8003;
+        server ${server_ip}:8004;
+        """
+    }
+
+    backends = [
+        {
+            "id": f"deproxy-{step}",
+            "type": "deproxy",
+            "port": f"800{step}",
+            "response": "static",
+            "response_content": (
+                "HTTP/1.1 200 OK\r\nContent-Length: 0\r\nServer: deproxy\r\n\r\n",
+            ),
+        }
+        for step in range(backends_count)
+    ]
+
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        }
+    ]
 
     def test_hash_add_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_add)
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(self.tempesta_orig["config"])
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(self.tempesta_add_srv["config"])
+        tempesta.reload()
 
     def test_hash_del_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_del)
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(self.tempesta_add_srv["config"])
+        self.start_tempesta()
+
+        tempesta.config.set_defconfig(self.tempesta_orig["config"])
+        tempesta.reload()
 
     def test_hash_del_add_srvs(self):
-        self.stress_reconfig_generic(self.configure_srvs_start, self.configure_srvs_del_add)
+        tempesta = self.get_tempesta()
+        tempesta.config.set_defconfig(self.tempesta_orig["config"])
+        self.start_tempesta()
 
-
-class SchedHashExplDefaultSg(SchedHashImplDefaultSg):
-    def set_hash_sched(self):
-        for sg in self.tempesta.config.server_groups:
-            sg.sched = "hash"
-            sg.implicit = False
-
-
-class SchedHashCustomSg(SchedHashImplDefaultSg):
-
-    sg_name = "custom"
-    defconfig = "http_chain {\n" "  -> custom;\n" "}\n" "\n"
+        tempesta.config.set_defconfig(self.tempesta_replace_srv["config"])
+        tempesta.reload()
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
