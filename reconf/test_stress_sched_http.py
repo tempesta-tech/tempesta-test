1b967cb15d14a94c65dbe109c0ec2c813a78167e
Vitaly Skopets <vs@tempesta-tech.com>
2024-01-16 16:50:30 +0400

    Rewrite test stress sched http

--- reconf/test_stress_sched_http.py
+++ reconf/test_stress_sched_http.py
@@ -1,35 +1,223 @@
 """
-Live reconfiguration stress test for http scheduler.
+On the fly reconfiguration stress test for http scheduler.
 """
 
-from helpers import tempesta
-
-from . import reconf_stress
-
 __author__ = "Tempesta Technologies, Inc."
-__copyright__ = "Copyright (C) 2017 Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2024 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+from framework import tester
+from framework.external_client import ExternalTester
+from helpers import tf_cfg
+from helpers.control import Tempesta
 
-class SchedHttp(reconf_stress.LiveReconfStress):
+# Number of open connections
+CONCURRENT_CONNECTIONS = int(tf_cfg.cfg.get("General", "concurrent_connections"))
+# Number of threads to use for wrk and h2load tests
+THREADS = int(tf_cfg.cfg.get("General", "stress_threads"))
 
-    orig_sg = "origin"
-    alt_sg = "alternate"
+# Number of requests to make
+REQUESTS_COUNT = int(tf_cfg.cfg.get("General", "stress_requests_count"))
+# Time to wait for single request completion
+DURATION = int(tf_cfg.cfg.get("General", "duration"))
 
-    def configure_http_sched(self, active_group):
-        defconfig = "http_chain {\n" "  -> %s;\n" "}\n" "\n" % active_group
-        config = self.make_config(self.orig_sg, self.const_srvs, defconfig)
-        self.add_sg(config, self.alt_sg, self.add_srvs)
-        self.tempesta.config = config
+HTTP_RULES_START = 'uri == "/alternate" -> block;'
+HTTP_RULES_AFTER_RELOAD = 'uri == "/alternate" -> alternate;'
+STATUS_OK = "200"
+STATUS_FORBIDDEN = "403"
+ERR_MSG = "Error for curl"
+VHOSTS = ("origin", "alternate")
 
-    def configure_start(self):
-        self.configure_http_sched(self.orig_sg)
+TEMPESTA_CONFIG = """
+listen 443 proto=h2;
 
-    def configure_reconfig(self):
-        self.configure_http_sched(self.alt_sg)
+srv_group origin {
+    server ${server_ip}:8080;
+}
 
-    def test_hash_add_srvs(self):
-        self.stress_reconfig_generic(self.configure_start, self.configure_reconfig)
+srv_group alternate {
+    server ${server_ip}:8081;
+}
+
+vhost origin{
+    proxy_pass origin;
+}
+
+vhost alternate{
+    proxy_pass alternate;
+}
+
+tls_match_any_server_name;
+tls_certificate ${tempesta_workdir}/tempesta.crt;
+tls_certificate_key ${tempesta_workdir}/tempesta.key;
+
+cache 0;
+block_action attack reply;
+
+http_chain {
+    uri == "/origin" -> origin;
+    uri == "/alternate" -> block;
+}
+"""
+
+
+class TestSchedHttpReconfStress(tester.TempestaTest):
+    """
+    This class tests on-the-fly reconfig of Tempesta for the http scheduler.
+    """
+
+    backends = [
+        {
+            "id": f"srv-{vhost}",
+            "type": "deproxy",
+            "port": f"808{count}",
+            "response": "static",
+            "response_content": (
+                "HTTP/1.1 200 OK\r\n"
+                f"From: /{vhost}\r\n"
+                "Server: debian\r\n"
+                "Content-length: 0\r\n"
+                "\r\n"
+            ),
+        }
+        for count, vhost in enumerate(VHOSTS)
+    ]
+
+    clients = [
+        {
+            "id": "curl-%s-h2" % vhost,
+            "type": "external",
+            "binary": "curl",
+            "ssl": True,
+            "cmd_args": "-Ikf --http2 https://${tempesta_ip}:443/%s" % vhost,
+        }
+        for vhost in VHOSTS
+    ]
+
+    clients.append(
+        {
+            "id": "h2load",
+            "type": "external",
+            "binary": "h2load",
+            "ssl": True,
+            "cmd_args": (
+                " https://${tempesta_ip}/origin"
+                f" --clients {CONCURRENT_CONNECTIONS}"
+                f" --threads {THREADS}"
+                f" --max-concurrent-streams {REQUESTS_COUNT}"
+                f" --duration {DURATION}"
+            ),
+        },
+    )
+
+    tempesta = {"config": TEMPESTA_CONFIG}
+
+    def test_reconfig_on_the_fly_for_sched_http(self) -> None:
+        """Test Tempesta for change config on the fly."""
+        # launch all services except clients and getting Tempesta instance
+        self.start_all_services(client=False)
+        tempesta: Tempesta = self.get_tempesta()
+
+        # start config Tempesta check (before reload)
+        self._check_start_config(tempesta)
+
+        # launch H2Load
+        client_h2: ExternalTester = self.get_client("h2load")
+        client_h2.start()
+        self.wait_while_busy(client_h2)
+
+        # sending curl requests before reconfig Tempesta
+        response = self.make_curl_request("curl-origin-h2")
+        self.assertIn(STATUS_OK, response, msg=ERR_MSG)
+
+        response = self.make_curl_request("curl-alternate-h2")
+        self.assertIn(STATUS_FORBIDDEN, response, msg=ERR_MSG)
+
+        # config change and reload Tempesta
+        tempesta.config.defconfig = tempesta.config.defconfig.replace(
+            HTTP_RULES_START,
+            HTTP_RULES_AFTER_RELOAD,
+        )
+        tempesta.reload()
+
+        # check logs Tempesta after reload
+        self._check_tfw_log()
+
+        # check config Tempesta after reload
+        self._check_config_after_reload(tempesta)
+
+        # sending curl requests after reconfig Tempesta
+        for client in self.clients[:2]:
+            response = self.make_curl_request(client["id"])
+            self.assertIn(STATUS_OK, response, msg=ERR_MSG)
+
+        # H2Load stop
+        client_h2.stop()
+
+        self.assertEqual(client_h2.returncode, 0)
+        self.assertNotIn(" 0 2xx, ", client_h2.response_msg)
+
+    def make_curl_request(self, curl_client_id: str) -> str:
+        """
+        Make `curl` request.
+
+        Args:
+            curl_client_id (str): curl client id to make request for
+
+        Returns:
+            str: server response to the request as string
+        """
+        client: ExternalTester = self.get_client(curl_client_id)
+        client.start()
+        self.wait_while_busy(client)
+        self.assertEqual(
+            0,
+            client.returncode,
+            msg=(f"Curl return code is not 0. Received - {client.returncode}."),
+        )
+        client.stop()
+        return client.response_msg
+
+    def _check_tfw_log(self) -> None:
+        """Checking the Tempesta log."""
+        self.oops.update()
+        self.assertFalse(len(self.oops.log_findall("ERROR")))
+        self.assertIn(
+            b"[tempesta fw] Live reconfiguration of Tempesta.",
+            self.oops.log,
+        )
+
+    def _check_start_config(self, tempesta: Tempesta) -> None:
+        """
+        Checking the Tempesta start configuration.
+
+        Args:
+            tempesta: object of working Tempesta
+
+        """
+        self.assertIn(
+            HTTP_RULES_START,
+            tempesta.config.get_config(),
+        )
+
+        self.assertNotIn(
+            HTTP_RULES_AFTER_RELOAD,
+            tempesta.config.get_config(),
+        )
+
+    def _check_config_after_reload(self, tempesta: Tempesta) -> None:
+        """
+        Checking the Tempesta configuration after reload.
+
+        Args:
+            tempesta: object of working Tempesta
+
+        """
+        for vhost in VHOSTS:
+            self.assertIn(
+                f'uri == "/{vhost}" -> {vhost};',
+                tempesta.config.get_config(),
+            )
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
