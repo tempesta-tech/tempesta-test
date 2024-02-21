424177d617214311db041dccae5ce6da181e49a0
Vitaly Skopets <vs@tempesta-tech.com>
2024-02-21 21:49:46 +0400

    Rewrite server connections failovering test

--- regression/__init__.py
+++ regression/__init__.py
@@ -1,8 +1,8 @@
 __all__ = [
+    "test_reboot_under_load",
     "test_srv_failovering",
     "test_stress_failovering",
     "test_stress_pipeline",
-    "test_reboot_under_load",
 ]
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
