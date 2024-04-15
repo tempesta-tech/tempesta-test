e5c8519cca86dc8b56798b540818a8bcb63830c0
EvgeniiMekhanik <em@tempesta-tech.com>
2024-04-15 15:23:34 +0300

    Add tests to check cookie options and js_challenge.
    Cookie options and js_challenge directives in our
    config are only valid when cookie is present.
    If Path and Max-Age or Expires are not set in
    cookie eptions they should be explicitly set
    by Tempesta FW.

--- sessions/test_cookies.py
+++ sessions/test_cookies.py
@@ -6,8 +6,9 @@ import re
 import time
 
 from framework import tester
-from framework.parameterize import param, parameterize
-from helpers import tf_cfg
+from framework.parameterize import param, parameterize, parameterize_class
+from helpers import dmesg, remote, tf_cfg
+from helpers.remote import CmdError
 
 __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2019-2024 Tempesta Technologies, Inc."
@@ -542,3 +543,245 @@ class CookieLifetime(CookiesNotEnabled):
         )
         response = self.client_send_req(client, req)
         self.assertEqual(response.status, "302", "Unexpected redirect status code")
+
+
+GLOBAL_TEMPLATE = {
+    "config": """
+sticky {
+    %s
+}
+"""
+}
+
+VHOST_TEMPLATE = {
+    "config": """
+srv_group default {
+    server ${server_ip}:8000;
+}
+
+vhost example.com {
+    sticky {
+        %s
+    }
+
+    proxy_pass default;
+}
+"""
+}
+
+
+@parameterize_class(
+    [
+        {"name": "Global", "tempesta": GLOBAL_TEMPLATE},
+        {"name": "Vhost", "tempesta": VHOST_TEMPLATE},
+    ]
+)
+class StickyCookieConfig(tester.TempestaTest):
+    @dmesg.unlimited_rate_on_tempesta_node
+    def check_cannot_start_impl(self, msg):
+        self.oops_ignore = ["WARNING", "ERROR"]
+        with self.assertRaises(CmdError, msg=""):
+            self.start_tempesta()
+        self.assertTrue(
+            self.oops.find(msg, cond=dmesg.amount_positive), "Tempesta doesn't report error"
+        )
+
+    def setUp(self):
+        super().setUp()
+        srcdir = tf_cfg.cfg.get("Tempesta", "srcdir")
+        workdir = tf_cfg.cfg.get("Tempesta", "workdir")
+        remote.tempesta.run_cmd(f"cp {srcdir}/etc/js_challenge.js.tpl {workdir}")
+        remote.tempesta.run_cmd(f"cp {srcdir}/etc/js_challenge.tpl {workdir}/js1.tpl")
+
+    @parameterize.expand(
+        [
+            param(
+                name="cookie_options_no_cookie",
+                cookie_config="cookie_options Path=/;",
+                msg="http_sess: cookie options requires sticky cookies enabled and explicitly defined in the same section",
+            ),
+            param(
+                name="js_challenge_no_cookie",
+                cookie_config="js_challenge resp_code=503 delay_min=1000 delay_range=3000 %s/js1.html;"
+                % tf_cfg.cfg.get("Tempesta", "workdir"),
+                msg="http_sess: JavaScript challenge requires sticky cookies enabled and explicitly defined in the same section",
+            ),
+            param(
+                name="cookie_options_with_cookie",
+                cookie_config="cookie_options Path=/;\ncookie enforce;",
+                msg=None,
+            ),
+            param(
+                name="js_challenge_with_cookie",
+                cookie_config="js_challenge resp_code=503 delay_min=1000 delay_range=3000 %s/js1.html;\ncookie enforce;"
+                % tf_cfg.cfg.get("Tempesta", "workdir"),
+                msg=None,
+            ),
+        ]
+    )
+    def test(self, name, cookie_config, msg):
+        tempesta_conf = self.get_tempesta().config
+
+        tempesta_conf.set_defconfig(tempesta_conf.defconfig % cookie_config)
+        if msg is not None:
+            self.check_cannot_start_impl(msg)
+        else:
+            self.start_all_services()
+
+
+class StickyCookieOptions(tester.TempestaTest):
+    clients = [{"id": "deproxy", "type": "deproxy", "addr": "${tempesta_ip}", "port": "80"}]
+
+    tempesta = {
+        "config": """
+        srv_group default {
+            server ${server_ip}:8000;
+        }
+        srv_group example {
+            server ${server_ip}:8001;
+        }
+
+        sticky {
+            cookie enforce max_misses=10;
+            %s
+        }
+
+        vhost default {
+            proxy_pass default;
+        }
+
+        vhost example.com {
+            sticky {
+                cookie enforce max_misses=2;
+                %s
+            }
+
+            proxy_pass example;
+        }
+
+        http_chain {
+            host == "example.com" -> example.com;
+            -> default;
+        }
+        """
+    }
+
+    backends = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "port": "8000",
+            "response": "static",
+            "response_content": "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n",
+        }
+    ]
+
+    @parameterize.expand(
+        [
+            # If no options are set, session lifetime is equal to INT_MAX (4294967295)
+            # and Max-Age is set to 4294967295 also. Path is not set, default Path="/"
+            # is used.
+            param(
+                name="empty_options",
+                cookie_options_global="",
+                cookie_options_vhost="",
+                options_global_in_response=["Path=/", "Max-Age=4294967295"],
+                options_vhost_in_response=["Path=/", "Max-Age=4294967295"],
+            ),
+            # Global session lifetime is set and vhost session lifetime is
+            # empty. Global session lifetime is used to set Max-Age for global
+            # and vhost cookies.
+            param(
+                name="empty_vhost_global_sess_lifetime_is_set",
+                cookie_options_global="sess_lifetime 5;",
+                cookie_options_vhost="",
+                options_global_in_response=["Path=/", "Max-Age=5"],
+                options_vhost_in_response=["Path=/", "Max-Age=5"],
+            ),
+            # Global Expires is set, global session lifetime doesn't affect global
+            # cookie options, but vhost cookie options are empty, so global session
+            # lifetime is used to set Max-Age for vhost cookies.
+            param(
+                name="empty_vhost_global_expires_and_sess_lifetime_are_set",
+                cookie_options_global="cookie_options Path=/etc Expires=111;\nsess_lifetime 5;",
+                cookie_options_vhost="",
+                options_global_in_response=["Path=/etc", "Expires=111"],
+                options_vhost_in_response=["Path=/", "Max-Age=5"],
+            ),
+            # Global Max-Age is set, session lifetime doesn't  affect global cookie
+            # options. Vhost session lifetime is set and used to set vhost cookies
+            # Max-Age.
+            param(
+                name="vhost_sess_lifetime_and_global_sess_lifetime_and_max_age_are_set",
+                cookie_options_global="cookie_options Max-Age=111;\nsess_lifetime 5;",
+                cookie_options_vhost="sess_lifetime 3;",
+                options_global_in_response=["Path=/", "Max-Age=111"],
+                options_vhost_in_response=["Path=/", "Max-Age=3"],
+            ),
+            # Secure option is set, Path and Max-Age are set according to
+            # default values ("/" for Path and 4294967295 for Max-Age).
+            param(
+                name="vhost_and_global_options_other",
+                cookie_options_global="cookie_options Secure;",
+                cookie_options_vhost="cookie_options Secure;",
+                options_global_in_response=["Secure", "Path=/", "Max-Age=4294967295"],
+                options_vhost_in_response=["Secure", "Path=/", "Max-Age=4294967295"],
+            ),
+            # Vhost Expires is set, global session lifetime doesn't affect
+            # vhost cookie options.
+            param(
+                name="vhost_expires_is_set",
+                cookie_options_global="sess_lifetime 5;",
+                cookie_options_vhost="cookie_options Expires=111;",
+                options_global_in_response=["Path=/", "Max-Age=5"],
+                options_vhost_in_response=["Path=/", "Expires=111"],
+            ),
+            # Vhost Max-Age is set, global session lifetime doesn't affect
+            # vhost cookie options.
+            param(
+                name="vhost_max_age_is_set",
+                cookie_options_global="sess_lifetime 5;",
+                cookie_options_vhost="cookie_options Max-Age=111;",
+                options_global_in_response=["Path=/", "Max-Age=5"],
+                options_vhost_in_response=["Path=/", "Max-Age=111"],
+            ),
+        ]
+    )
+    def test(
+        self,
+        name,
+        cookie_options_global,
+        cookie_options_vhost,
+        options_global_in_response,
+        options_vhost_in_response,
+    ):
+        """
+        Send two request, first request to default vhost with global cookies and
+        second request to example.com with special cookies. Check Set-Cookie
+        header - Tempesta FW should set Path and Max-Age or Expires.
+        """
+        tempesta_conf = self.get_tempesta().config
+
+        tempesta_conf.set_defconfig(
+            tempesta_conf.defconfig % (cookie_options_global, cookie_options_vhost)
+        )
+        self.start_all_services()
+
+        client = self.get_client("deproxy")
+        client.send_request(client.create_request(method="GET", headers=[]), "302")
+        set_cookie = client.last_response.headers.get("Set-Cookie", None)
+        cookie_opt = set_cookie.split("; ")
+        for opt in options_global_in_response:
+            self.assertIn(opt, cookie_opt)
+        for opt in cookie_opt[1:]:
+            self.assertIn(opt, options_global_in_response)
+
+        client.send_request(
+            client.create_request(method="GET", authority="example.com", headers=[]), "302"
+        )
+        set_cookie = client.last_response.headers.get("Set-Cookie", None)
+        cookie_opt = set_cookie.split("; ")
+        for opt in options_vhost_in_response:
+            self.assertIn(opt, cookie_opt)
+        for opt in cookie_opt[1:]:
+            self.assertIn(opt, options_vhost_in_response)
