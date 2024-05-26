fede44d77bb186899156d525d22d185e392028ba
kingluo <home_king@163.com>
2024-05-26 17:49:01 +0800

    use DeproxyClientH2

--- t_stress/test_slow_read.py
+++ t_stress/test_slow_read.py
@@ -3,32 +3,24 @@ __copyright__ = "Copyright (C) 2022 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
 import os
-import socket
-import ssl
-import time
-import unittest
 from pathlib import Path
-from threading import Thread
-
-import h2
-from h2.settings import SettingCodes, Settings
-from hyperframe.frame import HeadersFrame, PriorityFrame, WindowUpdateFrame
 
 from framework import tester
-from framework.parameterize import param, parameterize
-from helpers import deproxy, tf_cfg
-from t_long_body import utils
+from helpers import tf_cfg
 
 
 class TestH2SlowRead(tester.TempestaTest):
+    parallel = 10
     clients = [
         {
-            "id": "deproxy",
-            "type": "deproxy_h2",  # "deproxy" for HTTP/1
+            "id": f"deproxy-{i}",
+            "type": "deproxy_h2",
             "addr": "${tempesta_ip}",
             "port": "443",
             "ssl": True,
-        },
+            "ssl_hostname": "tempesta-tech.com",
+        }
+        for i in range(parallel)
     ]
 
     backends = [
@@ -82,7 +74,7 @@ http {
     tempesta = {
         "config": """
         cache 0;
-        keepalive_timeout 60;
+        keepalive_timeout 10;
         listen 443 proto=h2;
 
         tls_match_any_server_name;
@@ -118,137 +110,39 @@ http {
     def test_window_update(self):
         self.start_all_services()
 
-        client = self.get_client("deproxy")
-        client.h2_connection.encoder.huffman = True
+        for client in self.get_clients():
+            client.update_initial_settings(initial_window_size=1)
+            client.send_bytes(client.h2_connection.data_to_send())
+            self.assertTrue(client.wait_for_ack_settings())
+            for _ in range(100):
+                # send HEADERS frame with END_STREAM flag
+                client.make_request(
+                    client.create_request(
+                        method="GET", headers=[], authority="tempesta-tech.com", uri="/large.file"
+                    ),
+                    end_stream=True,
+                )
 
-        headers = [
-            (":method", "GET"),
-            (":path", "/large.file"),
-            (":authority", "tempesta-tech.com"),
-            (":scheme", "https"),
-        ]
-        headers_data = client.h2_connection.encoder.encode(headers)
-
-        hostname = tf_cfg.cfg.get("Tempesta", "hostname")
-        port = 443
-
-        def run_test():
-            context = ssl.create_default_context()
-            context.set_alpn_protocols(["h2"])
-            context.check_hostname = False
-            context.verify_mode = ssl.CERT_NONE
-            context.keylog_filename = "secrets.txt"
-
-            with socket.create_connection((hostname, port)) as sock:
-                with context.wrap_socket(sock, server_hostname="tempesta-tech.com") as s:
-                    c = h2.connection.H2Connection()
-                    settings = dict()
-                    settings[SettingCodes.INITIAL_WINDOW_SIZE] = 1
-                    c.local_settings = Settings(initial_values=settings)
-                    c.local_settings.update(settings)
-                    c.initiate_connection()
-                    s.sendall(c.data_to_send())
-
-                    for i in range(1, 190, 2):
-                        c.send_headers(i, headers, end_stream=True)
-                        try:
-                            s.sendall(c.data_to_send())
-                        except ssl.SSLEOFError:
-                            return
-
-                    response_stream_ended = False
-
-                    while not response_stream_ended:
-                        # read raw data from the socket
-                        data = s.recv(65536 * 1024)
-                        if not data:
-                            break
-
-                        # feed raw data into h2, and process resulting events
-                        events = c.receive_data(data)
-                        for event in events:
-                            if isinstance(event, h2.events.DataReceived):
-                                # update flow control so the server doesn't starve us
-                                c.acknowledge_received_data(
-                                    event.flow_controlled_length, event.stream_id
-                                )
-                                win = WindowUpdateFrame(event.stream_id, 1).serialize()
-                                try:
-                                    s.sendall(win)
-                                except ssl.SSLEOFError:
-                                    return
-                                time.sleep(0.003)
-                            if isinstance(event, h2.events.StreamEnded):
-                                # response body completed, let's exit the loop
-                                response_stream_ended = True
-                                break
-                        try:
-                            # send any pending data to the server
-                            s.sendall(c.data_to_send())
-                        except ssl.SSLEOFError:
-                            return
-
-        parallel = 10
-        plist = []
-        for _ in range(parallel):
-            p = Thread(target=run_test, args=())
-            p.start()
-            plist.append(p)
-        for p in plist:
-            p.join()
+        for client in self.get_clients():
+            self.assertTrue(client.wait_for_connection_close())
 
     def test_tcp(self):
         self.start_all_services()
 
-        client = self.get_client("deproxy")
-        client.h2_connection.encoder.huffman = True
+        for client in self.get_clients():
+            client.update_initial_settings(initial_window_size=(1 << 31) - 10)
+            client.send_bytes(client.h2_connection.data_to_send())
+            self.assertTrue(client.wait_for_ack_settings())
+            client.readable = lambda: False
+            for _ in range(100):
+                client.make_request(
+                    client.create_request(
+                        method="GET", headers=[], authority="tempesta-tech.com", uri="/large.file"
+                    ),
+                    end_stream=True,
+                )
 
-        headers = [
-            (":method", "GET"),
-            (":path", "/large.file"),
-            (":authority", "tempesta-tech.com"),
-            (":scheme", "https"),
-        ]
-        headers_data = client.h2_connection.encoder.encode(headers)
-
-        hostname = tf_cfg.cfg.get("Tempesta", "hostname")
-        port = 443
-
-        def run_test():
-            context = ssl.create_default_context()
-            context.set_alpn_protocols(["h2"])
-            context.check_hostname = False
-            context.verify_mode = ssl.CERT_NONE
-
-            with socket.create_connection((hostname, port)) as sock:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 16)
-                with context.wrap_socket(sock, server_hostname="tempesta-tech.com") as s:
-                    c = h2.connection.H2Connection()
-                    settings = dict()
-                    settings[SettingCodes.INITIAL_WINDOW_SIZE] = (1 << 31) - 10
-                    c.local_settings = Settings(initial_values=settings)
-                    c.local_settings.update(settings)
-                    c.initiate_connection()
-                    s.sendall(c.data_to_send())
-
-                    for i in range(1, 190, 2):
-                        hf = HeadersFrame(
-                            stream_id=i,
-                            data=headers_data,
-                            flags=["END_HEADERS", "END_STREAM"],
-                        ).serialize()
-                        try:
-                            s.sendall(hf)
-                        except ssl.SSLEOFError:
-                            return
-
-                    time.sleep(10)
-
-        parallel = 10
-        plist = []
-        for _ in range(parallel):
-            p = Thread(target=run_test, args=())
-            p.start()
-            plist.append(p)
-        for p in plist:
-            p.join()
+        for client in self.get_clients():
+            self.assertFalse(client.wait_for_response(10))
+            client.readable = lambda: True
+            self.assertTrue(client.wait_for_connection_close())
