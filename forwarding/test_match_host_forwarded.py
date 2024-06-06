8b4ae6d41a7872465a27391404fb9000b05bbf5c
Roman <rb@tempesta-tech.com>
2024-06-06 16:30:48 +0400

    rework tests with parametrize

--- forwarding/test_match_host_forwarded.py
+++ forwarding/test_match_host_forwarded.py
@@ -3,14 +3,16 @@ Tests for verifying correctness of matching
 all host headers (URI, Host, Forwarded).
 """
 from helpers import chains
-from test_suite import tester
+from test_suite import marks, tester
 
 __author__ = "Tempesta Technologies, Inc."
-__copyright__ = "Copyright (C) 2022 Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2022-2025 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
 
 class TestMatchHost(tester.TempestaTest):
+    clients = [{"id": "deproxy", "type": "deproxy", "addr": "${tempesta_ip}", "port": "80"}]
+
     backends = [
         {
             "id": 0,
@@ -70,132 +72,110 @@ class TestMatchHost(tester.TempestaTest):
         """
     }
 
-    requests_opt = [
-        {
-            "uri": "http://testshop.com",  # <--must be matched by "host eq"
-            "headers": ["Host: testwiki.com", "Forwarded: host=testapp.com"],
-            "block": False,
-            "sid": 0,
-        },
-        {
-            "uri": "http://testshop.com",  # <--must be matched by "host eq"
-            "headers": ["Host: badhost.com", "Forwarded: host=badhost.com"],
-            "block": False,
-            "sid": 0,
-        },
-        {
-            "uri": "http://testshop.com",
-            "headers": [
-                "Host: testapp.com",  # <--must be matched by "hdr host == testapp.com"
-                "Forwarded: host=testwiki.com",
-            ],
-            "block": False,
-            "sid": 2,
-        },
-        {
-            "uri": "http://badhost.com",
-            "headers": [
-                "Host: badhost.com",
-                "Forwarded: host=testshop.com",  # <--must be matched by "hdr forwarded"
-            ],
-            "block": False,
-            "sid": 0,
-        },
-        {
-            "uri": "http://badhost.com",
-            "headers": [
-                "Host: badhost.com",
-                "Forwarded: host=unkhost.com",
-                "Forwarded: host=testshop.com",  # <--must be matched by "hdr forwarded"
-            ],
-            "block": False,
-            "sid": 0,
-        },
-        {
-            "uri": "/foo",
-            "headers": [
-                "Host: testwiki.com",  # <--must be matched by "host eq"
-                "Forwarded: host=forwarded.host.ignored",
-            ],
-            "block": False,
-            "sid": 1,
-        },
-        {
-            "uri": "/foo",
-            "headers": [
-                "Host: TesTaPp.cOm",  # <--must be matched by "host eq"
-                "Forwarded: HoSt=forwarded.host.ignored",
-            ],
-            "block": False,
-            "sid": 2,
-        },
-        {
-            "uri": "/foo",
-            "headers": [
-                ("Host", "[fd80::1cb2:ad12:ca16:98ef]:8080"),  # <--must be matched by "host eq"
-                (
-                    "Forwarded",
-                    'host="forwarded.host.ignored"',
-                ),
-            ],
-            "block": False,
-            "sid": 2,
-        },
-        {
-            "uri": "/foo",
-            "headers": ["Host: badhost.com", "Forwarded: host=forwarded.host.ignored"],
-            "block": True,
-            "sid": 0,
-        },
-        {"uri": "/foo", "headers": ["Host: unkhost.com"], "block": True, "sid": 0},
-    ]
-
-    blocked_response_status = "403"
-
-    def add_client(self, cid):
-        client = {"id": cid, "type": "deproxy", "addr": "${tempesta_ip}", "port": "80"}
-        self.clients.append(client)
-
-    def setUp(self):
-        # Create a client for each request
-        count = len(self.requests_opt)
-        for i in range(count):
-            self.add_client(i)
-        tester.TempestaTest.setUp(self)
-
-    def start_all(self):
-        self.start_all_servers()
-        self.start_tempesta()
-        self.start_all_clients()
-        self.deproxy_manager.start()
-        self.assertTrue(self.wait_all_connections())
-
-    def process(self, client, server, opt, req_id):
-        uri = opt["uri"]
-        first_line = "GET " + uri + " HTTP/1.1\r\n"
-        base_headers = "\r\n".join(opt["headers"]) + "\r\n"
-        req_id_hdr = "x-req-id: " + str(req_id) + "\r\n"
-        request = first_line + base_headers + req_id_hdr + "\r\n"
-
-        client.make_request(request)
-        client.wait_for_response()
-
-        if not opt["block"]:
-            last_req_id = int(server.last_request.headers.get("x-req-id"))
-            self.assertEqual(last_req_id, req_id)
-        else:
-            last_response_status = client.last_response.status
-            self.assertEqual(self.blocked_response_status, last_response_status)
-
-    def test(self):
+    @marks.Parameterize.expand(
+        [
+            marks.Param(
+                name=1,
+                uri="http://testshop.com",  # <--must be matched by "host eq"
+                host="testwiki.com",
+                headers=[("Forwarded", "host=testapp.com")],
+                status="200",
+                sid=0,
+            ),
+            marks.Param(
+                name=2,
+                uri="http://testshop.com",  # <--must be matched by "host eq"
+                host="badhost.com",
+                headers=[("Forwarded", "host=badhost.com")],
+                status="200",
+                sid=0,
+            ),
+            marks.Param(
+                name=3,
+                uri="http://testshop.com",
+                host="testapp.com",  # <--must be matched by "hdr host == testapp.com"
+                headers=[("Forwarded", "host=testwiki.com")],
+                status="200",
+                sid=2,
+            ),
+            marks.Param(
+                name=4,
+                uri="http://badhost.com",
+                host="badhost.com",
+                headers=[("Forwarded", "host=testshop.com")],  # must be matched by "hdr forwarded"
+                status="200",
+                sid=0,
+            ),
+            marks.Param(
+                name=5,
+                uri="http://badhost.com",
+                host="badhost.com",
+                headers=[
+                    ("Forwarded", "host=unkhost.com"),
+                    ("Forwarded", "host=testshop.com"),  # <--must be matched by "hdr forwarded"
+                ],
+                status="200",
+                sid=0,
+            ),
+            marks.Param(
+                name=6,
+                uri="/foo",
+                host="testwiki.com",  # <--must be matched by "host eq"
+                headers=[("Forwarded", "host=forwarded.host.ignored")],
+                status="200",
+                sid=1,
+            ),
+            marks.Param(
+                name=7,
+                uri="/foo",
+                host="TesTaPp.cOm",  # <--must be matched by "host eq"
+                headers=[("Forwarded", "HoSt=forwarded.host.ignored")],
+                status="200",
+                sid=2,
+            ),
+            marks.Param(
+                name=7,
+                uri="/foo",
+                host="[fd80::1cb2:ad12:ca16:98ef]:8080",  # <--must be matched by "host eq"
+                headers=[("Forwarded", 'host="forwarded.host.ignored"')],
+                status="200",
+                sid=2,
+            ),
+            marks.Param(
+                name=8,
+                uri="/foo",
+                host="badhost.com",
+                headers=[("Forwarded", "host=forwarded.host.ignored")],
+                status="403",
+                sid=0,
+            ),
+            marks.Param(
+                name=9,
+                uri="/foo",
+                host="unkhost.com",
+                headers=[],
+                status="403",
+                sid=0,
+            ),
+        ]
+    )
+    def test(self, name, uri, host, headers, status, sid):
         """
         Send requests with different hosts
         and check correctness of forwarding
         comparing id of the last request on
         client and server.
         """
-        self.start_all()
+        self.start_all_services()
+        client = self.get_client("deproxy")
+        server = self.get_server(sid)
 
-        for req_id, opt in enumerate(self.requests_opt):
-            sid = opt["sid"]
-            self.process(self.get_client(req_id), self.get_server(sid), opt, req_id)
+        client.send_request(
+            client.create_request(
+                method="GET", uri=uri, authority=host, headers=headers + [("x-req-id", str(name))]
+            ),
+            expected_status_code=status,
+        )
+
+        if status == "200":
+            self.assertEqual(int(server.last_request.headers.get("x-req-id")), name)
