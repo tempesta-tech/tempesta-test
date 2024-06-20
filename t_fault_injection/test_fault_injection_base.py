e043028137cda440d364b202a945d8d2c542213a
EvgeniiMekhanik <em@tempesta-tech.com>
2024-06-20 13:57:18 +0300

    Implement test to check BUG when hpack initialization fails

--- t_fault_injection/test_fault_injection_base.py
+++ t_fault_injection/test_fault_injection_base.py
@@ -28,23 +28,37 @@ class TestFailFunction(tester.TempestaTest):
             "addr": "${tempesta_ip}",
             "port": "80",
         },
+        {
+            "id": "deproxy_h2",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+        },
     ]
 
     tempesta = {
         "config": """
             listen 80;
+            listen 443 proto=h2;
+
+            tls_certificate ${tempesta_workdir}/tempesta.crt;
+            tls_certificate_key ${tempesta_workdir}/tempesta.key;
+            tls_match_any_server_name;
+
             server ${server_ip}:8000;
         """
     }
 
     @parameterize.expand(
         [
-            param(name="tfw_cli_conn_alloc", msg="can't allocate a new client connection"),
-            param(name="tfw_client_obtain", msg="can't obtain a client for frang accounting"),
+            param(name="tfw_cli_conn_alloc", id="deproxy", msg="can't allocate a new client connection", retval=0),
+            param(name="tfw_client_obtain", id="deproxy", msg="can't obtain a client for frang accounting", retval=0),
+            param(name="tfw_h2_context_init", id="deproxy_h2", msg="cannot establish a new h2 connection", retval=-12)
         ]
     )
     @dmesg.unlimited_rate_on_tempesta_node
-    def test(self, name, msg):
+    def test(self, name, id, msg, retval):
         """
         Basic test to check how Tempesta FW works when some internal
         function fails. Function should be marked as ALLOW_ERROR_INJECTION
@@ -57,7 +71,7 @@ class TestFailFunction(tester.TempestaTest):
         out = remote.client.run_cmd(cmd)
         # Write return code, which this function should return instead of
         # real return code.
-        cmd = f"printf %#x 0 > /sys/kernel/debug/fail_function/{name}/retval"
+        cmd = f"printf %#x {retval} > /sys/kernel/debug/fail_function/{name}/retval"
         out = remote.client.run_cmd(cmd)
 
         # Write probability in percent. 100 - function never executed
@@ -73,12 +87,13 @@ class TestFailFunction(tester.TempestaTest):
         cmd = "printf %#x -1 > /sys/kernel/debug/fail_function/times"
         out = remote.client.run_cmd(cmd)
 
-        client = self.get_client("deproxy")
+        client = self.get_client(id)
         request = client.create_request(method="GET", headers=[])
         client.start()
 
         self.oops_ignore = ["ERROR"]
         client.make_request(request)
+        self.assertFalse(client.wait_for_response(3))
         self.assertTrue(client.wait_for_connection_close())
 
         # Disable error injection
