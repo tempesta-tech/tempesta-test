cbaad5f0c6004d59b46495e5261772cbfc8812e7
EvgeniiMekhanik <em@tempesta-tech.com>
2024-07-08 13:38:18 +0300

    Add tests to check `tfw_tls_encrypt`
    Add tests to check that `tfw_tls_encrypt` is
    now can restore after -ENOMEM error.

--- t_fault_injection/test_fault_injection_base.py
+++ t_fault_injection/test_fault_injection_base.py
@@ -7,10 +7,11 @@ __license__ = "GPL2"
 from framework import tester
 from framework.parameterize import param, parameterize
 from framework.x509 import CertGenerator
-from helpers import dmesg, remote
+from helpers import dmesg, remote, sysnet, tf_cfg
+from helpers.networker import NetWorker
 
 
-class TestFailFunction(tester.TempestaTest):
+class TestFailFunction(tester.TempestaTest, NetWorker):
     backends = [
         {
             "id": "deproxy",
@@ -52,13 +53,87 @@ class TestFailFunction(tester.TempestaTest):
 
     @parameterize.expand(
         [
-            param(name="tfw_cli_conn_alloc", id="deproxy", msg="can't allocate a new client connection", retval=0),
-            param(name="tfw_client_obtain", id="deproxy", msg="can't obtain a client for frang accounting", retval=0),
-            param(name="tfw_hpack_init", id="deproxy_h2", msg="cannot establish a new h2 connection", retval=-12)
+            param(
+                name="tfw_cli_conn_alloc",
+                func_name="tfw_cli_conn_alloc",
+                id="deproxy",
+                msg="can't allocate a new client connection",
+                times=-1,
+                should_fail=True,
+                response_len=0,
+                mtu=None,
+                retval=0,
+            ),
+            param(
+                name="tfw_client_obtain",
+                func_name="tfw_client_obtain",
+                id="deproxy",
+                msg="can't obtain a client for frang accounting",
+                times=-1,
+                should_fail=True,
+                response_len=0,
+                mtu=None,
+                retval=0,
+            ),
+            param(
+                name="tfw_hpack_init",
+                func_name="tfw_hpack_init",
+                id="deproxy_h2",
+                msg="cannot establish a new h2 connection",
+                times=-1,
+                should_fail=True,
+                response_len=0,
+                mtu=None,
+                retval=-12,
+            ),
+            param(
+                name="ss_skb_expand_head_tail",
+                func_name="ss_skb_expand_head_tail",
+                id="deproxy_h2",
+                msg="tfw_tls_encrypt: cannot encrypt data",
+                times=1,
+                should_fail=False,
+                response_len=0,
+                mtu=None,
+                retval=-12,
+            ),
+            param(
+                name="ss_skb_to_sgvec_with_new_pages",
+                func_name="ss_skb_to_sgvec_with_new_pages",
+                id="deproxy_h2",
+                msg="tfw_tls_encrypt: cannot encrypt data",
+                times=1,
+                should_fail=False,
+                response_len=0,
+                mtu=None,
+                retval=-12,
+            ),
+            param(
+                name="ss_skb_to_sgvec_with_new_pages_long_resp",
+                func_name="ss_skb_to_sgvec_with_new_pages",
+                id="deproxy_h2",
+                msg="tfw_tls_encrypt: cannot encrypt data",
+                times=1,
+                should_fail=False,
+                response_len=30000,
+                mtu=100,
+                retval=-12,
+            ),
         ]
     )
     @dmesg.unlimited_rate_on_tempesta_node
-    def test(self, name, id, msg, retval):
+    def test(self, name, func_name, id, msg, times, should_fail, response_len, mtu, retval):
+        if mtu:
+            try:
+                dev = sysnet.route_dst_ip(remote.client, tf_cfg.cfg.get("Tempesta", "ip"))
+                prev_mtu = sysnet.change_mtu(remote.client, dev, mtu)
+                self._test(name, func_name, id, msg, times, should_fail, response_len, retval)
+            finally:
+                sysnet.change_mtu(remote.client, dev, prev_mtu)
+        else:
+            self._test(name, func_name, id, msg, times, should_fail, response_len, retval)
+
+    def _test(self, name, func_name, id, msg, times, should_fail, response_len, retval):
         """
         Basic test to check how Tempesta FW works when some internal
         function fails. Function should be marked as ALLOW_ERROR_INJECTION
@@ -66,12 +141,15 @@ class TestFailFunction(tester.TempestaTest):
         """
         self.start_all_services(client=False)
 
-        # Write function name to special debug fs file. 
-        cmd = f"echo {name} > /sys/kernel/debug/fail_function/inject"
+        if response_len != 0:
+            server = self.get_server("deproxy").set_response(self.make_response(response_len))
+
+        # Write function name to special debug fs file.
+        cmd = f"echo {func_name} > /sys/kernel/debug/fail_function/inject"
         out = remote.client.run_cmd(cmd)
         # Write return code, which this function should return instead of
         # real return code.
-        cmd = f"printf %#x {retval} > /sys/kernel/debug/fail_function/{name}/retval"
+        cmd = f"printf %#x {retval} > /sys/kernel/debug/fail_function/{func_name}/retval"
         out = remote.client.run_cmd(cmd)
 
         # Write probability in percent. 100 - function never executed
@@ -84,17 +162,22 @@ class TestFailFunction(tester.TempestaTest):
         out = remote.client.run_cmd(cmd)
         # Specifies how many times failures may happen at most.
         # A value of -1 means “no limit”.
-        cmd = "printf %#x -1 > /sys/kernel/debug/fail_function/times"
+        cmd = f"printf %#x {times} > /sys/kernel/debug/fail_function/times"
         out = remote.client.run_cmd(cmd)
 
         client = self.get_client(id)
         request = client.create_request(method="GET", headers=[])
         client.start()
 
-        self.oops_ignore = ["ERROR"]
+        if should_fail:
+            self.oops_ignore = ["ERROR"]
         client.make_request(request)
-        self.assertFalse(client.wait_for_response(3))
-        self.assertTrue(client.wait_for_connection_close())
+        if should_fail:
+            self.assertFalse(client.wait_for_response(3))
+            self.assertTrue(client.wait_for_connection_close())
+        else:
+            self.assertTrue(client.wait_for_response(3))
+            self.assertEqual(client.last_response.status, "200")
 
         # Disable error injection
         cmd = "echo 0 > /sys/kernel/debug/fail_function/times"
@@ -102,8 +185,20 @@ class TestFailFunction(tester.TempestaTest):
         # Remove function from fault injection list
         cmd = "echo > /sys/kernel/debug/fail_function/inject"
         out = remote.client.run_cmd(cmd)
+        # Restore times
+        cmd = f"printf %#x -1 > /sys/kernel/debug/fail_function/times"
+        out = remote.client.run_cmd(cmd)
 
         self.assertTrue(
             self.oops.find(msg, cond=dmesg.amount_positive),
             "Tempesta doesn't report error",
+        )
+
+    @staticmethod
+    def make_response(body_len):
+        body = body_len * "A"
+        return (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Length: " + str(len(body)) + "\r\n"
+            "Connection: keep-alive\r\n\r\n" + body
         )
