72ef8708dc136aaaea8d6baea642f1fab467bd1c
EvgeniiMekhanik <em@tempesta-tech.com>
2024-09-03 15:28:18 +0300

    Implement error injection tests for pipelined responses

--- t_fault_injection/test_fault_injection_base.py
+++ t_fault_injection/test_fault_injection_base.py
@@ -10,7 +10,7 @@ from framework.x509 import CertGenerator
 from helpers import dmesg, remote
 
 
-class TestFailFunction(tester.TempestaTest):
+class TestFailFunctionBase(tester.TempestaTest):
     backends = [
         {
             "id": "deproxy",
@@ -46,7 +46,7 @@ class TestFailFunction(tester.TempestaTest):
             tls_certificate_key ${tempesta_workdir}/tempesta.key;
             tls_match_any_server_name;
 
-            server ${server_ip}:8000;
+            server ${server_ip}:8000 conns_n=1;
             
             frang_limits {
                 http_strict_host_checking false;
@@ -54,43 +54,14 @@ class TestFailFunction(tester.TempestaTest):
         """
     }
 
-    @parameterize.expand(
-        [
-            param(
-                name="tfw_cli_conn_alloc",
-                id="deproxy",
-                msg="can't allocate a new client connection",
-                retval=0,
-            ),
-            param(
-                name="tfw_client_obtain",
-                id="deproxy",
-                msg="can't obtain a client for frang accounting",
-                retval=0,
-            ),
-            param(
-                name="tfw_hpack_init",
-                id="deproxy_h2",
-                msg="cannot establish a new h2 connection",
-                retval=-12,
-            ),
-        ]
-    )
-    @dmesg.unlimited_rate_on_tempesta_node
-    def test(self, name, id, msg, retval):
-        """
-        Basic test to check how Tempesta FW works when some internal
-        function fails. Function should be marked as ALLOW_ERROR_INJECTION
-        in Tempesta FW source code.
-        """
-        self.start_all_services(client=False)
-
+    @staticmethod
+    def setup_fail_function_test(func_name, times, retval):
         # Write function name to special debug fs file.
-        cmd = f"echo {name} > /sys/kernel/debug/fail_function/inject"
+        cmd = f"echo {func_name} > /sys/kernel/debug/fail_function/inject"
         out = remote.client.run_cmd(cmd)
         # Write return code, which this function should return instead of
         # real return code.
-        cmd = f"printf %#x {retval} > /sys/kernel/debug/fail_function/{name}/retval"
+        cmd = f"printf %#x {retval} > /sys/kernel/debug/fail_function/{func_name}/retval"
         out = remote.client.run_cmd(cmd)
 
         # Write probability in percent. 100 - function never executed
@@ -103,26 +74,163 @@ class TestFailFunction(tester.TempestaTest):
         out = remote.client.run_cmd(cmd)
         # Specifies how many times failures may happen at most.
         # A value of -1 means “no limit”.
-        cmd = "printf %#x -1 > /sys/kernel/debug/fail_function/times"
+        cmd = f"printf %#x {times} > /sys/kernel/debug/fail_function/times"
         out = remote.client.run_cmd(cmd)
 
-        client = self.get_client(id)
-        request = client.create_request(method="GET", headers=[])
-        client.start()
-
-        self.oops_ignore = ["ERROR"]
-        client.make_request(request)
-        self.assertFalse(client.wait_for_response(3))
-        self.assertTrue(client.wait_for_connection_close())
-
+    @staticmethod
+    def teardown_fail_function_test():
         # Disable error injection
         cmd = "echo 0 > /sys/kernel/debug/fail_function/times"
         out = remote.client.run_cmd(cmd)
         # Remove function from fault injection list
         cmd = "echo > /sys/kernel/debug/fail_function/inject"
         out = remote.client.run_cmd(cmd)
+        # Restore times
+        cmd = f"printf %#x -1 > /sys/kernel/debug/fail_function/times"
+        out = remote.client.run_cmd(cmd)
+
+
+class TestFailFunction(TestFailFunctionBase):
+    @parameterize.expand(
+        [
+            param(
+                name="tfw_cli_conn_alloc",
+                func_name="tfw_cli_conn_alloc",
+                id="deproxy",
+                msg="can't allocate a new client connection",
+                times=-1,
+                should_fail=True,
+                retval=0,
+            ),
+            param(
+                name="tfw_client_obtain",
+                func_name="tfw_client_obtain",
+                id="deproxy",
+                msg="can't obtain a client for frang accounting",
+                times=-1,
+                should_fail=True,
+                retval=0,
+            ),
+            param(
+                name="tfw_hpack_init",
+                func_name="tfw_hpack_init",
+                id="deproxy_h2",
+                msg="cannot establish a new h2 connection",
+                times=-1,
+                should_fail=True,
+                retval=-12,
+            ),
+        ]
+    )
+    @dmesg.unlimited_rate_on_tempesta_node
+    def test(self, name, func_name, id, msg, times, should_fail, retval):
+        """
+        Basic test to check how Tempesta FW works when some internal
+        function fails. Function should be marked as ALLOW_ERROR_INJECTION
+        in Tempesta FW source code.
+        """
+        srv = self.get_server("deproxy")
+        srv.conns_n = 1
+        self.start_all_services(client=False)
+
+        self.setup_fail_function_test(func_name, times, retval)
+
+        client = self.get_client(id)
+        request = client.create_request(method="GET", headers=[])
+        client.start()
+
+        if should_fail:
+            self.oops_ignore = ["ERROR"]
+        client.make_request(request)
+
+        if should_fail:
+            # This is necessary to be sure that Tempesta FW write
+            # appropriate message in dmesg.
+            self.assertFalse(client.wait_for_response(3))
+            self.assertTrue(client.wait_for_connection_close())
+        else:
+            self.assertTrue(client.wait_for_response(3))
+            self.assertEqual(client.last_response.status, "200")
 
         self.assertTrue(
             self.oops.find(msg, cond=dmesg.amount_positive),
             "Tempesta doesn't report error",
         )
+
+        # This should be called in case if test fails also
+        self.teardown_fail_function_test()
+
+
+class TestFailFunctionPipelinedResponses(TestFailFunctionBase):
+    clients = [
+        {
+            "id": "deproxy_1",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+        },
+        {
+            "id": "deproxy_2",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+        },
+        {
+            "id": "deproxy_3",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+        },
+    ]
+
+    ids = ["deproxy_1", "deproxy_2", "deproxy_3"]
+
+    @parameterize.expand(
+        [
+            param(
+                name="tfw_http_msg_create_sibling",
+                func_name="tfw_http_msg_create_sibling",
+                id="deproxy_h2",
+                msg="Can't create pipelined response",
+                times=-1,
+                retval=0,
+            )
+        ]
+    )
+    @dmesg.unlimited_rate_on_tempesta_node
+    def test(self, name, func_name, id, msg, times, retval):
+        srv = self.get_server("deproxy")
+        srv.pipelined = 3
+        srv.conns_n = 1
+        self.start_all_services(client=False)
+
+        self.setup_fail_function_test(func_name, times, retval)
+
+        i = 0
+        for id in self.ids:
+            i = i + 1
+            client = self.get_client(id)
+            request = client.create_request(method="GET", headers=[])
+            client.start()
+            client.make_request(request)
+            srv.wait_for_requests(i)
+
+        i = 0
+        for id in self.ids:
+            i = i + 1
+            client = self.get_client(id)
+            if i >= 2:
+                self.assertFalse(client.wait_for_response(1))
+            else:
+                self.assertTrue(client.wait_for_response())
+                self.assertTrue(client.last_response.status, "200")
+        self.assertTrue(
+            self.oops.find(msg, cond=dmesg.amount_positive),
+            "Tempesta doesn't report error",
+        )
+
+        # This should be called in case if test fails also
+        self.teardown_fail_function_test()
