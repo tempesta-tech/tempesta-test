20b7d9ed63a226852465d9d4bea39334d88e7da4
EvgeniiMekhanik <em@tempesta-tech.com>
2024-11-07 19:36:49 +0200

    Implement more tests to check Tempesta FW under flood
    - Implement tests to check Tempesta FW under different
      type of floods. (different types of RST flood, settings
      flood ping flood)
    - Implement tests to check that Tempesta FW process requests
      from other clients under flood.

--- t_stress/test_stress.py
+++ t_stress/test_stress.py
@@ -3,6 +3,7 @@ HTTP Stress tests - load Tempesta FW with multiple connections.
 """
 
 import os
+import threading
 import time
 from pathlib import Path
 
@@ -790,6 +791,15 @@ class TestContinuationFlood(tester.TempestaTest):
         self.assertEqual(0, client.returncode)
 
 
+DEPROXY_CLIENT_H2 = {
+    "id": "deproxy",
+    "type": "deproxy_h2",
+    "addr": "${tempesta_ip}",
+    "port": "443",
+    "ssl": True,
+}
+
+
 @marks.parameterize_class(
     [
         {
@@ -802,7 +812,9 @@ class TestContinuationFlood(tester.TempestaTest):
                     "ssl": True,
                     "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type ping_frame -frame_count 100000",
                 },
+                DEPROXY_CLIENT_H2,
             ],
+            "timeout": 120,
         },
         {
             "name": "SettingsFlood",
@@ -814,25 +826,74 @@ class TestContinuationFlood(tester.TempestaTest):
                     "ssl": True,
                     "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type settings_frame -frame_count 100000",
                 },
+                DEPROXY_CLIENT_H2,
             ],
+            "timeout": 120,
         },
         {
-            "name": "RstFlood",
+            "name": "WndUpdateFlood",
             "clients": [
                 {
                     "id": "ctrl_frames_flood",
                     "type": "external",
                     "binary": "ctrl_frames_flood",
                     "ssl": True,
-                    "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type rst_stream_frame -frame_count 100000",
+                    "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type window_update -frame_count 100000",
                 },
+                DEPROXY_CLIENT_H2,
             ],
+            "timeout": 120,
+        },
+        {
+            "name": "RstFloodByHeaders",
+            "clients": [
+                {
+                    "id": "ctrl_frames_flood",
+                    "type": "external",
+                    "binary": "ctrl_frames_flood",
+                    "ssl": True,
+                    "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type rst_stream_frame -rst_reason_type headers -frame_count 100000",
+                },
+                DEPROXY_CLIENT_H2,
+            ],
+            "timeout": 120,
+        },
+        {
+            "name": "RstFloodByWndUpdate",
+            "clients": [
+                {
+                    "id": "ctrl_frames_flood",
+                    "type": "external",
+                    "binary": "ctrl_frames_flood",
+                    "ssl": True,
+                    "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type rst_stream_frame -rst_reason_type window_update -frame_count 1000000",
+                },
+                DEPROXY_CLIENT_H2,
+            ],
+            "timeout": 120,
+        },
+        {
+            "name": "RstFloodByPriority",
+            "clients": [
+                {
+                    "id": "ctrl_frames_flood",
+                    "type": "external",
+                    "binary": "ctrl_frames_flood",
+                    "ssl": True,
+                    "cmd_args": "-address ${tempesta_ip}:443 -threads 4 -connections 100 -debug 1 -ctrl_frame_type rst_stream_frame -rst_reason_type priority -frame_count 1000000",
+                },
+                DEPROXY_CLIENT_H2,
+            ],
+            "timeout": 240,
         },
     ]
 )
-class TestCtrlFrameFlood(tester.TempestaTest):
+class TestRequestsUnderCtrlFrameFlood(tester.TempestaTest):
     """
-    Test stability against comtrol frames frame flood.
+    Test ability to handle requests from the client
+    under control frames frame flood.
+    Also check that there is no kernel BUGS and WARNINGs
+    under flood.
     """
 
     backends = [
@@ -858,20 +919,45 @@ class TestCtrlFrameFlood(tester.TempestaTest):
     """
     }
 
+    stop_flag = False
+
     def setUp(self):
         self.enable_memleak_check()
         super().setUp()
 
+    def __flood(self):
+        client = self.get_client("ctrl_frames_flood")
+        client.start()
+        self.wait_while_busy(client, timeout=self.timeout)
+        client.stop()
+        self.stop_flag = True
+
     @dmesg.limited_rate_on_tempesta_node
     def test(self):
-        client = self.get_client("ctrl_frames_flood")
+        self.start_all_services(client=False)
         tempesta = self.get_tempesta()
 
-        self.start_all_servers()
-        self.start_tempesta()
-        self.start_all_clients()
-        self.wait_while_busy(client, timeout=120)
-        client.stop()
+        client = self.get_client("deproxy")
+        client.start()
+        client.update_initial_settings()
+        client.send_bytes(client.h2_connection.data_to_send())
+        client.h2_connection.clear_outbound_data_buffer()
+
+        self.assertTrue(
+            client.wait_for_ack_settings(),
+            "Tempesta foes not returns SETTINGS frame with ACK flag.",
+        )
+
+        request = client.create_request(method="GET", headers=[])
+
+        t = threading.Thread(target=self.__flood)
+        t.start()
+
+        for _ in range(1, 20):
+            client.send_request(request, "200")
+        while not self.stop_flag:
+            pass
+        self.stop_flag = False
 
         tempesta.get_stats()
         """
@@ -880,7 +966,7 @@ class TestCtrlFrameFlood(tester.TempestaTest):
         established.
         """
         self.assertTrue(
-            tempesta.stats.wq_full > 0 or tempesta.stats.cl_established_connections == 100
+            tempesta.stats.wq_full > 0 or tempesta.stats.cl_established_connections == 101
         )
 
 
--- tools/ctrl_frames_flood/main.go
+++ tools/ctrl_frames_flood/main.go
@@ -21,7 +21,17 @@ const (
 	CtrlFrameTypeUnknown CtrlFrameType = iota
 	CtrlFramePing
 	CtrlFrameSettings
+	CtrlFrameWndUpdate
 	CtrlFrameRst
+)
+
+type RstByType int
+
+const (
+	RstByUnknown RstByType = iota
+	RstByHeaderFrames
+	RstByWndUpdateFrames
+	RstByPriority
 )
 
 const (
@@ -37,18 +47,21 @@ var (
 	address         	string
 	host       		string
 	ctrl_frame_type		string
+	rst_reason_type		string
 	frame_count		int
 	debug			int
 )
 
 var finished int64
 
-func transition(s string) CtrlFrameType {
+func frameTypeTransition(s string) CtrlFrameType {
 	switch s {
 	case "ping_frame":
 		return CtrlFramePing
 	case "settings_frame":
 		return CtrlFrameSettings
+	case "window_update":
+		return CtrlFrameWndUpdate
 	case "rst_stream_frame":
 		return CtrlFrameRst
 	default:
@@ -58,12 +71,39 @@ func transition(s string) CtrlFrameType {
 	return CtrlFrameTypeUnknown
 }
 
+func rstTypeTransision(frame_type CtrlFrameType, s string) RstByType {
+	var rst_type RstByType = RstByUnknown
+
+	switch s {
+	case "unknown":
+		rst_type = RstByUnknown
+	case "headers":
+		rst_type = RstByHeaderFrames
+	case "window_update":
+		rst_type = RstByWndUpdateFrames
+	case "priority":
+		rst_type = RstByPriority
+	default:
+		panic(fmt.Errorf("Unknown frame type which causes RST: %s", s))
+	}
+
+	if (frame_type != CtrlFrameRst && rst_type != RstByUnknown) {
+		panic(fmt.Errorf("frame type which causes RST specified for not RST flood"))
+	}
+	if (frame_type == CtrlFrameRst && rst_type == RstByUnknown) {
+		panic(fmt.Errorf("frame type which causes RST not specified for RST flood"))
+	}
+
+	return rst_type
+}
+
 func init() {
 	flag.StringVar(&address, "address", ":443", "server address")
 	flag.StringVar(&host, "host", "localhost", "host/authority header")
 	flag.IntVar(&threads, "threads", 1, "number of threads to start")
 	flag.IntVar(&connections, "connections", 1, "number of connections to start")
 	flag.StringVar(&ctrl_frame_type, "ctrl_frame_type", "ping_frame", "type of control frames to flood")
+	flag.StringVar(&rst_reason_type, "rst_reason_type", "unknown", "type of frames which causes RST stream flood")
 	flag.IntVar(&frame_count, "frame_count", 100000, "count of frames to flood")
 	flag.IntVar(&debug, "debug", 0, "debug level")
 	flag.Parse()
@@ -96,8 +136,41 @@ func main() {
 	<-done
 }
 
+func rst_frame_flood(framer *http2.Framer, stream_id uint32, rst_type RstByType) error  {
+	blockBuffer := bytes.Buffer{}
+	henc := hpack.NewEncoder(&blockBuffer)
+	henc.WriteField(hpack.HeaderField{Name: ":method", Value: "POST"})
+	henc.WriteField(hpack.HeaderField{Name: ":path", Value: "/"})
+	henc.WriteField(hpack.HeaderField{Name: ":scheme", Value: "https"})
+	henc.WriteField(hpack.HeaderField{Name: ":authority", Value: "localhost"})
+
+	err := framer.WriteHeaders(http2.HeadersFrameParam{
+		StreamID:	stream_id,
+		EndStream:	false,
+		EndHeaders:	true,
+		BlockFragment:	blockBuffer.Bytes(),
+	})
+
+	if (err != nil) {
+		return err
+	}
+
+	switch rst_type {
+	case RstByHeaderFrames:
+		// Rst stream causes because when streams count exceeded max
+		// concurrent streams, dont need to do anything more.
+	case RstByWndUpdateFrames:
+		err = framer.WriteWindowUpdate(stream_id, 2147483647)
+	case RstByPriority:
+		err = framer.WritePriority(stream_id, http2.PriorityParam {StreamDep: stream_id, Exclusive: false, Weight: 0})
+	}
+
+	return err
+}
+
 func ctrl_frame_flood(cid int, ctrl_frame_type string) {
-	var frame_type = transition(ctrl_frame_type)
+	var frame_type = frameTypeTransition(ctrl_frame_type)
+	var rst_type = rstTypeTransision(frame_type, rst_reason_type)
 	conf := &tls.Config {
 		InsecureSkipVerify: true,
 		NextProtos: []string{"h2"},
@@ -145,12 +218,6 @@ func ctrl_frame_flood(cid int, ctrl_frame_type string) {
 	}()
 
 	var stream_id uint32 = 1
-	blockBuffer := bytes.Buffer{}
-	henc := hpack.NewEncoder(&blockBuffer)
-	henc.WriteField(hpack.HeaderField{Name: ":method", Value: "POST"})
-	henc.WriteField(hpack.HeaderField{Name: ":path", Value: "/"})
-	henc.WriteField(hpack.HeaderField{Name: ":scheme", Value: "https"})
-	henc.WriteField(hpack.HeaderField{Name: ":authority", Value: "localhost"})
 
 	for sentFrames := 0; sentFrames < frame_count; sentFrames++ {
 		switch frame_type {
@@ -158,15 +225,10 @@ func ctrl_frame_flood(cid int, ctrl_frame_type string) {
     			err = framer.WritePing(false, [8]byte{1,2,3,4})
 		case CtrlFrameSettings:
 			err = framer.WriteSettings(http2.Setting{ID: http2.SettingMaxConcurrentStreams, Val : uint32(sentFrames)})
+		case CtrlFrameWndUpdate:
+			err = framer.WriteWindowUpdate(0, 1)
 		case CtrlFrameRst:
-			// Send headers frames until max_concurrent_streams will be
-			// exceeded, then each new headers frame causes RST stream
-			err = framer.WriteHeaders(http2.HeadersFrameParam{
-				StreamID:	stream_id,
-				EndStream:	false,
-				EndHeaders:	true,
-				BlockFragment:	blockBuffer.Bytes(),
-			})
+			err = rst_frame_flood(framer, stream_id, rst_type)
 			stream_id += 2
 		}
 		if err != nil {
