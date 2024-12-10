c1cf5206a353b8e19cf95e61d94dbafa39eec5ae
Roman <rb@tempesta-tech.com>
2024-12-11 00:46:41 +0400

    update ddos test: - add docstring; - remove unnecessary code; - some fixes after review.

--- tools/mhddos/start.py
+++ tools/mhddos/start.py
@@ -2,7 +2,6 @@
 import argparse
 import random
 from contextlib import suppress
-from json import load
 from logging import basicConfig, getLogger, shutdown
 from math import log2, trunc
 from multiprocessing import RawValue
@@ -10,18 +9,8 @@ from os import urandom as randbytes
 from pathlib import Path
 from random import choice as randchoice
 from re import compile
-from socket import (
-    AF_INET,
-    IPPROTO_TCP,
-    SOCK_DGRAM,
-    SOCK_RAW,
-    SOCK_STREAM,
-    TCP_NODELAY,
-    gethostname,
-    socket,
-)
+from socket import AF_INET, IPPROTO_TCP, SOCK_STREAM, TCP_NODELAY, socket
 from ssl import CERT_NONE, SSLContext, create_default_context
-from sys import argv
 from sys import exit as _exit
 from threading import Event, Thread
 from time import sleep, time
@@ -32,11 +21,8 @@ import urllib3
 import yarl
 from certifi import where
 from cloudscraper import create_scraper
-from dns import resolver
-from icmplib import ping
-from psutil import cpu_percent, net_io_counters, process_iter, virtual_memory
 from PyRoxy import Tools as ProxyTools
-from requests import Response, Session, cookies, get
+from requests import Response, Session, cookies
 
 basicConfig(format="[%(asctime)s - %(levelname)s] %(message)s", datefmt="%H:%M:%S")
 logger = getLogger("MHDDoS")
@@ -87,13 +73,6 @@ tor2webs = [
     "s5.tor-gateways.de",
 ]
 
-with open(__dir__ / "config.json") as f:
-    con = load(f)
-
-with socket(AF_INET, SOCK_DGRAM) as s:
-    s.connect(("8.8.8.8", 80))
-    __ip__ = s.getsockname()[0]
-
 
 class bcolors:
     HEADER = "\033[95m"
@@ -115,34 +94,6 @@ def exit(*message):
 
 
 class Methods:
-    LAYER7_METHODS: Set[str] = {
-        "CFB",
-        "BYPASS",
-        "GET",
-        "POST",
-        "OVH",
-        "STRESS",
-        "DYN",
-        "SLOW",
-        "HEAD",
-        "NULL",
-        "COOKIE",
-        "PPS",
-        "EVEN",
-        "GSB",
-        "DGB",
-        "AVB",
-        "CFBUAM",
-        "APACHE",
-        "XMLRPC",
-        "BOT",
-        "DOWNLOADER",
-        "KILLER",
-        "TOR",
-        "RHEX",
-        "STOMP",
-    }
-
     # attack methods used for TempestaFW
     VALID_L7_METHODS = {
         "GET",
@@ -174,26 +125,6 @@ class Methods:
         "CFB",
         "BYPASS",
     }
-
-    LAYER4_AMP: Set[str] = {"MEM", "NTP", "DNS", "ARD", "CLDAP", "CHAR", "RDP"}
-
-    LAYER4_METHODS: Set[str] = {
-        *LAYER4_AMP,
-        "TCP",
-        "UDP",
-        "SYN",
-        "VSE",
-        "MINECRAFT",
-        "MCBOT",
-        "CONNECTION",
-        "CPS",
-        "FIVEM",
-        "TS3",
-        "MCPE",
-        "ICMP",
-    }
-
-    ALL_METHODS: Set[str] = {*LAYER4_METHODS, *LAYER7_METHODS}
 
 
 google_agents = [
@@ -239,8 +170,7 @@ class Tools:
             value = i / pow(base, multiple)
             suffix = MULTIPLES[multiple].format("i" if binary else "")
             return f"{value:.{precision}f} {suffix}"
-        else:
-            return "-- B"
+        return "-- B"
 
     @staticmethod
     def humanformat(num: int, precision: int = 2):
@@ -434,10 +364,7 @@ class HttpFlood(Thread):
             self.open_connection = self._open_connection
 
     def select(self, name: str) -> None:
-        self.SENT_FLOOD = self.GET
-        for key, value in self.methods.items():
-            if name == key:
-                self.SENT_FLOOD = value
+        self.SENT_FLOOD = self.methods.get(name, self.GET)
 
     def run(self) -> None:
         if self._synevent:
@@ -503,7 +430,7 @@ class HttpFlood(Thread):
 
     @staticmethod
     def getMethodType(method: str) -> str:
-        if {method.upper()} & {
+        if method.upper() in {
             "CFB",
             "CFBUAM",
             "GET",
@@ -520,9 +447,9 @@ class HttpFlood(Thread):
             "STOMP",
         }:
             return "GET"
-        elif {method.upper()} & {"POST", "XMLRPC", "STRESS"}:
+        elif method.upper() in {"POST", "XMLRPC", "STRESS"}:
             return "POST"
-        elif {method.upper()} & {"GSB", "HEAD"}:
+        elif method.upper() in {"GSB", "HEAD"}:
             return "HEAD"
         else:
             return "PUT"
@@ -853,279 +780,17 @@ class HttpFlood(Thread):
                     break
 
 
-class ToolsConsole:
-    METHODS = {"INFO", "TSSRV", "CFIP", "DNS", "PING", "CHECK", "DSTAT"}
-
-    @staticmethod
-    def checkRawSocket():
-        with suppress(OSError):
-            with socket(AF_INET, SOCK_RAW, IPPROTO_TCP):
-                return True
-        return False
-
-    @staticmethod
-    def runConsole():
-        cons = f"{gethostname()}@MHTools:~#"
-
-        while 1:
-            cmd = input(cons + " ").strip()
-            if not cmd:
-                continue
-            if " " in cmd:
-                cmd, args = cmd.split(" ", 1)
-
-            cmd = cmd.upper()
-            if cmd == "HELP":
-                print("Tools:" + ", ".join(ToolsConsole.METHODS))
-                print("Commands: HELP, CLEAR, BACK, EXIT")
-                continue
-
-            if {cmd} & {"E", "EXIT", "Q", "QUIT", "LOGOUT", "CLOSE"}:
-                exit(-1)
-
-            if cmd == "CLEAR":
-                print("\033c")
-                continue
-
-            if not {cmd} & ToolsConsole.METHODS:
-                print(f"{cmd} command not found")
-                continue
-
-            if cmd == "DSTAT":
-                with suppress(KeyboardInterrupt):
-                    ld = net_io_counters(pernic=False)
-
-                    while True:
-                        sleep(1)
-
-                        od = ld
-                        ld = net_io_counters(pernic=False)
-
-                        t = [(last - now) for now, last in zip(od, ld)]
-
-                        logger.info(
-                            (
-                                "Bytes Sent %s\n"
-                                "Bytes Received %s\n"
-                                "Packets Sent %s\n"
-                                "Packets Received %s\n"
-                                "ErrIn %s\n"
-                                "ErrOut %s\n"
-                                "DropIn %s\n"
-                                "DropOut %s\n"
-                                "Cpu Usage %s\n"
-                                "Memory %s\n"
-                            )
-                            % (
-                                Tools.humanbytes(t[0]),
-                                Tools.humanbytes(t[1]),
-                                Tools.humanformat(t[2]),
-                                Tools.humanformat(t[3]),
-                                t[4],
-                                t[5],
-                                t[6],
-                                t[7],
-                                str(cpu_percent()) + "%",
-                                str(virtual_memory().percent) + "%",
-                            )
-                        )
-            if cmd in ["CFIP", "DNS"]:
-                print("Soon")
-                continue
-
-            if cmd == "CHECK":
-                while True:
-                    with suppress(Exception):
-                        domain = input(f"{cons}give-me-ipaddress# ")
-                        if not domain:
-                            continue
-                        if domain.upper() == "BACK":
-                            break
-                        if domain.upper() == "CLEAR":
-                            print("\033c")
-                            continue
-                        if {domain.upper()} & {"E", "EXIT", "Q", "QUIT", "LOGOUT", "CLOSE"}:
-                            exit(-1)
-                        if "/" not in domain:
-                            continue
-                        logger.info("please wait ...")
-
-                        with get(domain, timeout=20) as r:
-                            logger.info(
-                                ("status_code: %d\n" "status: %s")
-                                % (r.status_code, "ONLINE" if r.status_code <= 500 else "OFFLINE")
-                            )
-
-            if cmd == "INFO":
-                while True:
-                    domain = input(f"{cons}give-me-ipaddress# ")
-                    if not domain:
-                        continue
-                    if domain.upper() == "BACK":
-                        break
-                    if domain.upper() == "CLEAR":
-                        print("\033c")
-                        continue
-                    if {domain.upper()} & {"E", "EXIT", "Q", "QUIT", "LOGOUT", "CLOSE"}:
-                        exit(-1)
-                    domain = domain.replace("https://", "").replace("http://", "")
-                    if "/" in domain:
-                        domain = domain.split("/")[0]
-                    print("please wait ...", end="\r")
-
-                    info = ToolsConsole.info(domain)
-
-                    if not info["success"]:
-                        print("Error!")
-                        continue
-
-                    logger.info(
-                        ("Country: %s\n" "City: %s\n" "Org: %s\n" "Isp: %s\n" "Region: %s\n")
-                        % (info["country"], info["city"], info["org"], info["isp"], info["region"])
-                    )
-
-            if cmd == "TSSRV":
-                while True:
-                    domain = input(f"{cons}give-me-domain# ")
-                    if not domain:
-                        continue
-                    if domain.upper() == "BACK":
-                        break
-                    if domain.upper() == "CLEAR":
-                        print("\033c")
-                        continue
-                    if {domain.upper()} & {"E", "EXIT", "Q", "QUIT", "LOGOUT", "CLOSE"}:
-                        exit(-1)
-                    domain = domain.replace("https://", "").replace("http://", "")
-                    if "/" in domain:
-                        domain = domain.split("/")[0]
-                    print("please wait ...", end="\r")
-
-                    info = ToolsConsole.ts_srv(domain)
-                    logger.info(f"TCP: {(info['_tsdns._tcp.'])}\n")
-                    logger.info(f"UDP: {(info['_ts3._udp.'])}\n")
-
-            if cmd == "PING":
-                while True:
-                    domain = input(f"{cons}give-me-ipaddress# ")
-                    if not domain:
-                        continue
-                    if domain.upper() == "BACK":
-                        break
-                    if domain.upper() == "CLEAR":
-                        print("\033c")
-                    if {domain.upper()} & {"E", "EXIT", "Q", "QUIT", "LOGOUT", "CLOSE"}:
-                        exit(-1)
-
-                    domain = domain.replace("https://", "").replace("http://", "")
-                    if "/" in domain:
-                        domain = domain.split("/")[0]
-
-                    logger.info("please wait ...")
-                    r = ping(domain, count=5, interval=0.2)
-                    logger.info(
-                        ("Address: %s\n" "Ping: %d\n" "Aceepted Packets: %d/%d\n" "status: %s\n")
-                        % (
-                            r.address,
-                            r.avg_rtt,
-                            r.packets_received,
-                            r.packets_sent,
-                            "ONLINE" if r.is_alive else "OFFLINE",
-                        )
-                    )
-
-    @staticmethod
-    def stop():
-        print("All Attacks has been Stopped !")
-        for proc in process_iter():
-            if proc.name() == "python.exe":
-                proc.kill()
-
-    @staticmethod
-    def usage():
-        print(
-            (
-                "* MHDDoS - DDoS Attack Script With %d Methods\n"
-                "Note: If the Proxy list is empty, The attack will run without proxies\n"
-                "      If the Proxy file doesn't exist, the script will download proxies and check them.\n"
-                "      Proxy Type 0 = All in config.json\n"
-                "      SocksTypes:\n"
-                "         - 6 = RANDOM\n"
-                "         - 5 = SOCKS5\n"
-                "         - 4 = SOCKS4\n"
-                "         - 1 = HTTP\n"
-                "         - 0 = ALL\n"
-                " > Methods:\n"
-                " - Layer4\n"
-                " | %s | %d Methods\n"
-                " - Layer7\n"
-                " | %s | %d Methods\n"
-                " - Tools\n"
-                " | %s | %d Methods\n"
-                " - Others\n"
-                " | %s | %d Methods\n"
-                " - All %d Methods\n"
-                "\n"
-                "Example:\n"
-                "   L7: python3 %s <method> <url> <socks_type> <threads> <proxylist> <rpc> <duration> <debug=optional>\n"
-                "   L4: python3 %s <method> <ip:port> <threads> <duration>\n"
-                "   L4 Proxied: python3 %s <method> <ip:port> <threads> <duration> <socks_type> <proxylist>\n"
-                "   L4 Amplification: python3 %s <method> <ip:port> <threads> <duration> <reflector file (only use with"
-                " Amplification)>\n"
-            )
-            % (
-                len(Methods.ALL_METHODS) + 3 + len(ToolsConsole.METHODS),
-                ", ".join(Methods.LAYER4_METHODS),
-                len(Methods.LAYER4_METHODS),
-                ", ".join(Methods.LAYER7_METHODS),
-                len(Methods.LAYER7_METHODS),
-                ", ".join(ToolsConsole.METHODS),
-                len(ToolsConsole.METHODS),
-                ", ".join(["TOOLS", "HELP", "STOP"]),
-                3,
-                len(Methods.ALL_METHODS) + 3 + len(ToolsConsole.METHODS),
-                argv[0],
-                argv[0],
-                argv[0],
-                argv[0],
-            )
-        )
-
-    # noinspection PyBroadException
-    @staticmethod
-    def ts_srv(domain):
-        records = ["_ts3._udp.", "_tsdns._tcp."]
-        DnsResolver = resolver.Resolver()
-        DnsResolver.timeout = 1
-        DnsResolver.lifetime = 1
-        Info = {}
-        for rec in records:
-            try:
-                srv_records = resolver.resolve(rec + domain, "SRV")
-                for srv in srv_records:
-                    Info[rec] = str(srv.target).rstrip(".") + ":" + str(srv.port)
-            except:
-                Info[rec] = "Not found"
-
-        return Info
-
-    # noinspection PyUnreachableCode
-    @staticmethod
-    def info(domain):
-        with suppress(Exception), get(f"https://ipwhois.app/json/{domain}/") as s:
-            return s.json()
-        return {"success": False}
-
-
 if __name__ == "__main__":
     with suppress(KeyboardInterrupt):
         with suppress(IndexError):
             parser = argparse.ArgumentParser(description="ddos tools")
             parser.add_argument("--url", type=str, default="http://localhost")
-            parser.add_argument("--threads", type=int, default=1, help="threads per each method")
-            parser.add_argument("--rpc", type=int, default=1)
+            parser.add_argument("--threads", type=int, default=1, help="total number of threads")
+            parser.add_argument("--rpc", type=int, default=1, help="requests per connection")
             parser.add_argument("--duration", type=int, default=1)
-            parser.add_argument("--hostname", type=str, default="localhost")
+            parser.add_argument(
+                "--hostname", type=str, default="localhost", help="Host header and ssl hostname."
+            )
             arguments = parser.parse_args()
 
             URL = yarl.URL(arguments.url)
@@ -1190,5 +855,3 @@ if __name__ == "__main__":
 
             EVENT.clear()
             exit()
-
-        ToolsConsole.usage()
