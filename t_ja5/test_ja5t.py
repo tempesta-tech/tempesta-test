4d76e1fef7eaaf9ec06bfd913542a1fb8b700fa1
symstu <stukalo.maksym@gmail.com>
2025-01-27 00:14:56 +0100

    fixed unstable the ja5 stress test on seg

--- t_ja5/test_ja5t.py
+++ t_ja5/test_ja5t.py
@@ -4,6 +4,8 @@ __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2024 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+import time
+
 from helpers import dmesg, tf_cfg
 from helpers.control import Tempesta
 from test_suite import marks, tester
@@ -86,7 +88,7 @@ class TestJa5t(tester.TempestaTest):
                 name="hash_for_client_not_block",
                 tempesta_ja5_config="""
 					ja5t {
-						hash 66cb9fd8d4250000 1 10;
+						hash 66cb9fd8d4250000 1 100;
 					}
 				""",
                 expected_block=False,
@@ -175,7 +177,7 @@ class TestJa5tStress(tester.TempestaTest):
     tempesta_ja5_config_2 = """
         ja5t {
             hash deadbeef 10 1000;
-            hash 1f5a9a29ef170000 1 2;
+            hash 1f5a9a29ef170000 1 100;
         }
     """
     tempesta_ja5_config_empty = ""
@@ -184,14 +186,17 @@ class TestJa5tStress(tester.TempestaTest):
         tempesta: Tempesta = self.get_tempesta()
         config = tempesta.config.defconfig
         ja5_configs = [
+            self.tempesta_ja5_config_empty,
             self.tempesta_ja5_config_1,
             self.tempesta_ja5_config_2,
-            self.tempesta_ja5_config_empty,
         ] * 4
 
         for ja5_config in ja5_configs:
             tempesta.config.defconfig = config + ja5_config
             self.get_tempesta().reload()
+
+            # sleep sometime to receive 200 responses
+            time.sleep(0.5)
 
     @marks.Parameterize.expand(
         [
