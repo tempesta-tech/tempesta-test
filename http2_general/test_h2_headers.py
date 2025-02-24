f028a49c2dcc2cdde4e8a890bed56462c89447e9
symstu <stukalo.maksym@gmail.com>
2025-02-24 23:17:36 +0100

    save

--- http2_general/test_h2_headers.py
+++ http2_general/test_h2_headers.py
@@ -1418,3 +1418,195 @@ class TestHeadersBlockedByMaxHeaderListSize(tester.TempestaTest):
 
         deproxy_cl.wait_for_response(strict=True)
         self.assertEqual(deproxy_cl.last_response.status, "200")
+
+
+import string
+
+from test_suite import marks, tester
+
+__author__ = "Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2018-2025 Tempesta Technologies, Inc."
+__license__ = "GPL2"
+
+
+class CustomTemplate(string.Template):
+    delimiter = "&"
+
+
+@marks.parameterize_class(
+    [
+        {"name": "MethodHEADStatus200", "method": "HEAD", "status": "200"},
+        {"name": "MethodPOSTStatus200", "method": "POST", "status": "200"},
+        {"name": "MethodDeleteStatus200", "method": "DELETE", "status": "200"},
+        {"name": "MethodDeleteStatus204", "method": "DELETE", "status": "204"},
+        {"name": "MethodPatchStatus200", "method": "PATCH", "status": "200"},
+        {
+            "name": "MethodPUTStatus200",
+            "method": "PUT",
+            "status": "200",
+        },
+        {
+            "name": "MethodPUTStatus204",
+            "method": "PUT",
+            "status": "204",
+        },
+    ]
+)
+class TestMissedContentLengthInMethod(tester.TempestaTest):
+    backends = [
+        {
+            "id": "nginx",
+            "type": "nginx",
+            "port": "8000",
+            "status_uri": "http://${server_ip}:8000/nginx_status",
+            "config": (
+                "pid ${pid};\n"
+                "worker_processes  auto;\n"
+                "events { "
+                "   worker_connections   1024;\n"
+                "   use epoll;\n"
+                "}\n"
+                "http {\n"
+                "   keepalive_timeout ${server_keepalive_timeout};\n"
+                "   keepalive_requests ${server_keepalive_requests};\n"
+                "   access_log off;\n"
+                "   server {\n"
+                "       listen        ${server_ip}:8000;\n"
+                "       location / {\n"
+                "          return &response_status;\n"
+                "       }\n"
+                "       location /nginx_status {\n"
+                "           stub_status on;\n"
+                "       }\n"
+                "   }\n"
+                "}\n"
+            ),
+        }
+    ]
+
+    tempesta = {
+        "config": """
+            listen 80;
+            access_log dmesg;
+            frang_limits {http_methods OPTIONS HEAD GET PUT POST PUT PATCH DELETE;}
+            server ${server_ip}:8000;
+        """
+    }
+
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        },
+    ]
+    method: str = None
+    status: str = None
+
+    def setUp(self):
+        nginx_conf = self.backends[0]["config"]
+        self.backends[0]["config"] = CustomTemplate(nginx_conf).substitute(
+            response_status=self.status,
+        )
+        super().setUp()
+
+    def test_request_success(self):
+        self.start_all_services()
+        client = self.get_client("deproxy")
+        client.send_request(
+            request=client.create_request(method=self.method, headers=[]),
+            expected_status_code=self.status,
+        )
+        self.assertEqual(
+            client.last_response.headers["content-length"],
+            "0",
+            msg=f"Tempesta should proxy the Content-Length header for the {self.status} status code also",
+        )
+
+
+@marks.parameterize_class(
+    [
+        {
+            "name": "POST",
+            "method": "POST",
+        },
+        {
+            "name": "PUT",
+            "method": "PUT",
+        },
+        {
+            "name": "PATCH",
+            "method": "PATCH",
+        },
+        {
+            "name": "DELETE",
+            "method": "DELETE",
+        },
+    ]
+)
+class TestMissedContentType(tester.TempestaTest):
+    backends = [
+        {
+            "id": "nginx",
+            "type": "nginx",
+            "port": "8000",
+            "status_uri": "http://${server_ip}:8000/nginx_status",
+            "config": (
+                "pid ${pid};\n"
+                "worker_processes  auto;\n"
+                "events {\n"
+                "   worker_connections   1024;\n"
+                "   use epoll;\n"
+                "}\n"
+                "http {\n"
+                "   keepalive_timeout ${server_keepalive_timeout};\n"
+                "   keepalive_requests ${server_keepalive_requests};\n"
+                "   access_log off;\n"
+                "   server {\n"
+                "       listen        ${server_ip}:8000;\n"
+                "       location / {\n"
+                "          default_type 'text/html; charset=utf-8';\n"
+                "          return 200;\n"
+                "       }\n"
+                "       location /nginx_status {\n"
+                "           stub_status on;\n"
+                "       }\n"
+                "   }\n"
+                "}\n"
+            ),
+        }
+    ]
+
+    tempesta = {
+        "config": """
+            listen 80;
+            access_log dmesg;
+            frang_limits {http_methods OPTIONS HEAD GET PUT POST PUT PATCH DELETE;}
+            http_allow_empty_body_content_type true;
+            server ${server_ip}:8000;
+        """
+    }
+
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        },
+    ]
+    method: str = None
+
+    def test_request_success(self):
+        self.start_all_services()
+        client = self.get_client("deproxy")
+        client.send_request(
+            request=client.create_request(method=self.method, headers=[]),
+            expected_status_code="200",
+        )
+        self.assertEqual(
+            client.last_response.headers["content-type"],
+            "text/html; charset=utf-8",
+            msg="Tempesta should proxy the Content-Type header for the CRUD method with empty body also",
+        )
