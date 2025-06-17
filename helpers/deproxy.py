0d68e0ddb98dcbeca0e0b5f50d1f3d6acc326545
EvgeniiMekhanik <em@tempesta-tech.com>
2025-06-17 06:57:34 +0300

    Add more tests to check hpack
    Add tests to check how Tempesta FW load method
    from hpack dynamic table.

--- helpers/deproxy.py
+++ helpers/deproxy.py
@@ -563,6 +563,9 @@ class Request(HttpMessage):
         "UNLOCK",
         "UPDATE",
         "UPDATEREDIRECTREF",
+        "PUTA",
+        "GETA",
+        "OPTIONA",
         "VERSION-CONTROL",
         # Not RFC methods:
         "PURGE",
