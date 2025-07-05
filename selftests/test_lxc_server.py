e1eee6d9b7840d253767d8ded3dee30ade11fd5d
Roman <rb@tempesta-tech.com>
2025-07-05 17:30:56 +0400

    rework start and stop method in lxc_server.py: - remove the starting and stopping lxc container, because it's require a lot of resources and time; - now lxc container must be run first; - rework setup.py for that (now lxc container uses 7000 port);

--- selftests/test_lxc_server.py
+++ selftests/test_lxc_server.py
@@ -1,6 +1,5 @@
 """Tests for `framework.lxc_server.LXCServer`."""
 
-from helpers import remote
 from test_suite import tester
 
 
@@ -8,11 +7,11 @@ class TestLxcServer(tester.TempestaTest):
     tempesta = {
         "config": """
         listen 80 proto=http;
-        server ${server_ip}:8000;
+        server ${server_ip}:${server_website_port};
         """
     }
 
-    backends = [{"id": "lxc", "type": "lxc", "external_port": "8000", "make_snapshot": False}]
+    backends = [{"id": "lxc", "type": "lxc"}]
 
     clients = [
         {
@@ -38,21 +37,14 @@ class TestLxcServer(tester.TempestaTest):
     def test_wait_for_connections_negative(self):
         """
         `wait_for_connections` method MUST return False
-        if Tempesta and server has different connection ports.
+        if Tempesta doesn't create connection to lxc ports.
         """
         server = self.get_server("lxc")
-        server.external_port = "19000"
 
         server.start()
-        self.start_tempesta()
         self.assertFalse(server.wait_for_connections(1))
 
     def test_status(self):
         """`server.status` MUST return a correct container status."""
         server = self.get_server("lxc")
-        self.assertEqual(server.status, "stopped")
-
-        server.start()
-        self.start_tempesta()
-        self.assertTrue(server.wait_for_connections(3))
         self.assertEqual(server.status, "running")
