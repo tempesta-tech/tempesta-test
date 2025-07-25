1226a854d8ff2746a1cb96bc6bbd126902cf8b3e
Roman <rb@tempesta-tech.com>
2025-07-25 15:12:46 +0400

    add tests for log loss during startup and selftest for clickhouse update tests_disabled* files

---
+++ selftests/test_clickhouse.py
@@ -0,0 +1,53 @@
+__author__ = "Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2025 Tempesta Technologies, Inc."
+__license__ = "GPL2"
+
+
+from test_suite import tester
+
+
+class TestClickhouse(tester.TempestaTest):
+    backends = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "port": "8000",
+            "response": "static",
+            "response_content": "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n",
+        },
+    ]
+    tempesta = {
+        "config": """
+            listen 80;
+            server ${server_ip}:8000;
+            access_log mmap logger_config=${tfw_logger_logger_config};
+        """
+    }
+
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy",
+            "addr": "${tempesta_ip}",
+            "port": "80",
+        }
+    ]
+
+    def test_wait_while_logger_start(self):
+        """
+        Test that documents issue #2314 - logs missing during first second of startup.
+        This test verifies that logs are not lost in clickhouse immediately after startup.
+        """
+        self.start_all_clients()
+        self.deproxy_manager.start()
+
+        self.start_tempesta()
+        self.get_tempesta().wait_while_logger_start()
+
+        client = self.get_client("deproxy")
+        client.start()
+
+        client.send_request(client.create_request(method="GET", headers=[]), "502")
+
+        self.assertWaitUntilEqual(self.loggers.clickhouse.access_log_records_count, 1)
+        self.assertEqual(self.loggers.clickhouse.access_log_last_message().status, 502)
