468023d2de0d06ef91fdcf5ecc9b155e016b461e
Constantine <kt@tempesta-tech.com>
2025-12-10 15:27:19 +0200

    fix: connection establishing
    Before this patch deproxy_client coldn't establish connection without
    sending request. `connect()` called from `start()` method is async
    and finishing of connection establising must be done in
    `handle_write()`. Connection is established only if socket is ready to
    write. But in previous version we couldn't reach `handle_write()`
    handler because `writable()` always returned `False`, due zero number
    of pending request at connection stage. To fix this we introduced two
    the new versions of `writable()` and `handle_write()` those used when
    connection is not established and changed to another two when
    connection established.

--- framework/deproxy_client.py
+++ framework/deproxy_client.py
@@ -64,6 +64,9 @@ class BaseDeproxyClient(BaseDeproxy, abc.ABC):
             is_ipv6=is_ipv6,
         )
 
+        self.writable = self._in_connecting_state
+        self.handle_write = self.__setup_write
+
         self.ssl = is_ssl
         self._is_http2 = isinstance(self, DeproxyClientH2)
         self._create_context()
@@ -146,6 +149,50 @@ class BaseDeproxyClient(BaseDeproxy, abc.ABC):
                 expected_error_code in self.__error_codes
             ), f"{expected_error_code} not found in {self.__error_codes}\n{msg}"
 
+    def _in_connecting_state(self):
+        return self.connecting
+
+    def _has_pending_data(self):
+        if self.cur_req_num >= self.nrreq:
+            return False
+        if time.time() < self.next_request_time():
+            return False
+        if (
+            self.segment_gap != 0
+            and time.time() - self.last_segment_time < self.segment_gap / 1000.0
+        ):
+            return False
+        return True
+
+    def _send_data(self):
+        """Send data from `self.request_buffers` and cut them."""
+        reqs = self.request_buffers[self.cur_req_num]
+
+        sent = self.send(reqs[: self.segment_size] if self.segment_size else reqs)
+        if sent < 0:
+            return
+        self.last_segment_time = time.time()
+        self.request_buffers[self.cur_req_num] = reqs[sent:]
+        if len(self.request_buffers[self.cur_req_num]) == 0:
+            self.cur_req_num += 1
+            self._http_logger.info(
+                f"A request was send. The current number of a request - {self.cur_req_num}"
+            )
+
+    def __setup_write(self):
+        self.writable = self._has_pending_data
+        self.handle_write = self._send_data
+
+        # Connection established and client has pending data to send
+        if self.writable():
+            self.handle_write()
+
+    def _do_close(self):
+        self.close()
+        self.writable = self._in_connecting_state
+        self.handle_write = self.__setup_write
+        self.conn_is_closed = True
+
     def handle_connect(self):
         if self.ssl:
             self.socket = self._context.wrap_socket(
@@ -156,8 +203,7 @@ class BaseDeproxyClient(BaseDeproxy, abc.ABC):
         self.start_time = time.time()
 
     def handle_close(self):
-        self.close()
-        self.conn_is_closed = True
+        self._do_close()
         self.state = stateful.STATE_STOPPED
 
     def handle_error(self):
@@ -189,7 +235,7 @@ class BaseDeproxyClient(BaseDeproxy, abc.ABC):
         self.rps = rps
 
     def _stop_deproxy(self):
-        self.close()
+        self._do_close()
 
     def _run_deproxy(self):
         self.create_socket(socket.AF_INET6 if self.is_ipv6 else socket.AF_INET, socket.SOCK_STREAM)
@@ -205,37 +251,10 @@ class BaseDeproxyClient(BaseDeproxy, abc.ABC):
     @abc.abstractmethod
     def handle_read(self): ...
 
-    def writable(self):
-        if self.cur_req_num >= self.nrreq:
-            return False
-        if time.time() < self.next_request_time():
-            return False
-        if (
-            self.segment_gap != 0
-            and time.time() - self.last_segment_time < self.segment_gap / 1000.0
-        ):
-            return False
-        return True
-
     def next_request_time(self):
         if self.rps == 0:
             return self.start_time
         return self.start_time + float(self.cur_req_num) / self.rps
-
-    def handle_write(self):
-        """Send data from `self.request_buffers` and cut them."""
-        reqs = self.request_buffers[self.cur_req_num]
-
-        sent = self.send(reqs[: self.segment_size] if self.segment_size else reqs)
-        if sent < 0:
-            return
-        self.last_segment_time = time.time()
-        self.request_buffers[self.cur_req_num] = reqs[sent:]
-        if len(self.request_buffers[self.cur_req_num]) == 0:
-            self.cur_req_num += 1
-            self._http_logger.info(
-                f"A request was send. The current number of a request - {self.cur_req_num}"
-            )
 
     @abc.abstractmethod
     def make_requests(self, requests): ...
@@ -758,7 +777,7 @@ class DeproxyClientH2(BaseDeproxyClient):
             )
             raise
 
-    def handle_write(self):
+    def _send_data(self):
         """
         Send data from `self.request_buffers` and cut them.
         Move data from `self.req_body_buffers` to `self.request_buffers`
@@ -769,7 +788,7 @@ class DeproxyClientH2(BaseDeproxyClient):
         cur_req_num = self.cur_req_num
 
         if self.request_buffers[cur_req_num]:
-            super().handle_write()
+            super()._send_data()
 
         body = self.req_body_buffers[cur_req_num].body
         if self.request_buffers[cur_req_num] == b"" and body is not None:
