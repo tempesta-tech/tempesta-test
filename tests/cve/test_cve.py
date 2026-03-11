efa2594f4646068392e6dfdaf66266ee6c4a767c
Roman Belozerov <rb@tempesta-tech.com>
2026-03-11 14:07:27 +0400

    Move header leak tests to CVE directory

--- tests/cve/test_cve.py
+++ tests/cve/test_cve.py
@@ -2,10 +2,14 @@ __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2026 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+import random
+import string
 from pathlib import Path
 
+from hyperframe.frame import HeadersFrame
+
 from framework.deproxy.deproxy_message import HttpMessage
-from framework.helpers import dmesg, remote, tf_cfg
+from framework.helpers import dmesg, memworker, remote, tf_cfg
 from framework.test_suite import marks, tester
 
 
@@ -353,3 +357,137 @@ class TestHttp2FrameFlood(tester.TempestaTest):
         self.assertEqual(0, flood_client.returncode)
         tempesta.get_stats()
         self.assertEqual(tempesta.stats.__dict__[stat_name], 100)
+
+
+class TestH2Headers(tester.TempestaTest):
+
+    clients = [
+        {
+            "id": "deproxy",
+            "type": "deproxy_h2",
+            "addr": "${tempesta_ip}",
+            "port": "443",
+            "ssl": True,
+            "ssl_hostname": "tempesta-tech.com",
+        },
+    ]
+
+    backends = [
+        {
+            "id": "nginx",
+            "type": "nginx",
+            "status_uri": "http://${server_ip}:8000/nginx_status",
+            "config": """
+pid ${pid};
+worker_processes  auto;
+events {
+    worker_connections   1024;
+    use epoll;
+}
+http {
+    keepalive_timeout ${server_keepalive_timeout};
+    keepalive_requests 10;
+    tcp_nopush       on;
+    tcp_nodelay      on;
+    error_log /dev/null emerg;
+    access_log off;
+    server {
+        listen        ${server_ip}:8000;
+        location / {
+            return 200 'foo';
+        }
+        location /nginx_status {
+            stub_status on;
+        }
+    }
+}
+""",
+        }
+    ]
+
+    tempesta = {
+        "config": """
+        cache 0;
+
+        keepalive_timeout 1000;
+
+        listen 443 proto=h2;
+
+        tls_match_any_server_name;
+        max_concurrent_streams 10000;
+
+        srv_group default {
+            server ${server_ip}:8000;
+        }
+        frang_limits {
+            http_strict_host_checking false;
+            http_header_cnt 1000;
+        }
+
+        ctrl_frame_rate_multiplier 65536;
+
+        vhost tempesta-tech.com {
+           tls_certificate ${tempesta_workdir}/tempesta.crt;
+           tls_certificate_key ${tempesta_workdir}/tempesta.key;
+           proxy_pass default;
+        }
+        """
+    }
+
+    @staticmethod
+    def randomword(length):
+        letters = string.ascii_lowercase
+        return "".join(random.choice(letters) for i in range(length))
+
+    @dmesg.limited_rate_on_tempesta_node
+    def test_2019_9516(self):
+        """
+        CVE-2019-9516 “0-Length Headers Leak”
+        Some HTTP/2 implementations are vulnerable to a header leak, potentially leading to a denial of service.
+        The attacker sends a stream of headers with a 0-length header name and 0-length header value,
+        optionally Huffman encoded into 1-byte or greater headers. Some implementations allocate memory
+        for these headers and keep the allocation alive until the session dies. This can consume excess memory.
+
+        1. For 0-length header name, tempesta returns 400 and GOAWAY and
+        closes the connection, so no further effect happens after.
+        2. If we send a request followed by RST_STREAM, temepesta will close
+        the connection when the response arrives from the backend,
+        so no further effect too.
+        """
+        self.start_all_services()
+        client = self.get_client("deproxy")
+
+        request = client.create_request(
+            uri="/",
+            method="GET",
+            headers=[(self.randomword(100), self.randomword(100)) for _ in range(50)],
+        )
+
+        # create http2 connection and stream 1. The stream and connection are open.
+        # It is necessary for check of a memory consumption
+        client.make_request(client.create_request(method="GET", headers=[]), end_stream=False)
+
+        with memworker.check_memory_consumptions(self):
+            for stream_id in range(3, 20000, 2):
+                client.stream_id = stream_id
+                client.make_request(request, end_stream=False)
+
+                # send trailer headers with invalid `x-forwarded-for` header
+                # it is necessary for calling the RST_STREAM
+                client.send_bytes(
+                    HeadersFrame(
+                        stream_id=stream_id,
+                        data=client.h2_connection.encoder.encode(
+                            [("x-forwarded-for", "1.1.1.1.1.1")]
+                        ),
+                        flags=["END_STREAM", "END_HEADERS"],
+                    ).serialize(),
+                    expect_response=True,
+                )
+
+            self.assertTrue(client.wait_for_response(120))
+
+        # close first stream and http2 connection and finish test
+        client.stream_id = 1
+        client.make_request("data", end_stream=True)
+        self.assertTrue(client.wait_for_response())
