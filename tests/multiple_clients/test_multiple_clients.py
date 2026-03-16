32e7704d8ca95356417443fe616fb40befd5eb2b
Roman Belozerov <rb@tempesta-tech.com>
2026-03-16 12:11:18 +0400

    add async to new tests

--- tests/multiple_clients/test_multiple_clients.py
+++ tests/multiple_clients/test_multiple_clients.py
@@ -2,6 +2,7 @@ __author__ = "Tempesta Technologies, Inc."
 __copyright__ = "Copyright (C) 2026 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
+import asyncio
 import time
 
 from framework.deproxy import deproxy_message
@@ -56,8 +57,8 @@ server ${server_ip}:8000;
         for n in range(clients_n)
     ]
 
-    def disable_close_connection_and_send_requests(self):
-        self.start_all_services()
+    async def disable_close_connection_and_send_requests(self):
+        await self.start_all_services()
         request = self.get_client("deproxy-0").create_request(
             method="GET", headers=[("Content-Type", "invalid")]
         )
@@ -66,9 +67,9 @@ server ${server_ip}:8000;
             client.close_connection_for_tcp_fin = False
 
         for client in self.get_clients():
-            client.send_request(request)
+            await client.send_request(request)
 
-    def test(self):
+    async def test(self):
         """
         Run several clients from different ips. Since `client_lru_size` is equal to 1
         Tempesta FW client free list has only one element and will be exceeded when
@@ -76,18 +77,18 @@ server ${server_ip}:8000;
         database, otherwise if client connection hung we can't destroy it during Tempesta
         FW stopping.
         """
-        self.disable_close_connection_and_send_requests()
+        await self.disable_close_connection_and_send_requests()
         self.get_tempesta().stop()
 
-    def test_tcp_fin_timeout(self):
+    async def test_tcp_fin_timeout(self):
         """
         The same test as previous but we check that hung connections will be dropped
         after timeout (set inside Tempesta FW to 10 seconds if socket is shutdowned)
         will be expired.
         """
         tcp_fin_timeout = 10
-        self.disable_close_connection_and_send_requests()
-        time.sleep(tcp_fin_timeout + 1)
+        await self.disable_close_connection_and_send_requests()
+        await asyncio.sleep(tcp_fin_timeout + 1)
 
         t = time.time()
         self.get_tempesta().stop()
